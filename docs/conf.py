afc4b33aad3defeebfa0d3748a35310ba29ee59a
Nicolas Trangez <ikke@nicolast.be>
2018-07-23 17:07:03 +0200

    Release: Prepare 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'Scality'
 # The short X.Y version
 version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0-dev'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
