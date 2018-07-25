4e221e55cdff80ff7f5ba43d200a1b0f9b94b446
Nicolas Trangez <ikke@nicolast.be>
2018-07-25 16:20:01 +0200

    artwork: Import a script to render logos

---
+++ artwork/generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+set -u
+
+XSLTPROC=$(command -v xsltproc)
+INKSCAPE=$(command -v inkscape)
+
+function generate_svg() {
+        local kind=$1
+        local color=$2
+        local color_code=$3
+
+        local src="metalk8s-logo-${kind}.svg"
+        local dst="generated/metalk8s-logo-${kind}-${color}.svg"
+
+        ${XSLTPROC} --novalid --stringparam color "${color_code}" color.xsl "${src}" > "${dst}.tmp"
+        mv "${dst}.tmp" "${dst}"
+}
+
+function generate_png() {
+        local kind=$1
+        local color=$2
+        local width=$3
+
+        local src="generated/metalk8s-logo-${kind}-${color}.svg"
+        local dst="generated/metalk8s-logo-${kind}-${color}-${width}.png"
+
+        ${INKSCAPE} -D -e "${dst}.tmp" -w "${width}" -z "${src}"
+        mv "${dst}.tmp" "${dst}"
+}
+
+mkdir -p generated/
