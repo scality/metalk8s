4ddd0bc330d9657a1055627d8c221ad871c5c00e
Nicolas Trangez <ikke@nicolast.be>
2018-07-25 20:54:03 +0200

    docs: Add logo to PDF version

--- artwork/generate.sh
+++ artwork/generate.sh
@@ -30,9 +30,23 @@ function generate_png() {
         mv "${dst}.tmp" "${dst}"
 }
 
+function generate_pdf() {
+        local kind=$1
+        local color=$2
+
+        local src="generated/metalk8s-logo-${kind}-${color}.svg"
+        local dst="generated/metalk8s-logo-${kind}-${color}.pdf"
+
+        ${INKSCAPE} -D --export-pdf "${dst}.tmp" --export-pdf-version="1.4" -z "${src}"
+        mv "${dst}.tmp" "${dst}"
+}
+
 mkdir -p generated/
 
 generate_svg wide white '#FFFFFF'
 generate_svg vertical black '#000000'
+generate_svg wide black '#000000'
 
 generate_png wide white 200
+
+generate_pdf wide black
