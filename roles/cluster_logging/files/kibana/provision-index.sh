6655a417218b87d9699400e6531406d0d3b9c5cd
alxf <alxf@vmail.me>
2018-07-31 09:45:46 +0200

    Fix job shell script after review

--- roles/cluster_logging/files/kibana/provision-index.sh
+++ roles/cluster_logging/files/kibana/provision-index.sh
@@ -4,11 +4,13 @@
 # Script to setup the default index pattern in kibana
 #
 
+set -ue
+
 INDEX_ID="logstash-*"
 
-KIBANA="http://kibana:443"
+API_HOSTNAME="$KIBANA_SERVICE_HOST:$KIBANA_SERVICE_PORT"
 
-API_READY=0
+COUNTDOWN=3
 
 
 set_index_pattern()
@@ -17,7 +19,7 @@ set_index_pattern()
         -XPOST \
         -H "Content-type: application/json" \
         -H "kbn-xsrf: anything" \
-        "$KIBANA/api/saved_objects/index-pattern/$INDEX_ID" \
+        "http://$API_HOSTNAME/api/saved_objects/index-pattern/$INDEX_ID" \
         -d "{\"attributes\": {\"title\": \"$INDEX_ID\", \"timeFieldName\": \"@timestamp\"}}"
 }
 
@@ -27,36 +29,41 @@ set_default_index()
         -XPOST \
         -H "Content-type: application/json" \
         -H "kbn-xsrf: anything" \
-        "$KIBANA/api/kibana/settings/defaultIndex" \
+        "http://$API_HOSTNAME/api/kibana/settings/defaultIndex" \
         -d "{\"value\": \"$INDEX_ID\"}"
 }
 
 
 # Wait for kibana service to be ready
-while [ "$API_READY" -eq 0 ]; do
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" "$KIBANA")
+while [ "$COUNTDOWN" -gt 0 ]; do
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" "$API_HOSTNAME")
     if [ "$http_code" = "200" ]; then
-        API_READY=1
+        break
     else
-        sleep 30
+        COUNTDOWN=$((COUNTDOWN - 1))
+        if [ "$COUNTDOWN" -le 0 ]; then
+            echo "Error: Can't connect to the kibana api server"
+            exit 1
+        else
+            sleep 30
+        fi
     fi
 done
 
 http_code=$(set_index_pattern)
 
 if [ "$http_code" = "409" ]; then
-    # Index is already registered
-    exit 0
-fi
-
-if [ "$http_code" != "200" ]; then
-    # An error occured
+    echo "Index pattern '$INDEX_ID' is already registered"
+elif [ "$http_code" != "200" ]; then
+    echo "Error: An error occured when trying to set kibana index pattern (http code: $http_code)"
     exit 1
 fi
 
 http_code=$(set_default_index)
 
 if [ "$http_code" != "200" ]; then
-    # An error occured
+    echo "Error: An error occured when trying to set default kibana index (http code: $http_code)"
     exit 1
 fi
+
+echo "Successful register '$INDEX_ID' as kibana index pattern"
