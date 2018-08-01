eecfa72437dfb3f95d7f4654d3d4cf0eca87ff26
alxf <alex.foulon@scality.com>
2018-08-01 14:36:32 +0200

    kube_prometheus: customize tags from Makefile

--- roles/kube_prometheus/hack/fix-dashboard.py
+++ roles/kube_prometheus/hack/fix-dashboard.py
@@ -21,7 +21,7 @@ def _iter_panels(dashboard):
             yield panel
 
 
-def main(dashboard_data, output, filename, title=None):
+def main(dashboard_data, output, filename, title=None, tags=None):
     name = os.path.splitext(filename)[0]
 
     dashboard = json.load(dashboard_data)
@@ -65,6 +65,13 @@ def main(dashboard_data, output, filename, title=None):
                     '$' not in datasource):
                 annotation['datasource'] = source_template
                 change = True
+
+    if tags is not None:
+        # reset initial tags list
+        dashboard['tags'] = []
+        for tag in tags.split(','):
+            if tag not in dashboard['tags']:
+                dashboard['tags'].append(tag)
 
     if change and not has_source:
         __input = dashboard.setdefault('__inputs', [])
@@ -129,4 +136,5 @@ if __name__ == '__main__':
         sys.stdout,
         filename=os.environ['FILENAME'],
         title=os.environ.get('TITLE', None),
+        tags=os.environ.get('TAGS', None),
     )
