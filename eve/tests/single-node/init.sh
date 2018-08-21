1b89ffbc0b7c00fd3feb3356fba072b44e3e84df
Julien Girardin <julien.girardin@scality.com>
2018-08-21 17:33:07 +0200

    Init script for single-node require root access directly

--- eve/tests/single-node/init.sh
+++ eve/tests/single-node/init.sh
@@ -3,17 +3,17 @@ die() {
 }
 
 echo "Creating loopback block-device and configuring VM"
-sudo truncate -s 256G /kube-lvm || die
-sudo losetup /dev/loop0 /kube-lvm || die
+truncate -s 256G /kube-lvm || die
+losetup /dev/loop0 /kube-lvm || die
 
 echo "Disabling iptables"
 if systemctl is-enabled --quiet iptables; then
-  sudo systemctl disable --now iptables || die
-  sudo systemctl disable --now ip6tables || die
+  systemctl disable --now iptables || die
+  systemctl disable --now ip6tables || die
 fi
-sudo iptables -F || die
-sudo iptables -X || die
+iptables -F || die
+iptables -X || die
 
-sudo yum install -y epel-release
-sudo yum install -y python2-pip python2-devel python36 python36-devel
-sudo pip2 install tox
+yum install -y epel-release
+yum install -y python2-pip python2-devel python36 python36-devel
+pip2 install tox
