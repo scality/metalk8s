e88788625df3b6f8317b0c3d48aa7d68a9628f8b
Nicolas Trangez <ikke@nicolast.be>
2019-01-30 17:09:15 +0100

    salt: PoC Salt 'roster' module backed by K8s API

---
+++ salt/_roster/kubernetes_nodes.py
@@ -0,0 +1,44 @@
+import logging
+log = logging.getLogger(__name__)
+
+try:
+    import kubernetes.client
+    import kubernetes.config
+    HAS_DEPS = True
+except ImportError:
+    log.exception('Unable to load dependencies')
+    HAS_DEPS = False
+
+__virtualname__ = 'kubernetes'
+
+def __virtual__():
+    return HAS_DEPS
+
+def targets(tgt, tgt_type='glob', **kwargs):
+    client = None
+    try:
+        client = kubernetes.config.new_client_from_config(
+            config_file='/etc/kubernetes/admin.conf',
+        )
+    except:
+        log.exception('Failed to load kubeconfig')
+        raise
+
+    v1 = kubernetes.client.CoreV1Api(api_client=client)
+
+    nodes = None
+    try:
+        nodes = v1.list_node()
+    except:
+        log.exception('Failed to retrieve v1/NodeList')
+        raise
+
+    # TODO Use `tgt_type`
+    return {
+        item.metadata.name: {
+            # Assume node name is resolvable
+            'host': item.metadata.annotations.get('ssh-host', item.metadata.name),
+            'port': int(item.metadata.annotations.get('ssh-port', 22)),
+            'user': item.metadata.annotations.get('ssh-user', 'root'),
+        } for item in nodes.items
+    }
