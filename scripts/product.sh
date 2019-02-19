9d22a700607506c0fc95be7777a36b5a2e85ae01
thomasdanan <thomas.danan@scality.com>
2019-02-19 16:05:22 +0100

    Change way product build timestamp is computed for MacOS support

--- scripts/product.sh
+++ scripts/product.sh
@@ -10,6 +10,6 @@ VERSION=${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${VERSION_SUFFIX}
 SHORT_VERSION=${VERSION_MAJOR}.${VERSION_MINOR}
 GIT=$(git describe --long --always --tags --dirty)
 DEVELOPMENT_RELEASE=$(test "x${VERSION_SUFFIX}" = 'x-dev' && echo 1 || echo 0)
-BUILD_TIMESTAMP=$(date --utc +%FT%TZ)
+BUILD_TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 BUILD_HOST=$(hostname)
 EOF
