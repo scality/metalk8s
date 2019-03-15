9efda72d3acf27d81db0c64fb28ca3a04ce733e2
Charles Prost <charles.prost@scality.com>
2019-03-15 11:43:09 +0100

    CI: add multiple generic nodes to the multi-VMs env
    Fixes #724
    
    By default only 2 more nodes are created.
    Can be set in the main.yaml.

--- eve/workers/openstack-multiple-nodes/terraform/nodes.tf
+++ eve/workers/openstack-multiple-nodes/terraform/nodes.tf
@@ -11,3 +11,20 @@ resource "openstack_compute_instance_v2" "bootstrap" {
 output "bootstrap_ip" {
   value = "${openstack_compute_instance_v2.bootstrap.network.0.fixed_ip_v4}"
 }
+
+variable "nodes_count" {
+  type    = "string"
+  default = "1"
+}
+
+resource "openstack_compute_instance_v2" "nodes" {
+  name        = "${local.prefix}-node-${count.index+1}"
+  image_name  = "${var.openstack_image_name}"
+  flavor_name = "${var.openstack_flavour_name}"
+  key_pair    = "${openstack_compute_keypair_v2.local_ssh_key.name}"
+  security_groups = ["${openstack_networking_secgroup_v2.nodes.name}"]
+
+  network = ["${var.openstack_network}"]
+
+  count = "${var.nodes_count}"
+}
