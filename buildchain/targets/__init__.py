a2dbe314afede6f62cabb1b9a9623fd0e9f33cd6
Sylvain Laperche <sylvain.laperche@scality.com>
2019-03-18 18:02:44 +0100

    build/target: add a thin object layer on tasks
    Using task as dict is convenient and easy, but for complex task it's
    likely that you'll want to combine some fields or do some computation
    (e.g. infer the path where to store a package from its name and version,
    …) and to do so you'll need an object.
    
    There is also some default behavior that we'll want to apply (e.g.
    allowing task to "revert" their actions, which is done by setting
    `clean` to True in most of the cases) and a class is also convenient
    here.
    
    That's enough reason to create this `Target` abstract classes.
    
    Another thing to consider is that while simple target will probably be
    produced by a single task, more complex pone (such as creating a
    package) will be more involved and will likely require several tasks to
    be accomplished: enter `AtomicTarget` and `CompositeTarget`.
    
    Refs: #708
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

---
+++ buildchain/targets/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+
+"""Module gathering custom tasks producing generic and reusable targets."""
+
+
+from buildchain.targets.base import Target, AtomicTarget, CompositeTarget
---
+++ buildchain/targets/base.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+
+
+"""Base classes definition for classes that produces tasks."""
+
+
+import abc
+import operator
+from pathlib import Path
+from typing import List, Optional, Sequence
+
+from buildchain import types
+
+
+class Target:
+    """Base class for target that generate tasks."""
+
+    def __init__(
+        self,
+        targets: Optional[Sequence[Path]]=None,
+        file_dep: Optional[Sequence[Path]]=None,
+        task_dep: Optional[Sequence[str]]=None,
+        basename: Optional[str]=None,
+        task_name: Optional[str]=None,
+    ):
+        """Initialize the input/output of the target.
+
+        Arguments:
+            targets:  paths to the output files
+            file_dep: paths to the input files, if any
+            task_dep: names of the prerequisites tasks, if any
+            basename: tasks basename
+            name:     name of the sub-task
+        """
+        self._actions  = []  # type: List[types.Action]
+        self._targets  = targets or []
+        self._file_dep = file_dep or []
+        self._task_dep = task_dep or []
+        self._basename = basename
+        self._task_name = task_name
+
+    actions   = property(operator.attrgetter('_actions'))
+    targets   = property(operator.attrgetter('_targets'))
+    file_dep  = property(operator.attrgetter('_file_dep'))
+    task_dep  = property(operator.attrgetter('_task_dep'))
+    task_name = property(operator.attrgetter('_task_name'))
+
+    @property
+    def basename(self) -> Optional[str]:
+        """Task basename."""
+        return self._basename
+
+    @property
+    def basic_task(self) -> dict:
+        """Minimal default task that can be build upon."""
+        task = {
+            'actions': self.actions.copy(),
+            'targets': self.targets.copy(),
+            'file_dep': self.file_dep.copy(),
+            'task_dep': self.task_dep.copy(),
+            'clean': True
+        }
+        if self.basename:
+            task['basename'] = self.basename
+        if self.task_name:
+            task['name'] = self.task_name
+        return task
+
+
+
+# This one is a mix-in, hence the limited interface.
+# pylint: disable=too-few-public-methods
+class AtomicTarget(abc.ABC):
+    """Target that can be built with a single task."""
+
+    @property
+    @abc.abstractmethod
+    def task(self) -> dict:
+        """Task producing the target."""
+
+
+# This one is a mix-in, hence the limited interface.
+# pylint: disable=too-few-public-methods
+class CompositeTarget(abc.ABC):
+    """Target whose build requires multiple tasks."""
+
+    @property
+    @abc.abstractmethod
+    def execution_plan(self) -> List[dict]:
+        """List of tasks producing the target."""
