5a0b67fd0a31c25589285d201319e72ce17bea1e
Sylvain Laperche <sylvain.laperche@scality.com>
2019-03-19 04:39:01 +0100

    build/iso: add a task to generate product.txt
    Instead of relying on an external shell script (such
    `scripts/product.sh`) we generate most of the file's content from Python
    itself (except for the git info), which gives us some advantages (less
    fork/more portability).
    
    We also group together the name/paths of external commands we're using:
    - that way it's easy to see our depedencies
    - if someone has an exotic install, they can tweak these variable as
      they need in order to make it work
    
    Refs: #708
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- buildchain/iso.py
+++ buildchain/iso.py
@@ -27,6 +27,12 @@ Overview:
 """
 
 
+import datetime as dt
+import socket
+import subprocess
+from typing import Sequence
+
+from buildchain import config
 from buildchain import constants
 from buildchain import targets as helper
 from buildchain import utils
@@ -62,6 +68,7 @@ def task_populate_iso() -> dict:
         'task_dep': [
             '_iso_mkdir_root',
             '_iso_render_bootstrap',
+            '_iso_generate_product_txt',
         ],
     }
 
@@ -75,6 +82,51 @@ def task__iso_render_bootstrap() -> dict:
         file_dep=[constants.VERSION_FILE],
         task_dep=['_iso_mkdir_root'],
     ).task
+
+
+def task__iso_generate_product_txt() -> dict:
+    """Generate the product.txt file."""
+    def action(targets: Sequence[str]) -> None:
+        datefmt = "%Y-%m-%dT%H:%M:%SZ"
+        dev_release = '1' if constants.VERSION_SUFFIX == '-dev' else '0'
+        info = (
+            ('NAME', config.PROJECT_NAME),
+            ('VERSION', constants.VERSION),
+            ('SHORT_VERSION', constants.SHORT_VERSION),
+            ('GIT', git_revision()),
+            ('DEVELOPMENT_RELEASE', dev_release),
+            ('BUILD_TIMESTAMP', dt.datetime.utcnow().strftime(datefmt)),
+            ('BUILD_HOST', socket.gethostname()),
+        )
+        with open(targets[0], 'w', encoding='utf-8') as fp:
+            data = '\n'.join('{}={}'.format(key, value) for key, value in info)
+            fp.write(data)
+            fp.write('\n')
+
+    return {
+        'title': lambda task: utils.title_with_target1('GENERATE', task),
+        'actions': [action],
+        'targets': [constants.ISO_ROOT/'product.txt'],
+        'file_dep': [constants.VERSION_FILE],
+        'task_dep': ['_iso_mkdir_root'],
+        'uptodate': [False],  # False because we include the build timestamp.
+        'clean': True,
+    }
+
+
+def git_revision() -> str:
+    """Return the current git revision.
+
+    Return an empty string if we can't get the information.
+    """
+    git_cmd = [
+        config.GIT, 'describe', '--long', '--always', '--tags', '--dirty'
+    ]
+    try:
+        stdout : bytes = subprocess.check_output(git_cmd)
+        return stdout.decode('utf-8').rstrip()
+    except subprocess.CalledProcessError:
+        return ''
 
 
 __all__ = utils.export_only_tasks(__name__)
