baf7101987474ef9e91a0b330b32c0de47e162b1
Guillaume Demonet <guillaume.demonet@scality.com>
2019-03-25 14:20:43 +0100

    Expose kube-apiserver to the world
    In order to reach the API server from our bastion VM when running tests,
    we need its port (6443) to be open.
    
    Issue: GH-781

--- eve/workers/openstack-multiple-nodes/terraform/network.tf
+++ eve/workers/openstack-multiple-nodes/terraform/network.tf
@@ -21,6 +21,16 @@ resource "openstack_networking_secgroup_rule_v2" "nodes_ssh" {
   security_group_id = "${openstack_networking_secgroup_v2.nodes.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "apiserver" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.nodes.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "nodes_icmp" {
   direction         = "ingress"
   ethertype         = "IPv4"
