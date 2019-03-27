bb98a83048d91e03f8c46a6b2f56c0ff5c17ae75
Sylvain Laperche <sylvain.laperche@scality.com>
2019-03-27 16:52:40 +0100

    salt: populate the mine with etcd endpoints
    Add a function that push endpoints if an etcd is running on the current
    node.
    
    By using this function in the mine, we have an automatically up-to-date
    list of etcd endpoints, which is useful to extend the etcd cluster
    (since we need to provide a value for `initial-cluster`.
    
    Refs: #738
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- salt/_modules/metalk8s.py
+++ salt/_modules/metalk8s.py
@@ -39,3 +39,17 @@ def get_workload_plane_ips():
         return
 
     return __salt__['network.ip_addrs'](cidr=cidr)
+
+
+def get_etcd_endpoint():
+    '''Get endpoint only if it's an etcd node.'''
+    DEFAULT_ETCD_PORT = 2380
+    cidr = __pillar__.get('networks', {}).get('control_plane')
+    if not cidr:
+        return
+    if __salt__['cri.component_is_running'](name='etcd'):
+        ips = __salt__['network.ip_addrs'](cidr=cidr)
+        hostname = __salt__['network.get_hostname']()
+        return '{host}=https://{ip}:{port}'.format(
+            host=hostname, ip=ips[0], port=DEFAULT_ETCD_PORT
+        )
