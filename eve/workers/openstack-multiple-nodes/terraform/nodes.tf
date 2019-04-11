9b9760f94bcaecb497b6f925b7b441e0ade87999
Guillaume Demonet <guillaume.demonet@scality.com>
2019-04-11 14:28:15 +0200

    Add bastion VM as part of multi-nodes deployment
    We previously considered the OpenStack worker from Eve as the bastion
    from which to orchestrate multi-nodes deployments in the CI. However,
    since we cannot attach this worker to the private networks it deployed,
    we introduce another VM, which we call "bastion", that will assume (in
    the long-term) the responsibility of installing and testing the product.
    
    Issue: GH-889

--- eve/workers/openstack-multiple-nodes/terraform/nodes.tf
+++ eve/workers/openstack-multiple-nodes/terraform/nodes.tf
@@ -1,3 +1,41 @@
+resource "openstack_compute_instance_v2" "bastion" {
+  name        = "${local.prefix}-bastion"
+  image_name  = "${var.openstack_image_name}"
+  flavor_name = "${var.openstack_flavour_name}"
+  key_pair    = "${openstack_compute_keypair_v2.local_ssh_key.name}"
+
+  security_groups = [
+    "${openstack_networking_secgroup_v2.nodes.name}",
+    "${openstack_networking_secgroup_v2.nodes_internal.name}"
+  ]
+
+  network = [
+    "${var.openstack_network}",
+    "${local.control_plane_network}",
+    "${local.workload_plane_network}",
+  ]
+
+  # We need the subnets to be created before attempting to reach the DHCP server
+  depends_on = [
+    "openstack_networking_subnet_v2.control_plane_subnet",
+    "openstack_networking_subnet_v2.workload_plane_subnet",
+  ]
+
+  connection {
+    user        = "centos"
+    private_key = "${file("~/.ssh/terraform")}"
+  }
+
+  # Obtain IP addresses for both private networks
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chattr +i /etc/resolv.conf",
+      "sudo dhclient -r eth1 eth2",  # Release first
+      "sudo dhclient eth1 eth2",  # Then request new IPs
+    ]
+  }
+}
+
 resource "openstack_compute_instance_v2" "bootstrap" {
   name        = "${local.prefix}-bootstrap"
   image_name  = "${var.openstack_image_name}"
@@ -15,12 +53,10 @@ resource "openstack_compute_instance_v2" "bootstrap" {
     "${local.workload_plane_network}",
   ]
 
+  # We need the subnets before attempting to reach their DHCP servers
   depends_on = [
-    # We need the subnets before attempting to reach their DHCP servers
     "openstack_networking_subnet_v2.control_plane_subnet",
     "openstack_networking_subnet_v2.workload_plane_subnet",
-    # We need the Bastion's public key
-    # "openstack_compute_instance_v2.bastion",
   ]
 
   connection {
@@ -84,6 +120,7 @@ resource "openstack_compute_instance_v2" "nodes" {
     }
 
 locals {
+  bastion_ip   = "${openstack_compute_instance_v2.bastion.network.0.fixed_ip_v4}"
   bootstrap_ip = "${openstack_compute_instance_v2.bootstrap.network.0.fixed_ip_v4}"
 
   # FIXME: this syntax does not work (but will in v0.12)
@@ -96,6 +133,7 @@ locals {
 
 output "ips" {
   value = {
+    bastion   = "${local.bastion_ip}"
     bootstrap = "${local.bootstrap_ip}"
   }
 }
