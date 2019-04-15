feff962819cd5c4cc992176bc56f5ecb7841d623
Charles NGUYEN <charles.nguyen@scality.com>
2019-04-15 10:54:27 -0700

    Add unit tests

--- ui/src/ducks/app/nodes.test.js
+++ ui/src/ducks/app/nodes.test.js
@@ -3,7 +3,7 @@ import { fetchNodes, createNode, SET_NODES, CREATE_NODE_FAILED } from './nodes';
 import * as Api from '../../services/api';
 import history from '../../history';
 
-it('update the nodes state when fetchNodes', () => {
+it('update the control plane nodes state when fetchNodes', () => {
   const gen = fetchNodes();
 
   expect(gen.next().value).toEqual(call(Api.getNodes));
@@ -22,10 +22,87 @@ it('update the nodes state when fetchNodes', () => {
               'metalk8s.scality.com/ssh-key-path':
                 '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
               'metalk8s.scality.com/ssh-sudo': 'true'
+            },
+            labels: {
+              'node-role.kubernetes.io/etcd': '',
+              'node-role.kubernetes.io/master': ''
             }
           },
-          label: {
-            'node-role.kubernetes.io/bootstrap': ''
+          status: {
+            capacity: {
+              cpu: '2',
+              memory: '1000ki'
+            },
+            conditions: [
+              {
+                type: 'Ready',
+                status: 'True'
+              }
+            ]
+          },
+          spec: {
+            taints: [
+              {
+                key: 'node-role.kubernetes.io/master',
+                effect: 'NoSchedule'
+              },
+              {
+                key: 'node-role.kubernetes.io/etcd',
+                effect: 'NoSchedule'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  const nodes = [
+    {
+      name: 'boostrap',
+      cpu: '2',
+      memory: '1000 KiB',
+      creationDate: '2019-29-03',
+      workload_plane: false,
+      control_plane: true,
+      bootstrap: false,
+      statusType: {
+        status: 'True',
+        type: 'Ready'
+      }
+    }
+  ];
+
+  expect(gen.next(result).value).toEqual(
+    put({ type: SET_NODES, payload: nodes })
+  );
+});
+
+it('update the bootstrap nodes state when fetchNodes', () => {
+  const gen = fetchNodes();
+
+  expect(gen.next().value).toEqual(call(Api.getNodes));
+
+  const result = {
+    body: {
+      items: [
+        {
+          metadata: {
+            name: 'boostrap',
+            creationTimestamp: '2019-29-03',
+            annotations: {
+              'metalk8s.scality.com/ssh-user': 'vagrant',
+              'metalk8s.scality.com/ssh-port': '22',
+              'metalk8s.scality.com/ssh-host': '172.21.254.7',
+              'metalk8s.scality.com/ssh-key-path':
+                '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
+              'metalk8s.scality.com/ssh-sudo': 'true'
+            },
+            labels: {
+              'node-role.kubernetes.io/bootstrap': '',
+              'node-role.kubernetes.io/etcd': '',
+              'node-role.kubernetes.io/master': ''
+            }
           },
           status: {
             capacity: {
@@ -50,13 +127,139 @@ it('update the nodes state when fetchNodes', () => {
       cpu: '2',
       memory: '1000 KiB',
       creationDate: '2019-29-03',
-      control_plane: false,
-      bootstrap: '',
+      workload_plane: false,
+      control_plane: true,
+      bootstrap: true,
       statusType: {
         status: 'True',
         type: 'Ready'
-      },
-      workload_plane: true
+      }
+    }
+  ];
+
+  expect(gen.next(result).value).toEqual(
+    put({ type: SET_NODES, payload: nodes })
+  );
+});
+
+it('update the workload plane nodes state when fetchNodes', () => {
+  const gen = fetchNodes();
+
+  expect(gen.next().value).toEqual(call(Api.getNodes));
+
+  const result = {
+    body: {
+      items: [
+        {
+          metadata: {
+            name: 'boostrap',
+            creationTimestamp: '2019-29-03',
+            annotations: {
+              'metalk8s.scality.com/ssh-user': 'vagrant',
+              'metalk8s.scality.com/ssh-port': '22',
+              'metalk8s.scality.com/ssh-host': '172.21.254.7',
+              'metalk8s.scality.com/ssh-key-path':
+                '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
+              'metalk8s.scality.com/ssh-sudo': 'true'
+            },
+            labels: {
+              'node-role.kubernetes.io/node': ''
+            }
+          },
+          status: {
+            capacity: {
+              cpu: '2',
+              memory: '1000ki'
+            },
+            conditions: [
+              {
+                type: 'Ready',
+                status: 'True'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  const nodes = [
+    {
+      name: 'boostrap',
+      cpu: '2',
+      memory: '1000 KiB',
+      creationDate: '2019-29-03',
+      workload_plane: true,
+      control_plane: false,
+      bootstrap: false,
+      statusType: {
+        status: 'True',
+        type: 'Ready'
+      }
+    }
+  ];
+
+  expect(gen.next(result).value).toEqual(
+    put({ type: SET_NODES, payload: nodes })
+  );
+});
+
+it('update the control plane/workload plane nodes state when fetchNodes', () => {
+  const gen = fetchNodes();
+
+  expect(gen.next().value).toEqual(call(Api.getNodes));
+
+  const result = {
+    body: {
+      items: [
+        {
+          metadata: {
+            name: 'boostrap',
+            creationTimestamp: '2019-29-03',
+            annotations: {
+              'metalk8s.scality.com/ssh-user': 'vagrant',
+              'metalk8s.scality.com/ssh-port': '22',
+              'metalk8s.scality.com/ssh-host': '172.21.254.7',
+              'metalk8s.scality.com/ssh-key-path':
+                '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
+              'metalk8s.scality.com/ssh-sudo': 'true'
+            },
+            labels: {
+              'node-role.kubernetes.io/node': '',
+              'node-role.kubernetes.io/etcd': '',
+              'node-role.kubernetes.io/master': ''
+            }
+          },
+          status: {
+            capacity: {
+              cpu: '2',
+              memory: '1000ki'
+            },
+            conditions: [
+              {
+                type: 'Ready',
+                status: 'True'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  };
+
+  const nodes = [
+    {
+      name: 'boostrap',
+      cpu: '2',
+      memory: '1000 KiB',
+      creationDate: '2019-29-03',
+      workload_plane: true,
+      control_plane: true,
+      bootstrap: false,
+      statusType: {
+        status: 'True',
+        type: 'Ready'
+      }
     }
   ];
 
