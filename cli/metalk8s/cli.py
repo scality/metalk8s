afcc5ecc5dca23e4a9eecaef0c0eac05d425e7a7
Nicolas Trangez <ikke@nicolast.be>
2019-04-23 23:25:40 +0200

    Stash

--- cli/metalk8s/cli.py
+++ cli/metalk8s/cli.py
@@ -10,21 +10,21 @@ def main():
     client = kubernetes.config.new_client_from_config()
     corev1 = kubernetes.client.CoreV1Api(api_client=client)
 
-    if False:
+    if True:
         corev1.create_node(
             body=kubernetes.client.V1Node(
                 metadata={
                     'name': 'node1',
                     'labels': {
-                        'metalk8s.scality.com/version': '2.0',
+                        'metalk8s.scality.com/version': '2.1',
                         'node-role.kubernetes.io/node': '',
                         'node-role.kubernetes.io/infra': '',
                     },
                     'annotations': {
                         'metalk8s.scality.com/ssh-user': 'vagrant',
-                        'metalk8s.scality.com/ssh-host': '172.21.254.11',
+                        'metalk8s.scality.com/ssh-host': '172.21.254.13',
                         'metalk8s.scality.com/ssh-key-path':
-                        '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
+                            '/etc/metalk8s/pki/preshared_key_for_k8s_nodes',
                         'metalk8s.scality.com/ssh-sudo': 'true',
                     },
                 },
@@ -41,24 +41,25 @@ def main():
             ),
         )
 
-    salt = pepper.Pepper('http://172.21.254.9:4507', debug_http=True)
-    result = salt.login(
-        username='admin',
-        token='YWRtaW46YWRtaW4=',
-        token_type='Basic',
-        eauth='kubernetes_rbac',
-    )
-    print(result)
+    if True:
+        salt = pepper.Pepper('http://172.21.254.9:4507', debug_http=True)
+        result = salt.login(
+            username='admin',
+            token='YWRtaW46YWRtaW4=',
+            token_type='Basic',
+            eauth='kubernetes_rbac',
+        )
+        print(result)
 
-    pillar = {
-        'bootstrap_id': 'bootstrap',
-        'node_name': 'node1',
-    }
+        pillar = {
+            'bootstrap_id': 'bootstrap',
+            'node_name': 'node1',
+        }
 
-    result = salt.runner(
-        fun='state.orchestrate',
-        arg=('metalk8s.orchestrate.deploy_new_node',),
-        saltenv='metalk8s-2.0',
-        pillar=pillar,
-    )
-    print(result)
+        result = salt.runner(
+            fun='state.orchestrate',
+            arg=('metalk8s.orchestrate.deploy_new_node',),
+            saltenv='metalk8s-2.1',
+            pillar=pillar,
+        )
+        print(result)
