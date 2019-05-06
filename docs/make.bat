f9751f1826ad184696c99b40a9635a020340d98a
Guillaume Demonet <guillaume.demonet@scality.com>
2019-05-06 14:12:50 +0200

    Add a 'livehtml' Make target
    Allow users to watch the docs/ tree and build and serve HTML files from
    the sources.
    
    Issue: GH-1073

--- docs/make.bat
+++ docs/make.bat
@@ -9,8 +9,12 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
+set SPHINXPROJ=MetalK8s
+set SPHINXAUTOBUILD=sphinx-autobuild
+set SPHINXAUTOBUILDOPTS=--watch %SOURCEDIR%\.. --ignore "*~" --ignore "*.swp" --re-ignore "4913$" --re-ignore "\.git/"
 
 if "%1" == "" goto help
+if "%1" == "livehtml" goto livehtml
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -26,6 +30,10 @@ if errorlevel 9009 (
 )
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+goto end
+
+:livehtml
+%SPHINXAUTOBUILD% %SPHINXAUTOBUILDOPTS% %SPHINXOPTS% %SOURCEDIR% %BUILDDIR%
 goto end
 
 :help
