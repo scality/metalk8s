d350cddcefea61329ed3a31f12a482ad4b09f796
Guillaume Demonet <guillaume.demonet@scality.com>
2019-05-18 18:11:26 +0200

    Use TF 0.12 template syntax for SSH config files
    We were missing nodes connection info from the SSH config before, due to
    limitations of the previous templating language.
    We now have the `templatefile` function, and a `%{for ...}` loop syntax
    in the template.
    
    Issue: GH-1038

--- eve/workers/openstack-multiple-nodes/terraform/ssh.tf
+++ eve/workers/openstack-multiple-nodes/terraform/ssh.tf
@@ -8,36 +8,6 @@ resource "openstack_compute_keypair_v2" "local_ssh_key" {
   public_key = file(var.ssh_key_path)
   }
 
-
-data "template_file" "local_ssh_config" {
-  template = "${file("${path.module}/templates/ssh_config.tpl")}"
-  vars = {
-    identity_file = "~/.ssh/terraform"
-    bastion_ip = "${local.bastion_ip}"
-    bootstrap_ip = "${local.bootstrap_ip}"
-
-    # TODO: use Terraform v0.12 syntax once released
-    # nodes = [
-    #   for node in openstack_compute_instance_v2.nodes:
-    #   {
-    #     name = "node-${count.index}"  # May not work
-    #     ip   = node.network[0].fixed_ip_v4
-    #   }
-    # ]
-}
-}
-
-data "template_file" "bastion_ssh_config" {
-  template = "${file("${path.module}/templates/ssh_config.tpl")}"
-  vars = {
-    identity_file = "/home/centos/.ssh/bastion"
-    bastion_ip = "${local.bastion_ip}"
-    bootstrap_ip = "${local.bootstrap_ip}"
-
-    # nodes = []  # TODO: see above
-  }
-}
-
 resource "null_resource" "ssh_config" {
   triggers = {
     cluster_instance_ids = join(",", local.all_instances)
@@ -45,7 +15,15 @@ resource "null_resource" "ssh_config" {
 
   # Generate SSH config file for local usage
   provisioner "local-exec" {
-    command = "echo '${data.template_file.local_ssh_config.rendered}' > ssh_config"
+    command = "echo '${templatefile(
+      "${path.module}/templates/ssh_config.tpl",
+      {
+        identity_file = "~/.ssh/terraform"
+        bastion_ip    = local.bastion_ip
+        bootstrap_ip  = local.bootstrap_ip
+        nodes         = local.nodes
+      }
+    )}' > ssh_config"
   }
 
   # Generate SSH config file for the Bastion
@@ -56,7 +34,15 @@ resource "null_resource" "ssh_config" {
       private_key = file("~/.ssh/terraform")
     }
 
-    content = "${data.template_file.bastion_ssh_config.rendered}"
     destination = "/home/centos/ssh_config"
+    content = templatefile(
+      "${path.module}/templates/ssh_config.tpl",
+      {
+        identity_file = "/home/centos/.ssh/bastion"
+        bastion_ip    = local.bastion_ip
+        bootstrap_ip  = local.bootstrap_ip
+        nodes         = local.nodes
+      }
+    )
   }
 }
