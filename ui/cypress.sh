a326e2ca7b4cbc3e1ceeadd51c855ae9716e2b06
Alexandre Allard <alexandre.allard@scality.com>
2019-05-20 11:02:46 +0200

    Freeze cypress version to 3.2.0
    Issue: GH-1129

--- ui/cypress.sh
+++ ui/cypress.sh
@@ -4,6 +4,6 @@
 WORKLOAD_IP=$(ip a show eth0 | grep -Po "inet \K[\d.]+")
 UI_NODEPORT=$(sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf get svc metalk8s-ui -n kube-system -o jsonpath='{.spec.ports[0].nodePort}')
 
-npm install --no-save --quiet --no-package-lock cypress
+npm install --no-save --quiet --no-package-lock cypress@3.2.0
 
 ./node_modules/cypress/bin/cypress run -s cypress/integration/login.js -e target_url="http://${WORKLOAD_IP}:${UI_NODEPORT}"
