fe4ced4c966bfeb2edce5d53437b2fb044547ca2
Guillaume Demonet <guillaume.demonet@scality.com>
2019-05-23 14:01:27 +0200

    Return "_errors" in metalk8s:private ext_pillar
    Refs: #1018

--- salt/_pillar/metalk8s_private.py
+++ salt/_pillar/metalk8s_private.py
@@ -21,10 +21,9 @@ def _read_sa_private_key():
     #      also the 'ca' minion, the owner of this key
     # Suggestion: read through https://github.com/saltstack/salt/issues/45882
     if not os.path.isfile(SA_PRIVATE_KEY_PATH):
-        log.warn("Missing SA key to share with master node.")
-        return {}
+        return {"_errors": ["Missing SA key to share with master node."]}
 
-    with open(SA_PRIVATE_KEY_PATH, "r") as key_file:
+    with open(SA_PRIVATE_KEY_PATH, 'r') as key_file:
         key_data = key_file.read()
 
     return {"sa_private_key": key_data}
@@ -38,12 +37,21 @@ def ext_pillar(minion_id, pillar):
     if minion_id not in nodes_info:
         log.debug(
             "No information about minion '%s' in K8s API, skipping.", minion_id
-            )
+        )
         return {}
 
     node_info = nodes_info[minion_id]
 
     if "master" in node_info["roles"]:
-        private_data.update(_read_sa_private_key())
+        _update_with_errors(private_data, _read_sa_private_key())
 
     return {"metalk8s": {"private": private_data}}
+
+
+# Helpers
+
+def _update_with_errors(source, patch):
+    if "_errors" in patch:
+        source.setdefault("_errors", []).extend(patch.pop("_errors"))
+
+    source.update(patch)
