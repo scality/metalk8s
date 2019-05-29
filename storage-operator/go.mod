573cf0dbba1c9501bcde1146a5c4011d947d4421
Nicolas Trangez <ikke@nicolast.be>
2019-05-29 14:43:31 +0200

    storage-operator: generate `Volume` API
    ```
    $ operator-sdk add api --api-version storage.metalk8s.scality.com/v1alpha1 --kind Volume
    $ go mod vendor
    ```

--- storage-operator/go.mod
+++ storage-operator/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/emicklei/go-restful v2.8.1+incompatible // indirect
 	github.com/go-logr/logr v0.1.0 // indirect
 	github.com/go-logr/zapr v0.1.0 // indirect
-	github.com/go-openapi/spec v0.18.0 // indirect
+	github.com/go-openapi/spec v0.18.0
 	github.com/golang/groupcache v0.0.0-20180924190550-6f2cf27854a4 // indirect
 	github.com/golang/mock v1.2.0 // indirect
 	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
---
+++ storage-operator/pkg/apis/storage/v1alpha1/volume_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// VolumeSpec defines the desired state of Volume
+// +k8s:openapi-gen=true
+type VolumeSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// VolumeStatus defines the observed state of Volume
+// +k8s:openapi-gen=true
+type VolumeStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Volume is the Schema for the volumes API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type Volume struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VolumeSpec   `json:"spec,omitempty"`
+	Status VolumeStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VolumeList contains a list of Volume
+type VolumeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Volume `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Volume{}, &VolumeList{})
+}
