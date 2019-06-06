89df874c209b0c2e42eee267da4fafd2ef1904c2
Sylvain Laperche <sylvain.laperche@scality.com>
2019-06-06 15:52:53 +0200

    tests: move Kube API check to an upper level
    These steps will be needed by the tests under `install` as well.
    
    Refs: #982
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pathlib
 
 import kubernetes
 import pytest
+from pytest_bdd import given, then
 import yaml
 
 
@@ -92,5 +93,31 @@ def k8s_client(request, k8s_apiclient):
 
     return api_cls(api_client=k8s_apiclient)
 
+
+# }}}
+# Given {{{
+
+@given('the Kubernetes API is available')
+def check_service(host):
+    _verify_kubeapi_service(host)
+
+
+# }}}
+# Then {{{
+
+@then('the Kubernetes API is available')
+def verify_kubeapi_service(host):
+    _verify_kubeapi_service(host)
+
+
+# }}}
+# Helpers {{{
+
+def _verify_kubeapi_service(host):
+    """Verify that the kubeapi service answer"""
+    with host.sudo():
+        cmd = 'kubectl --kubeconfig=/etc/kubernetes/admin.conf cluster-info'
+        retcode = host.run(cmd).rc
+        assert retcode == 0
 
 # }}}
--- tests/post/steps/conftest.py
+++ tests/post/steps/conftest.py
@@ -1,29 +1,9 @@
-# -*- coding: utf-8 -*-
-from pytest_bdd import (
-    given,
-    then,
-    parsers,
-)
+# coding: utf-8
+from pytest_bdd import given, parsers
 
 from tests import kube_utils
 
-
-# Helpers
-def _verify_kubeapi_service(host):
-    """Verify that the kubeapi service answer"""
-    with host.sudo():
-        cmd = "kubectl --kubeconfig=/etc/kubernetes/admin.conf cluster-info"
-        retcode = host.run(cmd).rc
-        assert retcode == 0
-
-
-# Pytest-bdd steps
-
-# Given
-@given("the Kubernetes API is available")
-def check_service(host):
-    _verify_kubeapi_service(host)
-
+# Given {{{
 
 @given(parsers.parse("pods with label '{label}' are '{state}'"))
 def check_pod_state(host, label, state):
@@ -35,8 +15,4 @@ def check_pod_state(host, label, state):
         state.lower(), label
     )
 
-
-# Then
-@then("the Kubernetes API is available")
-def verify_kubeapi_service(host):
-    _verify_kubeapi_service(host)
+# }}}
