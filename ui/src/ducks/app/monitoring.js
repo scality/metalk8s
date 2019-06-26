425f7f34717a482ef30ebb3c25c2862c232660ff
Patrick Ear <patrick.ear@scality.com>
2019-06-26 18:37:41 +0200

    Add test to monitoring sagas

--- ui/src/ducks/app/monitoring.js
+++ ui/src/ducks/app/monitoring.js
@@ -5,10 +5,11 @@ const FETCH_ALERTS = 'FETCH_ALERTS';
 const SET_ALERTS = 'SET_ALERTS';
 
 const FETCH_CLUSTER_STATUS = 'FETCH_CLUSTER_STATUS';
-const SET_CLUSTER_STATUS = 'SET_CLUSTER_STATUS';
-const SET_APISERVER_STATUS = 'SET_APISERVER_STATUS';
-const SET_KUBESCHEDULER_STATUS = 'SET_KUBESCHEDULER_STATUS';
-const SET_KUBECONTROLLER_MANAGER_STATUS = 'SET_KUBECONTROLLER_MANAGER_STATUS';
+export const SET_CLUSTER_STATUS = 'SET_CLUSTER_STATUS';
+export const SET_APISERVER_STATUS = 'SET_APISERVER_STATUS';
+export const SET_KUBESCHEDULER_STATUS = 'SET_KUBESCHEDULER_STATUS';
+export const SET_KUBECONTROLLER_MANAGER_STATUS =
+  'SET_KUBECONTROLLER_MANAGER_STATUS';
 
 const defaultState = {
   alertList: [],
@@ -113,7 +114,6 @@ export function* fetchClusterStatus() {
     kubeControllerManager.data.data.result[0].value;
 
   if (apiserver && apiserver.data && apiserver.data.status === 'success') {
-    const apiServerValue = apiserver.data.data.result[0].value;
     yield put(setApiServerStatusAction(apiServerValue));
   }
 
@@ -135,11 +135,11 @@ export function* fetchClusterStatus() {
 
   if (
     apiServerValue.length > 1 &&
-    parseInt(apiServerValue[1]) > 1 &&
+    parseInt(apiServerValue[1]) > 0 &&
     kubeSchedulerValue.length > 1 &&
-    parseInt(kubeSchedulerValue[1]) > 1 &&
+    parseInt(kubeSchedulerValue[1]) > 0 &&
     kubeControllerManagerValue.length > 1 &&
-    parseInt(kubeControllerManagerValue[1]) > 1
+    parseInt(kubeControllerManagerValue[1]) > 0
   ) {
     // There are a least one actif job for api-server, kube-scheduler and
     // kube-controller
