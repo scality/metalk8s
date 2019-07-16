6d52800fe366272b81110eecd1247e59f1a8e65f
SayfEddine <sayf-eddine.hammemi@scality.com>
2019-07-16 14:09:33 +0200

    Fix node manifest examples task in buildchain

--- buildchain/buildchain/iso.py
+++ buildchain/buildchain/iso.py
@@ -95,23 +95,23 @@ def task__iso_mkdir_examples() -> types.TaskDict:
 
 def task__iso_add_node_manifest() -> types.TaskDict:
     """Copy the node announcement manifest to examples."""
+    # generic node manifest
+    src_generic = constants.ROOT/'examples'/'new-node.yaml'
     dest_generic = constants.ISO_ROOT/'examples'/'new-node.yaml'
+    new_node_generic = [src_generic, dest_generic]
+    # vagrant node manifest
+    src_vagrant = constants.ROOT/'examples'/'new-node_vagrant.yaml'
     dest_vagrant = constants.ISO_ROOT/'examples'/'new-node_vagrant.yaml'
-    new_node_generic = [
-        constants.ROOT/'examples'/'new-node.yaml', dest_generic
-    ]
-    new_node_vagrant = [
-        constants.ROOT/'examples'/'new-node_vagrant.yaml', dest_vagrant
-    ]
+    new_node_vagrant = [src_vagrant, dest_vagrant]
     return {
-         'title': lambda task: utils.title_with_target1('GENERATE', task),
+         'title': lambda task: utils.title_with_target1('COPY', task),
          'actions': [
              (coreutils.cp_file, new_node_generic),
              (coreutils.cp_file, new_node_vagrant)
          ],
          'targets': [dest_generic, dest_vagrant],
          'task_dep': ['_iso_mkdir_examples'],
-         'uptodate': [True],
+         'file_dep': [src_generic, src_vagrant],
          'clean': True,
      }
 
