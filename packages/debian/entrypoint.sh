c6920a948a3ff8354d4197cacf220372f0743d0d
Yohann Cointe <yohann.cointe@scality.com>
2019-08-30 09:13:01 +0200

    build/package: add an entrypoint for the Debian builder
    This commit add a Shell script that will be used by the Debian builder
    container to build DEB packages.
    It supports:
    - the creation of a DEB package from an RPM one, using alien
    - the creation of a DEB package from the sources, using debuild
    
    Note that we specify the version in the changelog instead of inside the
    control file because counterintuitively `dpkg-buildpackage` infers the
    package version number from this file, and not from a "Version" field in
    control.
    
    Refs: #1472

---
+++ packages/debian/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+set -e
+set -u
+set -o pipefail
+
+
+# Build a DEB from the source files, using debuild.
+builddeb() {
+    set -x
+
+    mkdir -p /debbuild/build-pkg/src
+    cp -r /debbuild/pkg-meta/* /debbuild/build-pkg
+    cp -r /debbuild/pkg-src/* /debbuild/build-pkg/src
+
+    # Overwrite version if supported
+    sed -i 's/_VERSION_/'"${VERSION}"'/g' "/debbuild/build-pkg/debian/changelog"
+
+    pushd /debbuild/build-pkg 1>/dev/null
+    debuild -b -uc -us
+
+    cp /debbuild/*.deb /debbuild/result
+    chown -R "${TARGET_UID}:${TARGET_GID}" /debbuild/result
+}
+
+
+# Build a DEB from an RPM file, using alien.
+rpm2deb() {
+    set -x
+
+    pushd /debbuild/result 1> /dev/null
+    alien --bump=0 /rpmbuild/source.rpm
+
+    chown "${TARGET_UID}:${TARGET_GID}" * 
+}
+
+case ${1:-''} in
+    builddeb)
+        builddeb
+        ;;
+    rpm2deb)
+        rpm2deb
+        ;;
+    '')
+        exec /bin/bash
+        ;;
+    *)
+        exec "$@"
+        ;;
+esac
