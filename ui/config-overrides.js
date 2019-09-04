f75a3d3f66e533bc92b35815b4d4b475977cbc02
Patrick Ear <patrick.ear@scality.com>
2019-09-04 14:40:43 +0200

    Improve readability of the code

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -2,12 +2,12 @@ const {
   useBabelRc,
   override,
   useEslintRc,
-  addWebpackPlugin
+  addWebpackPlugin,
 } = require('customize-cra');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = override(
   useBabelRc(),
   useEslintRc(),
-  addWebpackPlugin(new CompressionPlugin())
+  addWebpackPlugin(new CompressionPlugin()),
 );
