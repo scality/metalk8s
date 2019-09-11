9e05c3f931b8279c8a07b69caca16c7b5684daf3
Sylvain Laperche <sylvain.laperche@scality.com>
2019-09-11 14:48:34 +0200

    package/debian: compute the list of packages to download
    From each package we want to explicitly install, we walk the dependency
    tree and fetch all the required dependencies (ignoring duplicates).
    
    As for version selection, if one is specified (can be the case for some
    packages we explicitly install) we use that one, otherwise we select the
    most recent available.
    
    Refs: #1668
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- packages/debian/download_packages.py
+++ packages/debian/download_packages.py
@@ -3,12 +3,18 @@
 
 """Script that download a list of packages, and all of their dependencies."""
 
+import itertools
 import os
 import sys
 import subprocess
-from typing import Mapping, Sequence
+from typing import Dict, Mapping, Optional, Sequence
 import urllib.parse
 import urllib.request
+
+# pylint: disable=import-error
+import apt      # type: ignore
+import apt_pkg  # type: ignore
+# pylint: enable=import-error
 
 
 # Helpers {{{
@@ -26,6 +32,25 @@ def add_source_list(name: str, url: str) -> None:
     filepath = '/etc/apt/sources.list.d/{}.list'.format(name)
     with open(filepath, 'w', encoding='utf-8') as fp:
         print(url, file=fp)
+
+
+def select_package_version(
+    package: apt.package.Package, version: Optional[str]
+) -> apt.package.Version:
+    """Select one version of the given package.
+
+    If a version is specified we return this one (ValueError is raised if the
+    specified version is not available). Otherwise, we simply return the most
+    recent version available.
+    """
+    if version is None:
+        return sorted(package.versions)[-1]
+    for candidate in package.versions:
+        if version in candidate.version:
+            return candidate
+    raise ValueError('package {} not found in version {}: {}'.format(
+        package.name, version, package.versions
+    ))
 
 
 # }}}
@@ -61,9 +86,55 @@ def add_external_repositories(salt_version: str) -> None:
     subprocess.check_call(['apt', 'update'])
 
 
-def main(_: Sequence[str], env: Mapping[str, str]) -> None:
+def get_package_deps(
+    root_package_name: str, cache: apt.cache.Cache
+) -> Dict[str, apt.package.Version]:
+    """Return the given package and all of its dependencies.
+
+    This function works recursively, traversing the whole dependency tree
+    starting from `root_package`.
+
+    Arguments:
+        root_package_name: name of the package you want to explicitly install
+        cache:             packages cache
+
+    Returns:
+        A list of package versions, for the root package and its dependency
+        tree.
+    """
+    dependencies : Dict[str, apt.package.Version] = {}
+
+    name, _, version = root_package_name.partition('=')
+    root_package = cache[name]
+    stack = [root_package]
+    while stack:
+        package = stack.pop()
+        candidate = select_package_version(
+            package, version if package is root_package else None
+        )
+        # Skip already added dependency.
+        if candidate.sha256 in dependencies:
+            continue
+        dependencies[candidate.sha256] = candidate
+        # `dependencies` is a list of Or-Group, let's flatten it.
+        for dep in itertools.chain(*candidate.dependencies):
+            # Skip virtual package (there is no corresponding DEB).
+            if cache.is_virtual_package(dep.name):
+                continue
+            stack.append(cache[dep.name])
+    return dependencies
+
+
+def main(packages: Sequence[str], env: Mapping[str, str]) -> None:
     """Download the packages specified on the command-line."""
     add_external_repositories(env['SALT_VERSION'])
+    apt_pkg.init()
+    cache = apt.cache.Cache()
+    to_download = {}
+    for package in packages:
+        deps = get_package_deps(package, cache)
+        to_download.update(deps)
+
 
 
 if __name__ == '__main__':
