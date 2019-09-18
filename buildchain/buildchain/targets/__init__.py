e82ae2a29ea14d6e4185d034fab105ad2666e814
Yohann Cointe <yohann.cointe@scality.com>
2019-09-18 15:06:20 +0200

    build/packages: Add class DEBRepository
    We need the class DEBRepository to create Debian repository.
    For now the class is only use to permit the Debian packages download.
    
    Refs: #1701

--- buildchain/buildchain/targets/__init__.py
+++ buildchain/buildchain/targets/__init__.py
@@ -12,6 +12,8 @@ from buildchain.targets.local_image import LocalImage
 from buildchain.targets.operator_image import OperatorImage
 from buildchain.targets.package import Package, RPMPackage, DEBPackage
 from buildchain.targets.remote_image import RemoteImage
-from buildchain.targets.repository import Repository, RPMRepository
+from buildchain.targets.repository import (
+    Repository, RPMRepository, DEBRepository
+)
 from buildchain.targets.serialize import SerializedData
 from buildchain.targets.template import TemplateFile
