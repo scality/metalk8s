aca5a80f9e189e11b25f70999c20acac9e910f9c
YanJin <yanjin.cheng@scality.com>
2019-09-20 15:55:18 +0200

    ui/volume: add banner when failed to create volume
    Ref: #1705

--- ui/src/containers/VolumeInformation.js
+++ ui/src/containers/VolumeInformation.js
@@ -3,13 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { injectIntl, FormattedDate, FormattedTime } from 'react-intl';
 import styled from 'styled-components';
-import { padding } from '@scality/core-ui/dist/style/theme';
+import { padding, fontSize } from '@scality/core-ui/dist/style/theme';
 import { Breadcrumb } from '@scality/core-ui';
 import { makeGetNodeFromUrl, makeGetVolumesFromUrl } from '../services/utils';
 import {
   SPARSE_LOOP_DEVICE,
   RAW_BLOCK_DEVICE,
   STATUS_BOUND,
+  STATUS_BANNER_ERROR,
+  STATUS_FAILED,
 } from '../constants';
 import {
   fetchVolumesAction,
@@ -22,6 +24,7 @@ import {
   BreadcrumbLabel,
   StyledLink,
 } from '../components/BreadcrumbStyle';
+import Banner from '../components/Banner';
 import {
   InformationListContainer,
   InformationSpan,
@@ -38,6 +41,12 @@ const VolumeInformationContainer = styled.div`
   flex-direction: column;
   box-sizing: border-box;
   padding: ${padding.base};
+  width: 750px;
+`;
+
+const VolumeInformationTitle = styled.div`
+  font-size: ${fontSize.larger};
+  margin-left: ${padding.larger};
 `;
 
 const VolumeInformation = props => {
@@ -66,6 +75,7 @@ const VolumeInformation = props => {
       SC.metadata.name ===
       (volume && volume.spec && volume.spec.storageClassName),
   );
+
   return (
     <VolumeInformationContainer>
       <BreadcrumbContainer>
@@ -82,6 +92,23 @@ const VolumeInformation = props => {
           ]}
         />
       </BreadcrumbContainer>
+      <VolumeInformationTitle>
+        {intl.messages.detailed_information}
+      </VolumeInformationTitle>
+
+      {volume?.status?.phase === STATUS_FAILED ? (
+        <Banner
+          type={STATUS_BANNER_ERROR}
+          icon={<i className="fas fa-exclamation-triangle" />}
+          title={
+            volume?.status?.errorCode === 'CreationError'
+              ? intl.messages.failed_to_create_volume
+              : intl.messages.error
+          }
+          messages={[volume?.status?.errorMessage]}
+        />
+      ) : null}
+
       <VolumeInformationListContainer>
         <InformationSpan>
           <InformationLabel>{intl.messages.name}</InformationLabel>
@@ -99,7 +126,8 @@ const VolumeInformation = props => {
         <InformationSpan>
           <InformationLabel>{intl.messages.size}</InformationLabel>
           <InformationValue>
-            {pV && pV.spec && pV.spec.capacity && pV.spec.capacity.storage}
+            {(pV && pV.spec && pV.spec.capacity && pV.spec.capacity.storage) ||
+              intl.messages.unknown}
           </InformationValue>
         </InformationSpan>
         <InformationSpan>
@@ -127,13 +155,14 @@ const VolumeInformation = props => {
         <InformationSpan>
           <InformationLabel>{intl.messages.path}</InformationLabel>
           <InformationValue>
-            {pV && pV.spec && pV.spec.local && pV.spec.local.path}
+            {volume?.spec?.rawBlockDevice?.devicePath ||
+              intl.messages.not_applicable}
           </InformationValue>
         </InformationSpan>
         <InformationSpan>
           <InformationLabel>{intl.messages.access_mode}</InformationLabel>
           <InformationValue>
-            {pV && pV.spec && pV.spec.accessModes}
+            {(pV && pV.spec && pV.spec.accessModes) || intl.messages.unknown}
           </InformationValue>
         </InformationSpan>
         <InformationSpan>
