8c8070376a1e6de47b924cdf95bba3d6e94a0170
Sylvain Laperche <sylvain.laperche@scality.com>
2019-10-09 09:35:00 +0200

    build/doc: improve output in case of error
    Seems like `tox` (or the underlying processes) write its error onto
    `stdout` instead of `stderr`.
    You can reproduce by testing:
    - `tox -e docs -- cyka > /dev/null`  => stderr: no error
    - `tox -e docs -- cyka 2> /dev/null` => stdout: error
    
    And because `ContainerError` only capture `stderr`, we end up with
    useless output like:
    
        Command '['/entrypoint.sh', 'latexpdf']' in image 'metalk8s-doc-builder:latest' returned non-zero exit status 1: b'': b''
    
    So for this command, let's redirect `stdout` into `stderr`…
    
    Refs: #1872
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 set -u
 set -o pipefail
 
-tox --workdir /tmp/tox -e docs -- "$@"
+tox --workdir /tmp/tox -e docs -- "$@" 1>&2
 chown -R "${TARGET_UID}:${TARGET_GID}" /usr/src/metalk8s/docs/_build
