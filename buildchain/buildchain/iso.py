2abdea8c5a8b7a3bcd4faefd174f1a687e59cfb9
Guillaume Demonet <guillaume.demonet@scality.com>
2019-10-14 00:18:12 +0200

    WIP buildchain - add metalk8s_cli

--- buildchain/buildchain/iso.py
+++ buildchain/buildchain/iso.py
@@ -107,7 +107,52 @@ FILE_TREES : Tuple[helper.FileTree, ...] = (
         destination_directory=constants.ISO_ROOT,
         source_prefix=Path('scripts'),
         task_dep=['_iso_mkdir_root']
-    )
+    ),
+    helper.FileTree(
+        basename='_iso_add_tree',
+        files=(
+            Path('metalk8s'),
+
+            Path('metalk8s_cli/__init__.py'),
+            Path('metalk8s_cli/__main__.py'),
+            helper.TemplateFile(
+                task_name='metalk8s_cli/__version__.py',
+                source=constants.ROOT.joinpath(
+                    'scripts', 'metalk8s_cli', '__version__.py.in',
+                ),
+                destination=constants.ISO_ROOT.joinpath(
+                    'metalk8s_cli', '__version__.py',
+                ),
+                context={'VERSION': versions.VERSION},
+                file_dep=[versions.VERSION_FILE],
+                task_dep=['_iso_mkdir_root'],
+            ),
+            Path('metalk8s_cli/command.py'),
+            Path('metalk8s_cli/exceptions.py'),
+            Path('metalk8s_cli/parsers.py'),
+            Path('metalk8s_cli/utils.py'),
+
+            Path('metalk8s_cli/mixins/__init__.py'),
+            Path('metalk8s_cli/mixins/log.py'),
+            Path('metalk8s_cli/mixins/salt.py'),
+
+            Path('metalk8s_cli/solutions/__init__.py'),
+            Path('metalk8s_cli/solutions/command.py'),
+            Path('metalk8s_cli/solutions/config.py'),
+
+            Path('metalk8s_cli/solutions/add/__init__.py'),
+            Path('metalk8s_cli/solutions/add/command.py'),
+
+            Path('metalk8s_cli/solutions/deploy/__init__.py'),
+            Path('metalk8s_cli/solutions/deploy/command.py'),
+
+            Path('metalk8s_cli/solutions/remove/__init__.py'),
+            Path('metalk8s_cli/solutions/remove/command.py'),
+        ),
+        destination_directory=constants.ISO_ROOT,
+        source_prefix=Path('scripts'),
+        task_dep=['_iso_mkdir_root'],
+    ),
 )
 
 
