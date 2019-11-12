5e25071d11baa43b1934b49c801ec986854b685a
Alexandre Allard <alexandre.allard@scality.com>
2019-11-12 17:26:24 +0100

    scripts: use common lib in downgrade script
    Use the common.sh library and remove functions
    already moved to this file.
    It will avoid doing some changes to each and
    every scripts instead of only changing the code
    in this library

--- scripts/downgrade.sh
+++ scripts/downgrade.sh
@@ -10,6 +10,7 @@ SALTENV=""
 DESTINATION_VERSION=""
 SALT_CALL=${SALT_CALL:-salt-call}
 CRICTL=${CRICTL:-crictl}
+BASE_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 
 _usage() {
@@ -69,69 +70,8 @@ cleanup() {
 
 trap cleanup EXIT
 
-run_quiet() {
-    local name=$1
-    shift 1
-
-    echo -n "> ${name}..."
-    local start
-    start=$(date +%s)
-    set +e
-    "$@" 2>&1 | tee -ia "${LOGFILE}" > "${TMPFILES}/out"
-    local RC=$?
-    set -e
-    local end
-    end=$(date +%s)
-
-    local duration=$(( end - start ))
-
-    if [ $RC -eq 0 ]; then
-        echo " done [${duration}s]"
-    else
-        echo " fail [${duration}s]"
-        cat >/dev/stderr << EOM
-
-Failure while running step '${name}'
-
-Command: $@
-
-Output:
-
-<< BEGIN >>
-EOM
-        cat "${TMPFILES}/out" > /dev/stderr
-
-        cat >/dev/stderr << EOM
-<< END >>
-
-This script will now exit
-
-EOM
-
-        exit 1
-    fi
-}
-
-run_verbose() {
-    local name=$1
-    shift 1
-
-    echo "> ${name}..."
-    "$@"
-}
-
-run() {
-    if [ "$VERBOSE" -eq 1 ]; then
-        run_verbose "${@}"
-    else
-        run_quiet "${@}"
-    fi
-}
-
-die() {
-    echo 1>&2 "$@"
-    return 1
-}
+# shellcheck disable=SC1090
+. "$BASE_DIR"/common.sh
 
 _set_env() {
     if [ -z "$SALTENV" ]; then
@@ -139,26 +79,6 @@ _set_env() {
             string="{{ pillar.metalk8s.nodes[grains.id].version }}" \
             | cut -c 8-)"
     fi
-}
-
-get_salt_container() {
-    local -r max_retries=10
-    local salt_container='' attempts=0
-
-    while [ -z "$salt_container" ] && [ $attempts -lt $max_retries ]; do
-        salt_container="$(crictl ps -q \
-            --label io.kubernetes.pod.namespace=kube-system \
-            --label io.kubernetes.container.name=salt-master \
-            --state Running)"
-        (( attempts++ ))
-    done
-
-    if [ -z "$salt_container" ]; then
-        echo "Failed to find a running 'salt-master' container" >&2
-        exit 1
-    fi
-
-    echo "$salt_container"
 }
 
 _init () {
