a56f489abf594941a58d8b61de8f99c129f9617d
Teddy Andrieux <teddy.andrieux@scality.com>
2019-11-14 13:42:45 +0100

    salt: No longer store wrong etcd endpoints in the mine
    Since #877 we not longer use the mine to store etcd endpoints but mine
    function and mine push were still present in etcd installed state.
    
    Refs: #877
    Refs: #2043

--- salt/_modules/metalk8s.py
+++ salt/_modules/metalk8s.py
@@ -40,20 +40,6 @@ def wait_apiserver(retry=10, interval=1, **kwargs):
     return status
 
 
-def get_etcd_endpoint():
-    '''Get endpoint only if it's an etcd node.'''
-    DEFAULT_ETCD_PORT = 2380
-    cidr = __pillar__.get('networks', {}).get('control_plane')
-    if not cidr:
-        return
-    if __salt__['cri.component_is_running'](name='etcd'):
-        ips = __salt__['network.ip_addrs'](cidr=cidr)
-        hostname = __salt__['network.get_hostname']()
-        return '{host}=https://{ip}:{port}'.format(
-            host=hostname, ip=ips[0], port=DEFAULT_ETCD_PORT
-        )
-
-
 def format_san(names):
     '''Format a `subjectAlternativeName` section of a certificate.
 
