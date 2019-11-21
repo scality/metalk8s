a0200bc3e4f839b38cd29ca02c1a9ce46626bfef
Alexandre Allard <alexandre.allard@scality.com>
2019-11-21 09:58:25 +0100

    tf: create control plane network & interfaces
    Creation of the network used by the control plane,
    this allows the usage of a VIP for apiserver which
    will be used to test backup/restoration mechanism

--- eve/workers/openstack-multiple-nodes/terraform/network.tf
+++ eve/workers/openstack-multiple-nodes/terraform/network.tf
@@ -6,6 +6,17 @@ variable "openstack_network" {
   }
 }
 
+locals {
+  control_plane_network = {
+    name                  = "${local.prefix}-control-plane"
+    cidr                  = "192.168.1.0/28"
+    gateway               = "192.168.1.1"
+    vip                   = "192.168.1.2"
+    allocation_pool_start = "192.168.1.3"
+    allocation_pool_end   = "192.168.1.14"
+  }
+}
+
 data "openstack_networking_network_v2" "default_network" {
   name = var.openstack_network.name
 }
@@ -26,7 +37,6 @@ variable "openstack_link_local_ip" {
   type    = string
   default = "169.254.169.254"
 }
-
 
 # Security groups
 resource "openstack_networking_secgroup_v2" "nodes" {
@@ -93,4 +103,88 @@ resource "openstack_networking_secgroup_rule_v2" "dns_egress" {
 resource "openstack_networking_secgroup_v2" "bastion" {
   name        = "${local.prefix}-bastion"
   description = "Security group for the Bastion VM"
+}
+
+resource "openstack_networking_network_v2" "control_plane" {
+  name = local.control_plane_network.name
+}
+
+resource "openstack_networking_subnet_v2" "control_plane" {
+  name        = "${local.prefix}-control-plane"
+  network_id  = openstack_networking_network_v2.control_plane.id
+  enable_dhcp = true
+  cidr        = local.control_plane_network.cidr
+  ip_version  = 4
+  gateway_ip  = local.control_plane_network.gateway
+  allocation_pool {
+    start = local.control_plane_network.allocation_pool_start
+    end   = local.control_plane_network.allocation_pool_end
+  }
+}
+
+resource "openstack_networking_secgroup_v2" "control_plane" {
+  name                 = "${local.prefix}-control-plane"
+  description          = "Security group for MetalK8s control plane"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "control_plane_ingress" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_group_id   = openstack_networking_secgroup_v2.control_plane.id
+  security_group_id = openstack_networking_secgroup_v2.control_plane.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "control_plane_egress" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  remote_group_id   = openstack_networking_secgroup_v2.control_plane.id
+  security_group_id = openstack_networking_secgroup_v2.control_plane.id
+}
+
+resource "openstack_networking_port_v2" "control_plane_vip" {
+  name           = "${local.prefix}-control-plane-vip"
+  network_id     = openstack_networking_network_v2.control_plane.id
+
+  security_group_ids = [
+    openstack_networking_secgroup_v2.control_plane.id,
+  ]
+
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.control_plane.id
+    ip_address = local.control_plane_network.vip
+  }
+}
+
+resource "openstack_networking_port_v2" "control_plane" {
+  name           = "${local.prefix}-control-plane-${count.index}"
+  admin_state_up = "true"
+  network_id     = openstack_networking_network_v2.control_plane.id
+
+  security_group_ids = [
+    openstack_networking_secgroup_v2.control_plane.id,
+  ]
+
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.control_plane.id
+  }
+
+  allowed_address_pairs {
+    ip_address = local.control_plane_network.vip
+  }
+
+  count = var.nodes_count + 1
+}
+
+resource "openstack_networking_port_v2" "control_plane_bastion" {
+  name           = "${local.prefix}-control-plane-bastion"
+  admin_state_up = "true"
+  network_id     = openstack_networking_network_v2.control_plane.id
+
+  security_group_ids = [
+    openstack_networking_secgroup_v2.control_plane.id,
+  ]
+
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.control_plane.id
+  }
 }
