c4b2e6480be6ff8e051a1c57fc16a25e9480aa4d
Charles NGUYEN <charles.nguyen@scality.com>
2019-11-26 15:40:56 +0100

    UI: Display only username once logged in - Display only username instead of username + email - update cypress tests Ref: #2015

--- ui/cypress/integration/common/login.js
+++ ui/cypress/integration/common/login.js
@@ -15,11 +15,12 @@ Given('I log in', () => {
   //click "Login with email" button when DEX "alwaysShowLoginScreen" is true
   cy.get('.theme-form-row button').click();
 
+  const email = Cypress.env('email');
   const userName = Cypress.env('username');
   const userPassword = Cypress.env('password');
 
   //Fill Dex login form and validate it
-  cy.get('input[type=text]').type(userName);
+  cy.get('input[type=text]').type(email);
   cy.get('input[type=password]').type(userPassword);
   cy.get('button').click();
 
