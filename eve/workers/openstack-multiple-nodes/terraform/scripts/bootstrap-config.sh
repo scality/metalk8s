c34d963095a833972e52dbdd50fd77b519c8f2a6
Alexandre Allard <alexandre.allard@scality.com>
2019-12-04 11:44:24 +0100

    tf: retrieve networks dynamically in bootstrap config
    Networks for workload and control plane were hardcoded
    in the bootstrap configuration generation script,
    we now retrieve them from configured network interfaces
    on the host.
    Also enable keepalived, since we now have a VIP.

--- eve/workers/openstack-multiple-nodes/terraform/scripts/bootstrap-config.sh
+++ eve/workers/openstack-multiple-nodes/terraform/scripts/bootstrap-config.sh
@@ -9,16 +9,35 @@ mkdir -p "$(dirname $OUTPUT_FILE)"
 mkdir -p /etc/salt
 hostname > /etc/salt/minion_id
 
+KEEPALIVED_ENABLED=false
+API_SERVER_VIP=${1:-}
+
+[[ $API_SERVER_VIP ]] && KEEPALIVED_ENABLED=true
+
+WORKLOAD_PLANE_IP=$(
+  ip -4 address show eth0 | sed -rn 's/^\s*inet ([0-9.\/]+).*$/\1/p'
+)
+eval "$(ipcalc --network --prefix "$WORKLOAD_PLANE_IP")"
+WORKLOAD_PLANE_NETWORK=$NETWORK/$PREFIX
+
+CONTROL_PLANE_IP=$(
+  ip -4 address show eth1 | sed -rn 's/^\s*inet ([0-9.\/]+).*$/\1/p'
+)
+eval "$(ipcalc --network --prefix "$CONTROL_PLANE_IP")"
+CONTROL_PLANE_NETWORK=$NETWORK/$PREFIX
+
 cat > "$OUTPUT_FILE" << EOF
 apiVersion: metalk8s.scality.com/v1alpha2
 kind: BootstrapConfiguration
 networks:
-  controlPlane: 10.100.0.0/16
-  workloadPlane: 10.100.0.0/16
+  controlPlane: $CONTROL_PLANE_NETWORK
+  workloadPlane: $WORKLOAD_PLANE_NETWORK
 ca:
   minion: $(cat /etc/salt/minion_id)
 apiServer:
-  host: $(ip route get 10.100.0.0 | awk '/10.100.0.0/{ print $6 }')
+  host: ${API_SERVER_VIP:-${CONTROL_PLANE_IP%/*}}
+  keepalived:
+    enabled: $KEEPALIVED_ENABLED
 archives:
   - /var/tmp/metalk8s
 EOF
