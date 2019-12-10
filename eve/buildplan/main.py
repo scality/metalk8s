19ad8f9c4fe3532ce0002e852c50d1e5affecca9
Guillaume Demonet <guillaume.demonet@scality.com>
2019-12-10 15:52:14 +0100

    ci/buildplan: Add steps for "single-node" stage

--- eve/buildplan/main.py
+++ eve/buildplan/main.py
@@ -1,4 +1,5 @@
 """Generate the Eve YAML description of build plan for MetalK8s."""
+import pathlib
 import sys
 
 from buildplan import core
@@ -115,6 +116,7 @@ def lint():
 
 
 @dsl.WithStatus()
+@dsl.WithArtifacts(urls=["sosreport/single-node"])
 @dsl.WithSetup([dsl.SetupStep.GIT, dsl.SetupStep.CACHE, dsl.SetupStep.SSH])
 def single_node():
     return core.Stage(
@@ -124,7 +126,20 @@ def single_node():
             flavor=core.OpenStackWorker.Flavor.LARGE,
             image=core.OpenStackWorker.Image.CENTOS7,
         ),
-        steps=[],
+        steps=[
+            *get_iso_from_artifacts(),
+            *prepare_bootstrap(),
+            install_bootstrap(),
+            *run_tests(
+                {"fast": "not slow", "slow": "slow"},
+                mode="local",
+                common_filter="post and ci and not multinode",
+            ),
+            collect_sosreport(),
+            *dsl.copy_artifacts(
+                ["/var/tmp/sosreport*"], destination="sosreport/single-node",
+            ),
+        ],
     )
 
 
@@ -183,6 +198,166 @@ def lint_all():
         command="./doit.sh lint",
         use_pty=True,
         halt_on_failure=False,
+    )
+
+
+ARTIFACTS_URL = pathlib.Path("%(prop:artifacts_private_url)s")
+
+
+def get_iso_from_artifacts(destination=None, source=None):
+    base_url = ARTIFACTS_URL
+    name_suffix = ""
+    if source is not None:
+        base_url = base_url / source
+        name_suffix = " ({})".format(source)
+
+    dest_dir = pathlib.Path(".")
+    if destination is not None:
+        dest_dir = pathlib.Path(destination)
+
+    def _curl_cmd(filename):
+        return 'curl -s -XGET -o "{out_path}" "{in_url}"'.format(
+            out_path=dest_dir / filename, in_url=base_url / filename,
+        )
+
+    # Get ISO checksum
+    yield shell.Shell(
+        "Retrieve ISO image checksum" + name_suffix,
+        command=_curl_cmd("SHA256SUM"),
+        halt_on_failure=True,
+    )
+
+    # Get ISO archive, with retry
+    yield shell.Bash(
+        "Retrieve ISO image" + name_suffix,
+        command=shell._seq(
+            shell._for(
+                "{{1..{max_attempts}}}",
+                shell._seq(
+                    'echo "Attempt $i out of {max_attempts}"',
+                    "{curl_cmd} && exit",
+                    "sleep 2",
+                ),
+                var="i",
+            ),
+            'echo "Could not retrieve ISO after {max_attempts} attempts" >&2',
+            "exit 1",
+        ).format(max_attempts=20, curl_cmd=_curl_cmd("metalk8s.iso")),
+        inline=True,
+        halt_on_failure=True,
+    )
+
+    # Validate checksum
+    yield shell.Shell(
+        "Check ISO image with checksum" + name_suffix,
+        command="sha256sum -c {}".format(dest_dir / "SHA256SUM"),
+        halt_on_failure=True,
+    )
+
+
+SRV_SCAL = pathlib.Path("/srv/scality")
+DEFAULT_MOUNTPOINT = SRV_SCAL / "metalk8s-%(prop:metalk8s_version)s"
+
+
+def prepare_bootstrap(
+    iso="$(realpath metalk8s.iso)",
+    mountpoint=DEFAULT_MOUNTPOINT,
+    minion_id=None,
+):
+    # Create mountpoint
+    yield shell.Shell(
+        "Create ISO mountpoint",
+        command='mkdir -p "{}"'.format(mountpoint),
+        sudo=True,
+        halt_on_failure=True,
+    )
+
+    # Mount ISO
+    yield shell.Shell(
+        "Mount ISO image",
+        command='mount -o loop "{}" "{}"'.format(iso, mountpoint),
+        sudo=True,
+        halt_on_failure=True,
+    )
+
+    # Create BootstrapConfiguration
+    bootstrap_config_env = {
+        "ARCHIVE_PATH": iso,
+    }
+    if minion_id is not None:
+        bootstrap_config_env["MINION_ID"] = minion_id
+
+    yield shell.Bash(
+        "Create bootstrap configuration file",
+        "deploy/scripts/bootstrap-config.sh",
+        env=bootstrap_config_env,
+        wrap_env=True,
+        sudo=True,
+        inline=True,
+        halt_on_failure=True,
+    )
+
+
+def install_bootstrap(mountpoint=DEFAULT_MOUNTPOINT):
+    return shell.Bash(
+        "Start the bootstrap process",
+        mountpoint / "bootstrap.sh",
+        "--verbose",
+        sudo=True,
+        halt_on_failure=True,
+    )
+
+
+def run_tests(
+    filters,
+    mode="default",
+    common_filter=None,
+    branch="%(prop:branch)s",
+    iso_mountpoint=DEFAULT_MOUNTPOINT,
+    ssh_config=None,
+):
+    yield shell.Shell(
+        "Checkout required version before running tests",
+        'git checkout "{}" --quiet'.format(branch),
+        halt_on_failure=True,
+        hide_step_if=True,
+    )
+
+    env = {"ISO_MOUNTPOINT": iso_mountpoint}
+    if mode != "local":
+        env["SSH_CONFIG"] = ssh_config
+
+    for label, extra_filter in filters.items():
+        final_filter = shell._fmt_args(
+            common_filter, extra_filter, join_with=" and "
+        )
+
+        yield shell.Shell(
+            "Run tests - {}".format(label),
+            command=shell._fmt_args(
+                "tox",
+                "-e",
+                "tests-local" if mode == "local" else "tests",
+                '-- -m "{}"'.format(final_filter) if final_filter else None,
+        ),
+        env=env,
+            wrap_env=True,
+        halt_on_failure=True,
+    )
+
+
+def collect_sosreport(owner="eve", group="eve"):
+    return shell.Shell(
+        "Collect logs using sosreport",
+        command=shell._and(
+            shell._fmt_args(
+                "sudo sosreport --all-logs",
+                "-o metalk8s -kmetalk8s.podlogs=True",
+                "-o containerd -kcontainerd.all=True -kcontainerd.logs=True",
+                "--batch --tmp-dir /var/tmp",
+            ),
+            "sudo chown {}:{} /var/tmp/sosreport*".format(owner, group),
+        ),
     )
 
 
