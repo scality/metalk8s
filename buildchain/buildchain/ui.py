472c3f1452206b51270146ffd3188745bb854d52
Alexandre Allard <alexandre.allard@scality.com>
2019-12-19 10:04:52 +0100

    build: build metalk8s-ui from artifacts
    We now build the MetalK8s UI image from
    the artifacts generated from the doc & the ui tasks
    So, now we also ship the documentation inside the
    metalk8s-ui container image.
    The docs is accessible at the following URL:
      container_ip:container_port/docs
    
    Refs: #1595

--- buildchain/buildchain/ui.py
+++ buildchain/buildchain/ui.py
@@ -4,15 +4,16 @@
 
 Overview:
 
-┌───────┐    ┌──────────┐
-│ mkdir │───>│ build UI │
-└───────┘    └──────────┘
+┌────┐    ┌───────┐    ┌──────────┐    ┌──────────────────┐
+│ UI │───>│ mkdir │───>│ UI build │───>│ UI configuration │
+└────┘    └───────┘    └──────────┘    └──────────────────┘
 """
 
 
 from pathlib import Path
 
 from buildchain import builder
+from buildchain import config
 from buildchain import constants
 from buildchain import coreutils
 from buildchain import docker_command
@@ -28,6 +29,7 @@ def task_ui() -> types.TaskDict:
         'task_dep': [
             '_ui_mkdir_build_root',
             '_ui_build',
+            '_ui_config',
         ],
     }
 
@@ -42,7 +44,7 @@ def task__ui_mkdir_build_root() -> types.TaskDict:
 
 
 def task__ui_build() -> types.TaskDict:
-    """Build the MetalK8s UI NodeJS."""
+    """Build the MetalK8s UI NodeJS code."""
     def clean() -> None:
         coreutils.rm_rf(constants.UI_BUILD_ROOT)
 
@@ -69,6 +71,21 @@ def task__ui_build() -> types.TaskDict:
         'file_dep': list(utils.git_ls('ui')),
         'targets': [constants.UI_BUILD_ROOT/'index.html'],
         'clean': [clean],
+    }
+
+
+def task__ui_config() -> types.TaskDict:
+    """Copy MetalK8s UI configuration to the build directory."""
+    source = constants.ROOT/'images'/'metalk8s-ui'/'conf'/'nginx.conf'
+    target = config.BUILD_ROOT/'metalk8s-ui-nginx.conf'
+
+    return {
+        'title': utils.title_with_target1('COPY'),
+        'actions': [(coreutils.cp_file, (source, target))],
+        'targets': [target],
+        'task_dep': ['_build_root'],
+        'file_dep': [source],
+        'clean': True,
     }
 
 
