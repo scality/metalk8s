fffbeb9345fedbea03406ac678b2bf1b9f50eddf
Teddy Andrieux <teddy.andrieux@scality.com>
2019-12-23 11:48:23 +0100

    salt: Do etcd healthcheck by default and add pillar value to skip
    When we run `etcd` state we want to check `etcd` health by default but
    when we deploy a new `etcd` the new member is not yet registered in the
    `etcd` cluster so we need to skip the healthcheck of the `etcd` when
    deploying this new `etcd` node, so adding a pillar value to skip
    `etcd` healthcheck in the salt state

--- buildchain/buildchain/salt_tree.py
+++ buildchain/buildchain/salt_tree.py
@@ -370,7 +370,6 @@ SALT_FILES : Tuple[Union[Path, targets.AtomicTarget], ...] = (
     Path('salt/metalk8s/kubernetes/etcd/certs/peer.sls'),
     Path('salt/metalk8s/kubernetes/etcd/certs/server.sls'),
     Path('salt/metalk8s/kubernetes/etcd/files/manifest.yaml'),
-    Path('salt/metalk8s/kubernetes/etcd/healthy.sls'),
     Path('salt/metalk8s/kubernetes/etcd/init.sls'),
     Path('salt/metalk8s/kubernetes/etcd/installed.sls'),
 
