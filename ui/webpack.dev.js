83ac6ea03a70744f8df4b8e32d8f3d35b58b231a
Charles NGUYEN <charles.nguyen@scality.com>
2020-01-06 11:13:47 +0100

    solve cors problem

--- ui/webpack.dev.js
+++ ui/webpack.dev.js
@@ -7,6 +7,9 @@ config.plugins.push(new webpack.HotModuleReplacementPlugin());
 config.devServer = {
   headers: {
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'X-Requested-With, content-type, Authorization',
   },
 };
 
