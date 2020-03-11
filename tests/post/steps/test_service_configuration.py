5805ed5ba6e2da4974fdc9267b36e15604eb7b22
Claude Ebaneck <claude.ebaneck@scality.com>
2020-03-11 10:58:06 +0100

    tests: Add test to check propagation of service configurations
    We need to be able to check that newly added service configurations
    are propagated down the underlying services involved.
    
    For this to happen, we have added tests to edit and apply a service
    configuration then validate that the service runs with the newly
    added configuration automatically

---
+++ tests/post/steps/test_service_configuration.py
@@ -0,0 +1,217 @@
+from ast import literal_eval
+import yaml
+
+import pytest
+from pytest_bdd import scenario, given, then, when, parsers
+
+import kubernetes.client
+from kubernetes.client import AppsV1Api
+from kubernetes.client.rest import ApiException
+
+from tests import utils
+
+
+# Fixtures {{{
+
+
+@pytest.fixture
+def k8s_appsv1_client(k8s_apiclient):
+    return AppsV1Api(api_client=k8s_apiclient)
+
+# }}}
+
+
+# Scenarios {{{
+
+
+@scenario('../features/service_configuration.feature',
+          'Propagation of Service Configurations to underlying Services')
+def test_service_config_propagation(host):
+    pass
+
+
+# }}}
+
+
+# Given {{{
+
+
+@given(parsers.parse(
+    "we have a '{name}' CSC in namespace '{namespace}' with "
+    "'{path}' equal to '{value}'"))
+def check_csc_configuration(
+    k8s_client,
+    name,
+    namespace,
+    path,
+    value
+):
+    csc_response = read_namespace_config_map(k8s_client, name, namespace)
+
+    assert csc_response, (
+        "No ConfigMap with name {} in namespace {} found".format(
+            name, namespace
+        )
+    )
+
+    csc_obj = csc_load(csc_response, name, namespace)
+    response_value = utils.get_dict_element(csc_obj, path)
+
+    assert literal_eval(value) == response_value, (
+        "Expected value {} for key {} in ConfigMap {} found in namespace {}, "
+        "got {}".format(value, path, name, namespace, response_value)
+    )
+
+
+# }}}
+
+
+# When {{{
+
+
+@when(parsers.parse(
+    "we update '{name}' CSC in namespace '{namespace}' "
+    "'{path}' to '{value}'"))
+def update_service_configuration(
+    k8s_client,
+    name,
+    namespace,
+    path,
+    value
+):
+
+    full_csc = read_namespace_config_map(k8s_client, name, namespace)
+    csc_obj = utils.set_dict_element(
+        csc_load(full_csc, name, namespace), path, literal_eval(value)
+    )
+    patch = {
+        'data': {
+            'config.yaml': yaml.safe_dump(
+                csc_obj, default_flow_style=False
+            )
+        }
+    }
+    response = patch_namespace_config_map(
+        k8s_client, name, namespace, patch
+    )
+
+    assert response
+
+    patched_csc = read_namespace_config_map(
+        k8s_client, name, namespace
+    )
+    patched_value = utils.get_dict_element(
+        csc_load(patched_csc, name, namespace), path
+    )
+
+    assert literal_eval(value) == patched_value, (
+        "Expected value {} for key {} in ConfigMap {} found in namespace {}, "
+        "got {}".format(value, path, name, namespace, patched_value)
+    )
+
+
+@when(parsers.parse("we apply the '{state}' salt state"))
+def apply_service_config(host, version, request, k8s_client, state):
+    ssh_config = request.config.getoption('--ssh-config')
+
+    cmd = [
+        'salt-run', 'state.sls', '{}'.format(state),
+        'saltenv=metalk8s-{}'.format(version)
+    ]
+    utils.run_salt_command(host, cmd, ssh_config)
+
+
+# }}}
+
+
+# Then {{{
+
+
+@then(parsers.parse(
+    "we have '{value}' at '{path}' for '{service}' Deployment in namespace "
+    "'{namespace}'"))
+def get_deployments(
+    host,
+    version,
+    k8s_appsv1_client,
+    k8s_client,
+    request,
+    value,
+    path,
+    service,
+    namespace
+
+):
+    def _wait_for_deployment():
+        try:
+            response = k8s_appsv1_client.read_namespaced_deployment(
+                name=service, namespace=namespace
+            ).to_dict()
+        except Exception as exc:
+            pytest.fail(
+                "Unable to read {} Deployment with error: {!s}".format(
+                    service, exc
+                )
+            )
+        assert literal_eval(value) == utils.get_dict_element(response, path), (
+            "Expected value {} for service {}, got {}".format(
+                value, service, utils.get_dict_element(response, path)
+            )
+        )
+    utils.retry(
+        _wait_for_deployment,
+        times=10,
+        wait=5,
+        name="wait for available Deployments"
+    )
+
+
+# }}}
+
+
+# Helpers {{{
+
+
+def read_namespace_config_map(k8s_client, name, namespace):
+    try:
+        response = k8s_client.read_namespaced_config_map(
+            name, namespace
+        )
+    except Exception as exc:
+        pytest.fail(
+            "Unable to read {} ConfigMap  in namespace {} with error: {!s}"
+            .format(name, namespace, exc)
+        )
+    return response
+
+
+def patch_namespace_config_map(k8s_client, name, namespace, patch):
+    try:
+        response = k8s_client.patch_namespaced_config_map(
+            name, namespace, patch
+        )
+    except Exception as exc:
+        pytest.fail(
+            "Unable to patch ConfigMap {} in namespace {} with error {!s}"
+            .format(name, namespace, exc)
+        )
+    return response
+
+
+def csc_load(full_csc, name, namespace):
+    try:
+        csc_obj = yaml.safe_load(full_csc.data['config.yaml'])
+    except yaml.YAMLError as exc:
+        raise Exception(
+            'Invalid YAML format in ConfigMap {} found in namespace {}: {!s}'
+            .format(name, namespace, exc)
+        )
+    except Exception as exc:
+        raise Exception(
+            "Failed loading `config.yaml` from ConfigMap {} in namespace {}: "
+            "{!s}".format(name, namespace, exc)
+        )
+    return csc_obj
+
+
+# }}}
