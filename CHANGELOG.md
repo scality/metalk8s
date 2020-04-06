f5869403a1c5fcd1c5701f5c011494cd726c0329
Nicolas Trangez <ikke@nicolast.be>
2020-04-06 13:10:18 +0200

    packages: bump `containerd` to 1.2.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 
 ### Features Added
 - Upgrade Calico to 3.12.0 (PR [#2253](https://github.com/scality/metalk8s/pull/2253))
+- Upgrade `containerd` to 1.2.13 (PR [#2369](https://github.com/scality/metalk8s/pull/2369))
--- buildchain/buildchain/versions.py
+++ buildchain/buildchain/versions.py
@@ -20,7 +20,7 @@ Image = namedtuple('Image', ('name', 'version', 'digest'))
 CALICO_VERSION     : str = '3.12.0'
 K8S_VERSION        : str = '1.17.4'
 SALT_VERSION       : str = '2018.3.4'
-CONTAINERD_VERSION : str = '1.2.4'
+CONTAINERD_VERSION : str = '1.2.13'
 
 def load_version_information() -> None:
     """Load version information from `VERSION`."""
