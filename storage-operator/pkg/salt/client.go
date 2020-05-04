14d2d952cbde3faeb97c63f1084a216a828018e9
Teddy Andrieux <teddy.andrieux@scality.com>
2020-05-04 14:04:15 +0200

    storage-operator: No longer use `success` from Salt response
    In Salt version 3000.2 `success` keyword is properly set so we can't use
    it to pre-check for a failure, always use `retcode`.
    
            0: All good
            1: State do not run because .... (likely concurrent state
               execution), do retry
            2: Real error, do not retry
    
    Sees: https://github.com/saltstack/salt/pull/44726

--- storage-operator/pkg/salt/client.go
+++ storage-operator/pkg/salt/client.go
@@ -189,28 +189,8 @@ func (self *Client) PollJob(
 	nodeResult := result[nodeName].(map[string]interface{})
 
 	// The job is done: check if it has succeeded.
-	success := result[nodeName].(map[string]interface{})["success"].(bool)
 	retcode := result[nodeName].(map[string]interface{})["retcode"].(float64)
 
-	// `"success": false` == stacktrace => the job executed and failed.
-	if !success {
-		jobLogger.Info("Salt job failed")
-		reason := getStateFailureRootCause(nodeResult["return"])
-		return nil, &AsyncJobFailed{reason}
-	}
-	// /!\ `"success": true` != job ran and succeedeed!!
-	// See https://github.com/saltstack/salt/issues/4002
-	//
-	// IIUC, having `success == true` and `retcode != 0` means "the job failed
-	// before being executed": it didn't really fail (because it haven't run)
-	// so we got `"success": true` BUT something went wrong (so retcode != 0).
-	//
-	// Real life example: run `metalk8s.volumes` for volume Foo, then while it's
-	// still in progress run `metalk8s.volumes` for volume Bar => the job for
-	// Bar will fail with success:true/retcode:1 because the state is already
-	// running for Foo…
-	//
-	// So let's check `retcode` to be 100% sure it succeedeed.
 	switch int(retcode) {
 	case 0:
 		jobLogger.Info("Salt job succeeded")
