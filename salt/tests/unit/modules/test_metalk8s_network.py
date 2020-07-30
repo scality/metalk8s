bd8bcbbf1015f3c92e51e0ff32d42af41ee530d6
Teddy Andrieux <teddy.andrieux@scality.com>
2020-07-30 10:37:38 +0200

    salt/tests: Add unit tests for `get_ip_from_cidrs` salt function
    Add unit tests for `get_ip_from_cidrs` function from `metalk8s_network`
    salt custom module

--- salt/tests/unit/modules/test_metalk8s_network.py
+++ salt/tests/unit/modules/test_metalk8s_network.py
@@ -82,3 +82,51 @@ class Metalk8sNetworkTestCase(TestCase, LoaderModuleMockMixin):
                 error_msg,
                 metalk8s_network.get_cluster_dns_ip
             )
+
+    @parameterized.expand([
+        # 1 CIDR, 2 IP, take the first one
+        (['10.200.0.0/16'], ['10.200.0.1', '10.200.0.42'], '10.200.0.1'),
+        # 1 CIDR, 2 IP, current_ip set to the second one, take the second one
+        (['10.200.0.0/16'], ['10.200.0.1', '10.200.0.42'], '10.200.0.42', '10.200.0.42'),
+        # 1 CIDR, no IP, errors
+        (['10.200.0.0/16'], [], 'Unable to find an IP on this host in one of this cidr: 10.200.0.0/16', None, True),
+        # 2 CIDR, multiple IPs, take the first one of first CIDR
+        (['10.200.0.0/16', '10.100.0.0/16'], {'10.200.0.0/16': ['10.200.0.1', '10.200.0.42'], '10.100.0.0/16': ['10.100.0.12', '10.100.0.52']}, '10.200.0.1'),
+        # 2 CIDR, multiple IPs, with current_ip present
+        (['10.200.0.0/16', '10.100.0.0/16'], {'10.200.0.0/16': ['10.200.0.1', '10.200.0.42'], '10.100.0.0/16': ['10.100.0.12', '10.100.0.52']}, '10.100.0.52', '10.100.0.52'),
+        # 2 CIDR, multiple IPs, with current_ip absent
+        (['10.200.0.0/16', '10.100.0.0/16'], {'10.200.0.0/16': ['10.200.0.1', '10.200.0.42'], '10.100.0.0/16': ['10.100.0.12', '10.100.0.52']}, '10.200.0.1', '10.100.0.87'),
+        # 2 CIDR, first CIDR no IP
+        (['10.200.0.0/16', '10.100.0.0/16'], {'10.100.0.0/16': ['10.100.0.12', '10.100.0.52']}, '10.100.0.12'),
+        # 2 CIDR, no IP, with current_ip, errors
+        (['10.200.0.0/16', '10.100.0.0/16'], [], 'Unable to find an IP on this host in one of this cidr: 10.200.0.0/16, 10.100.0.0/16', '10.200.0.1', True),
+    ])
+    def test_get_ip_from_cidrs(self, cidrs, ip_addrs, result,
+                               current_ip=None, raises=False):
+        """
+        Tests the return of `get_ip_from_cidrs` function
+        """
+        def _get_ip_addrs(cidr):
+            if isinstance(ip_addrs, dict):
+                return ip_addrs.get(cidr)
+            return ip_addrs
+
+        salt_dict = {
+            'network.ip_addrs': MagicMock(side_effect=_get_ip_addrs)
+        }
+
+        with patch.dict(metalk8s_network.__salt__, salt_dict):
+            if raises:
+                self.assertRaisesRegexp(
+                    CommandExecutionError,
+                    result,
+                    metalk8s_network.get_ip_from_cidrs,
+                    cidrs=cidrs, current_ip=current_ip
+                )
+            else:
+                self.assertEqual(
+                    result,
+                    metalk8s_network.get_ip_from_cidrs(
+                        cidrs=cidrs, current_ip=current_ip
+                    )
+                )
