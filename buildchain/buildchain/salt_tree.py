e98d6ad74a997c4f556cc3e188b4bde328df7de2
Alexandre Allard <alexandre.allard@scality.com>
2020-08-04 16:42:37 +0200

    build: add loki formulas to salt tree
    Refs: #2682

--- buildchain/buildchain/salt_tree.py
+++ buildchain/buildchain/salt_tree.py
@@ -269,6 +269,9 @@ SALT_FILES : Tuple[Union[Path, targets.AtomicTarget], ...] = (
         renderer=targets.Renderer.SLS,
     ),
 
+    Path('salt/metalk8s/addons/logging/deployed/init.sls'),
+    Path('salt/metalk8s/addons/logging/deployed/namespace.sls'),
+    Path('salt/metalk8s/addons/logging/loki/deployed/chart.sls'),
     Path('salt/metalk8s/addons/logging/loki/deployed/init.sls'),
     Path('salt/metalk8s/addons/logging/loki/deployed/storageclass.sls'),
 
