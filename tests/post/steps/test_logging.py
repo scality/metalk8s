c51c331492826e5aa9ea719ba66477f35f573fb1
Alexandre Allard <alexandre.allard@scality.com>
2020-08-25 18:49:37 +0200

    tests: raise retries for logging tests
    As for now, sometimes the test checking the
    logging pipeline fails because we're not waiting
    enough.
    We now wait 2 minutes, it should avoid flakies.

--- tests/post/steps/test_logging.py
+++ tests/post/steps/test_logging.py
@@ -192,7 +192,7 @@ def retrieve_pod_logs_from_loki(k8s_client, nodename, pod_creation_ts):
 
     utils.retry(
         _check_log_line_exists,
-        times=20,
+        times=40,
         wait=3,
         name="check that a log exists for 'logger' pod"
     )
