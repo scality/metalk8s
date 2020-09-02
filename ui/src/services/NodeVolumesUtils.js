d131414f9486466f59f0f9b9506a40f4a234ac60
YanJin <yanjin.cheng@scality.com>
2020-09-02 11:15:56 +0200

    ui/tests: Addressed the comments for getVolumeListData function
    - Simplfied the state, remove the useless props
    - Simplfied the logic in getVolumeListData, since we should have the same behaviour for the wrong Node filter and non-volume Node
    - Use camelCase for all the mutable variables

--- ui/src/services/NodeVolumesUtils.js
+++ ui/src/services/NodeVolumesUtils.js
@@ -168,24 +168,11 @@ export const getVolumeListData = createSelector(
     volumeLatencyCurrent,
     volumeCapacityCurrentList,
   ) => {
-    let nodeVolumes = null;
-
+    let nodeVolumes = volumes;
     if (nodeFilter) {
-      // when there is node filter in URL, check if the node name is valid / exist
-      const node = nodeList?.find((node) => node.name === nodeFilter);
-      if (!node) {
-        return [];
-      } else {
-        // the node name exists, check if there is any volume belong to this node
-        nodeVolumes = volumes?.filter(
-          (volume) => volume.spec.nodeName === nodeFilter,
-        );
-        if (!nodeVolumes) {
-          return [];
-        }
-      }
-    } else {
-      nodeVolumes = volumes;
+      nodeVolumes = volumes?.filter(
+        (volume) => volume.spec.nodeName === nodeFilter,
+      );
     }
 
     return nodeVolumes?.map((volume) => {
