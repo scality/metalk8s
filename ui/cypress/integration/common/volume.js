6164b600229d2d3edc9b5812ea3b5d4d856a1c49
YanJin <yanjin.cheng@scality.com>
2020-09-07 16:35:49 +0200

    ui/tests: Improve the steps of create volume & Add delete volume steps
    Refs: #2762

--- ui/cypress/integration/common/volume.js
+++ ui/cypress/integration/common/volume.js
@@ -11,68 +11,98 @@ And('I choose the Volumes tag', () => {
   cy.get('.sc-tabs-bar .sc-tabs-item-title').eq(1).click();
 });
 
-And('I go to create volume page by click on Create a New Volume button', () => {
+And('I am on the volume creation page', () => {
+  cy.get('.sc-sidebar-item').eq(2).click();
   cy.get('[data-cy="create-volume-button"]').click();
 });
 
-When('I go to the volume page by click the volume icon in the sidebar', () => {
-  cy.get('.sc-sidebar-item').eq(2).click();
+When('I fill out the volume creation form using:', (dataTable) => {
+  cy.route('GET', '/api/kubernetes/api/v1/persistentvolumes').as(
+    'getPersistentVolumes',
+  );
+
+  // rowsHash: returns an object where each row corresponds to an entry (first column is the key, second column is the value).
+  const dataTableObject = dataTable.rowsHash();
+
+  const volumeName = dataTableObject.name;
+  const volumeType = dataTableObject.type;
+  const volumeSize = dataTableObject.size.split(' ')[0];
+  const volumeLabelName = dataTableObject.labelName;
+  const volumeLabelValue = dataTableObject.labelValue;
+
+  cy.get('input[name=name]').type(volumeName);
+  cy.get('input[name=labelName]').type(volumeLabelName);
+  cy.get('input[name=labelValue]').type(volumeLabelValue);
+  cy.get('[data-cy=add-volume-labels-button]').click();
+
+  // node name
+  cy.get('.sc-select').eq(0).click();
+  cy.get('.sc-select__menu').eq(0).click();
+
+  cy.get('.sc-select').eq(1).click();
+  cy.get('.sc-select__menu').find('[data-cy=storageClass-metalk8s]').click();
+
+  cy.get('.sc-select').eq(2).click();
+  cy.get('.sc-select__menu').find(`[data-cy="type-${volumeType}"]`).click();
+
+  cy.get('input[name=sizeInput]').type(volumeSize);
 });
 
-Then(
-  'I fill out the create volume form with SparseLoopDevice volume type and check if the status is Ready',
-  function () {
-    cy.route('GET', '/api/kubernetes/api/v1/persistentvolumes').as(
-      'getPersistentVolumes',
-    );
-    const volumeNameSparseLoopDevice = `volume-${new Date().getTime()}`;
-    const volumeCapacity = Cypress.env('volume_capacity');
-    const volume_label_name = Cypress.env('volume_label_name');
-    const volume_label_value = Cypress.env('volume_label_value');
+And('I click [Create] button', () => {
+  cy.get('[data-cy="submit-create-volume"]').click();
+});
 
-    cy.get('input[name=name]').type(volumeNameSparseLoopDevice);
+Then('I am redirected to the "test-volume-sparse" volume page', () => {
+  cy.location('pathname').should('eq', '/volumes/test-volume-sparse');
+});
 
-    cy.get('input[name=labelName]').type(volume_label_name);
-    cy.get('input[name=labelValue]').type(volume_label_value);
-    cy.get('[data-cy=add-volume-labels-button]').click();
+And('the volume "test-volume-sparse" becomes Ready', () => {
+  // Check if the right side panel updates
+  cy.get('[data-cy="volume_detail_card_name"]').should(
+    'contain',
+    'test-volume-sparse',
+  );
+  // Wait until the volume is ready
+  cy.waitUntil(
+    () =>
+      cy
+        .get('[data-cy=volume_status_value]')
+        .then(($span) => $span.text() === 'Ready'),
+    {
+      errorMsg: `Volume test-volume-sparse is not ready`,
+      timeout: 120000, // waits up to 120000 ms, default to 5000
+      interval: 5000, // performs the check every 5000 ms, default to 200
+    },
+  );
+});
 
-    // node name
-    cy.get('.sc-select').eq(0).click();
-    cy.get('.sc-select__menu').eq(0).click();
+And('the label of volume "test-volume-sparse" presents:', (dataTable) => {
+  const dataTableObject = dataTable.rowsHash();
 
-    cy.get('.sc-select').eq(1).click();
-    cy.get('.sc-select__menu').find('[data-cy=storageClass-metalk8s]').click();
+  const volumeLabelName = dataTableObject.labelName;
+  const volumeLabelValue = dataTableObject.labelValue;
 
-    cy.get('.sc-select').eq(2).click();
-    cy.get('.sc-select__menu')
-      .find('[data-cy="type-sparseLoopDevice"]')
-      .click();
+  cy.get('[data-cy="volume_label_name"]').should('contain', volumeLabelName);
+  cy.get('[data-cy="volume_label_value"]').should('contain', volumeLabelValue);
+});
 
-    cy.get('input[name=sizeInput]').type(volumeCapacity);
+When('I click [Delete] button', () => {
+  cy.get('[data-cy="delete_volume_button"]').click({ force: true });
+});
 
-    cy.get('.sc-select').eq(2).click();
-    //cy.get('[data-cy="size-KiB"]').click(); leave it at the default value GiB
+And('I confirm the deletion', () => {
+  cy.get('[data-cy="confirm_deletion_button"]').click();
+});
 
-    cy.get('[data-cy="submit-create-volume"]').click();
-
-    // Wait until the volume is ready
-    cy.waitUntil(
-      () =>
-        cy
-          .get('[data-cy=volume_status_value]')
-          .then(($span) => $span.text() === 'Ready'),
-      {
-        errorMsg: `Volume ${volumeNameSparseLoopDevice} is not ready`,
-        timeout: 120000, // waits up to 120000 ms, default to 5000
-        interval: 5000, // performs the check every 5000 ms, default to 200
-      },
-    );
-
-    // Check if all the labels are presents
-    cy.get('[data-cy=volume_label_name]').should('contain', volume_label_name);
-    cy.get('[data-cy=volume_label_value]').should(
-      'contain',
-      volume_label_value,
-    );
-  },
-);
+Then('the "test-volume-sparse" volume is removed from the list', () => {
+  // fiter delete this volume, we should only have 3 default volume in CI.
+  cy.waitUntil(
+    () =>
+      cy.get('[data-cy="volume_table_row"]').then(($el) => $el.length === 3),
+    {
+      errorMsg: `Volume test-volume-sparse is not deleted`,
+      timeout: 120000, // waits up to 120000 ms, default to 5000
+      interval: 5000, // performs the check every 5000 ms, default to 200
+    },
+  );
+});
