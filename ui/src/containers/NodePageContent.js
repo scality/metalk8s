deda894b0ac9d3365a6f50d0349d14584b0c7924
YanJin <yanjin.cheng@scality.com>
2020-09-08 11:33:39 +0200

    ui/nodes: Initiaise the NodePageContent component
    The <NodePageContent/> component
    - Set up the layout of NodePage included Nodelist table and Right-side panel
    - Retrieve the nodelist from <NodePage>
    - Retrieve the current node from the URL to display the information in RSP
    
    Refs: #2771

---
+++ ui/src/containers/NodePageContent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import NodeListTable from '../components/NodeListTable';
+import {
+  LeftSideInstanceList,
+  RightSidePanel,
+  NoInstanceSelectedContainer,
+  NoInstanceSelected,
+  PageContentContainer,
+} from '../components/CommonLayoutStyle';
+import { intl } from '../translations/IntlGlobalProvider';
+
+const NodePageContent = (props) => {
+  const history = useHistory();
+  const currentNodeName =
+    history?.location?.pathname?.split('/')?.slice(2)[0] || '';
+  return (
+    <PageContentContainer>
+      <LeftSideInstanceList>
+        <NodeListTable />
+      </LeftSideInstanceList>
+      {currentNodeName ? (
+        <RightSidePanel></RightSidePanel>
+      ) : (
+        <NoInstanceSelectedContainer>
+          <NoInstanceSelected>
+            {intl.translate('no_node_selected')}
+          </NoInstanceSelected>
+        </NoInstanceSelectedContainer>
+      )}
+    </PageContentContainer>
+  );
+};
+
+export default NodePageContent;
