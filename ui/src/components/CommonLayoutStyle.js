2ed06b838071bce52ed22ffa4c9b86e7e3413aac
YanJin <yanjin.cheng@scality.com>
2020-09-08 11:42:11 +0200

    ui/styles: Abstract the commont styles used by New Volume Page and New Node Page
    Refs: #2771

---
+++ ui/src/components/CommonLayoutStyle.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { padding } from '@scality/core-ui/dist/style/theme';
+
+export const PageContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  height: 100%;
+  flex-wrap: wrap;
+  padding: ${padding.small};
+`;
+
+export const LeftSideInstanceList = styled.div`
+  flex-direction: column;
+  min-height: 696px;
+  width: 45%;
+`;
+
+export const RightSidePanel = styled.div`
+  flex-direction: column;
+  width: 55%;
+  /* Make it scrollable for the small laptop screen */
+  overflow-y: scroll;
+  margin: ${padding.small} ${padding.small} ${padding.small} 0;
+`;
+
+export const NoInstanceSelectedContainer = styled.div`
+  margin: ${padding.small} ${padding.small} ${padding.small} 0;
+  width: 55%;
+  min-height: 700px;
+  background-color: ${(props) => props.theme.brand.primaryDark1};
+`;
+
+export const NoInstanceSelected = styled.div`
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  color: ${(props) => props.theme.brand.textPrimary};
+  text-align: center;
+`;
+
+export const PageContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  background-color: ${(props) => props.theme.brand.primary};
+`;
