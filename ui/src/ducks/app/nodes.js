edc6e4727450f14c209cf9c4c549165f40e6ca15
YanJin <yanjin.cheng@scality.com>
2020-09-09 21:27:45 +0200

    ui/ducks/nodes: Add conditions field for nodes
    The conditions contains the all the conditions to display in the node table
    
    Refs: #2772

--- ui/src/ducks/app/nodes.js
+++ ui/src/ducks/app/nodes.js
@@ -252,6 +252,17 @@ export function* fetchNodes() {
             node.status.conditions.find(
               (conditon) => conditon.type === 'Ready',
             );
+
+          // Store the name of conditions which the status are True in the array
+          // given the avaiable conditions (Ready, DiskPressure, MemoryPressure, PIDPressure, Network Unavailable, Unschedulable)
+          let conditions = [];
+          const activeCondition = node.status.conditions.find(
+            (cond) => cond.status === 'True',
+          );
+          if (activeCondition && activeCondition.types) {
+            conditions.push(activeCondition.type);
+          }
+
           let status;
           if (statusType && statusType.status === 'True') {
             status = API_STATUS_READY;
@@ -260,7 +271,6 @@ export function* fetchNodes() {
           } else {
             status = API_STATUS_UNKNOWN;
           }
-
           const roleTaintMatched = roleTaintMap.find((item) => {
             const nodeRoles = Object.keys(node.metadata.labels).filter((role) =>
               role.includes(ROLE_PREFIX),
@@ -291,12 +301,12 @@ export function* fetchNodes() {
               rolesLabel.push(intl.translate('infra'));
             }
           }
-
           return {
             name: node.metadata.name,
             metalk8s_version:
               node.metadata.labels['metalk8s.scality.com/version'],
             status: status,
+            conditions: conditions,
             control_plane: roleTaintMatched && roleTaintMatched.control_plane,
             workload_plane: roleTaintMatched && roleTaintMatched.workload_plane,
             bootstrap: roleTaintMatched && roleTaintMatched.bootstrap,
