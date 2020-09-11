7981db71548204aa24e5fca741cab7177640a3c2
YanJin <yanjin.cheng@scality.com>
2020-09-11 13:09:24 +0200

    ui/nodepagecontent: Add the RSP for Node Page
    Refs: #2773

--- ui/src/containers/NodePageContent.js
+++ ui/src/containers/NodePageContent.js
@@ -1,29 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { padding } from '@scality/core-ui/dist/style/theme';
 import { useHistory } from 'react-router';
 import NodeListTable from '../components/NodeListTable';
+import NodePageRSP from './NodePageRSP';
 import {
   LeftSideInstanceList,
-  RightSidePanel,
   NoInstanceSelectedContainer,
   NoInstanceSelected,
   PageContentContainer,
 } from '../components/CommonLayoutStyle';
 import { intl } from '../translations/IntlGlobalProvider';
 
+const NodePageRSPContainer = styled.div`
+  flex-direction: column;
+  width: 55%;
+  margin: ${padding.small} ${padding.small} ${padding.small} 0;
+`;
+
 const NodePageContent = (props) => {
   const { nodeTableData } = props;
 
   const history = useHistory();
-  const currentNodeName =
+  const selectedNodeName =
     history?.location?.pathname?.split('/')?.slice(2)[0] || '';
 
   return (
     <PageContentContainer>
       <LeftSideInstanceList>
-        <NodeListTable nodeTableData={nodeTableData} />
+        <NodeListTable
+          nodeTableData={nodeTableData}
+          selectedNodeName={selectedNodeName}
+        />
       </LeftSideInstanceList>
-      {currentNodeName ? (
-        <RightSidePanel></RightSidePanel>
+      {selectedNodeName ? (
+        <NodePageRSPContainer>
+          <NodePageRSP selectedNodeName={selectedNodeName} />
+        </NodePageRSPContainer>
       ) : (
         <NoInstanceSelectedContainer>
           <NoInstanceSelected>
