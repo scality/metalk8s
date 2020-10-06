2a4e402164574fccf609a36f4d38a5bec27e3951
YanJin <yanjin.cheng@scality.com>
2020-10-06 14:43:53 +0200

    ui/nodes: Fill the Overview tab for node page
    Refs: #2774

--- ui/src/components/NodePageHealthTab.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TabContainer } from './CommonLayoutStyle';
-
-const NodePageHealthTab = (props) => {
-  return <TabContainer>Health</TabContainer>;
-};
-
-export default NodePageHealthTab;
