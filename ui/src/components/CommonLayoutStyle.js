969e3e35ed430e0e45babe7657fb090503b9846d
Alex Le Dinh <alex.ledinh@quantum.com>
2020-10-09 20:11:06 +0200

    ui/volumes: TextBadge component to display a counter in a block

--- ui/src/components/CommonLayoutStyle.js
+++ ui/src/components/CommonLayoutStyle.js
@@ -64,3 +64,12 @@ export const TabTitle = styled.div`
   font-weight: ${fontWeight.bold};
   padding: 35px 0 ${padding.small} ${padding.large};
 `;
+
+export const TextBadge = styled.span`
+  background-color: ${(props) => props.theme.brand.base};
+  color: ${(props) => props.theme.brand.textPrimary};
+  padding: 2px ${padding.small};
+  border-radius: 4px;
+  font-size: ${fontSize.small}
+  font-weight: ${fontWeight.bold}
+`;
