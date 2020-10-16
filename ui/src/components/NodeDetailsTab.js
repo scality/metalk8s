3f7963a2106e5f208261bfcec549157fece80b53
YanJin <yanjin.cheng@scality.com>
2020-10-16 16:56:29 +0200

    ui/nodes: Add detail tab component
    Refs: #2843

---
+++ ui/src/components/NodeDetailsTab.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TabContainer } from './CommonLayoutStyle';
+
+const NodePageDetailsTab = (props) => {
+  return <TabContainer>Details</TabContainer>;
+};
+
+export default NodePageDetailsTab;
