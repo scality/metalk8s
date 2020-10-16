c5fae280f57435e733fcc5db37ff4a6dc4c2beeb
Alex Le Dinh <alex.ledinh@quantum.com>
2020-10-16 17:53:33 +0200

    ui/volumes: Improve volume listing. Sort by health (unhealthy first)

--- ui/src/services/NodeVolumesUtils.js
+++ ui/src/services/NodeVolumesUtils.js
@@ -175,7 +175,7 @@ export const getVolumeListData = createSelector(
       );
     }
 
-    return nodeVolumes?.map((volume) => {
+    nodeVolumes = nodeVolumes?.map((volume) => {
       const volumePV = pVList?.find(
         (pV) => pV.metadata.name === volume.metadata.name,
       );
@@ -269,5 +269,17 @@ export const getVolumeListData = createSelector(
         errorReason: volume?.status?.conditions[0]?.reason,
       };
     });
+
+    // Initial data sorting
+    // Following sorts should be handled by react-table directly in the component
+    return nodeVolumes.sort((a, b) => {
+      const weights = {};
+      weights[STATUS_CRITICAL] = 3;
+      weights[STATUS_WARNING] = 2;
+      weights[STATUS_NONE] = 1;
+      weights[STATUS_HEALTH] = 0;
+
+      return weights[b.health] - weights[a.health];
+    })
   },
 );
