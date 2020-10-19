20ff46380fe0db9a4a385d42db56405626ffbc35
YanJin <yanjin.cheng@scality.com>
2020-10-19 21:12:12 +0200

    ui/nodes: Display Node Object JSON in the Details Tab
    Refs: #2843

--- ui/src/components/NodeDetailsTab.js
+++ ui/src/components/NodeDetailsTab.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { padding } from '@scality/core-ui/dist/style/theme';
 import { TabContainer } from './CommonLayoutStyle';
 
+const NodeObjectContent = styled.div`
+  padding: ${padding.large} 50px 0 ${padding.larger};
+  white-space: pre-wrap;
+  overflow-y: auto;
+  height: 78vh;
+`;
+
 const NodePageDetailsTab = (props) => {
-  return <TabContainer>Details</TabContainer>;
+  const currentNodeObject = useSelector(
+    (state) => state.app.nodes.currentNodeObject,
+  );
+
+  return (
+    <TabContainer>
+      <NodeObjectContent>
+        {currentNodeObject && JSON.stringify(currentNodeObject, null, '\t')}
+      </NodeObjectContent>
+    </TabContainer>
+  );
 };
 
 export default NodePageDetailsTab;
