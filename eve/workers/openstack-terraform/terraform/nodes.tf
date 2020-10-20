19025f01640e8c72c1171d3ad49a2eae8eb18647
Teddy Andrieux <teddy.andrieux@scality.com>
2020-10-20 17:48:07 +0200

    eve: Add cypress support in `openstack-terraform` worker
    Add support for Cypress spawn using single terraform
    `openstack-terraform` worker (it basically install Cypress requirements
    on the Bastion VM, so that we can run Cypress test from Bastion)

--- eve/workers/openstack-terraform/terraform/nodes.tf
+++ eve/workers/openstack-terraform/terraform/nodes.tf
@@ -67,6 +67,14 @@ resource "openstack_compute_instance_v2" "bastion" {
     ]
   }
 
+  # Install Cypress requirements
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x scripts/cypress-requirements.sh",
+      "scripts/cypress-requirements.sh"
+    ]
+  }
+
   # Install basic dependencies for running end-to-end tests
   provisioner "remote-exec" {
     inline = [
