482fde1a1c0fcf71adf60791bd82ac946b3f8035
Guillaume Demonet <guillaume.demonet@scality.com>
2020-10-21 20:58:23 +0200

    ui/tests: Re-organize end-to-end Cypress tests
    We move all end-to-end Gherkin-based tests to `cypress/integration/e2e`
    (tried setting it up under `cypress/e2e`, but Cypress itself needs a
    single location for spec files, so having everything under
    `cypress/integration` makes it simpler).
    
    We also rename feature files to be more generic, including multiple
    scenarios in each when relevant (only for Solutions, at the moment),
    and make sure to scope step definitions to their related features,
    instead of putting everything under `common`.
    
    We finally update the `test:e2e` and `test:e2esolutions` npm scripts to
    use a more specific selection (only `.feature` files, and only under the
    `e2e` directory).
    
    See: #2878

--- ui/cypress/integration/e2e/Language/steps.js
+++ ui/cypress/integration/e2e/Language/steps.js
@@ -1,6 +1,6 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
 
-Then('I change the language by click the dropdown in the narbar', () => {
+Then('I change the language by click the dropdown in the navbar', () => {
   window.localStorage.setItem('language', 'EN');
   cy.get('.sc-dropdown')
     .eq(0)
