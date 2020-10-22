fb2015df3c77e123af5a8143d960e80b08e747b3
Guillaume Demonet <guillaume.demonet@scality.com>
2020-10-22 12:57:51 +0200

    ui/cypress: Add minimal API mock fixtures
    We define some useful fixtures for stubbing network responses that the
    UI expects on load.
    We also add a command, named `setupMocks`, to initialize this stubbing
    with the aforementioned fixtures.
    
    This serves as a first step, and will likely evolve according to the
    various test needs.
    
    See: #2878

--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -24,3 +24,26 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import 'cypress-wait-until';
+
+Cypress.Commands.add('setupMocks', () => {
+  cy.server();
+
+  cy.route('/config.json', 'fixture:config.json');
+  cy.route(
+    '/oidc/.well-known/openid-configuration',
+    'fixture:openid-config.json',
+  );
+  cy.route('POST', '/api/salt/login', 'fixture:salt-api/login.json');
+  cy.route(
+    '/api/prometheus/api/v1/alerts',
+    'fixture:prometheus/empty-alerts.json',
+  );
+  cy.route(
+    '/api/prometheus/api/v1/query?query=sum(up{job="*"})',
+    'fixture:prometheus/query-up-ok.json',
+  );
+  cy.route(
+    '/api/kubernetes/api/v1/namespaces?includeUninitialized=&pretty=&continue=&fieldSelector=metadata.name=kube-system',
+    'fixture:kubernetes/namespace-kube-system.json',
+  );
+});
