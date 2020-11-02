b2fd611692af0ba9364111c7d9986bb3a3f19ae5
YanJin <yanjin.cheng@scality.com>
2020-11-02 10:50:39 +0100

    ui/test: Refactorize the code between source tabs

--- ui/cypress/integration/node/nodetabs.spec.js
+++ ui/cypress/integration/node/nodetabs.spec.js
@@ -3,33 +3,38 @@ beforeEach(() => {
   cy.login();
 });
 
+const allTabs = ['overview', 'alerts', 'metrics', 'volumes', 'pods', 'details'];
+const tabSwitchingTestCases = (sourceTab) => {
+  allTabs
+    .filter((tab) => tab !== sourceTab)
+    .map((tab) => {
+      it(`brings me to the ${tab} tab`, () => {
+        cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
+        cy.get('@historyPush').should(
+          'be.calledWith',
+          `/nodes/master-0/${tab}`,
+        );
+      });
+    });
+
+  it('does not switch to the current tab', () => {
+    cy.get(`[data-cy="${sourceTab}_tab_node_page"]`).click();
+    cy.get('@historyPush').should('not.be.called');
+  });
+};
+
 // Navigation tests
 describe('Node page overview tab', () => {
   beforeEach(() => {
     cy.visit('/nodes/master-0/overview');
-    cy.window()
-      .its('__history__')
-      .then((history) => {
-        cy.stub(history, 'push').as('historyPush');
-      });
+    cy.stubHistory();
   });
 
-  const tabs = ['alerts', 'metrics', 'volumes', 'pods', 'details'];
-  tabs.map((tab) => {
-    it(`brings me to the ${tab} tab`, () => {
-      cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
-      cy.get('@historyPush').should('be.calledWith', `/nodes/master-0/${tab}`);
-    });
-  });
-
-  it('does not switch the tab', () => {
-    cy.get(`[data-cy="overview_tab_node_page"]`).click();
-    cy.get('@historyPush').should('not.be.called');
-  });
+  tabSwitchingTestCases('overview');
 
   const alertSeverities = ['critical', 'warning'];
   alertSeverities.map((severity) => {
-    it(`brings me to the alert tab prefiltered by ${severity} severity of master-0 node`, () => {
+    it(`brings me to the alert tab prefiltered by ${severity} severity`, () => {
       cy.get(`[data-cy="${severity}_counter_node"]`).click();
       cy.get('@historyPush').should(
         'be.calledWith',
@@ -42,25 +47,10 @@ describe('Node page overview tab', () => {
 describe('Node page alerts tab', () => {
   beforeEach(() => {
     cy.visit('/nodes/master-0/alerts');
-    cy.window()
-      .its('__history__')
-      .then((history) => {
-        cy.stub(history, 'push').as('historyPush');
-      });
+    cy.stubHistory();
   });
 
-  const tabs = ['overview', 'metrics', 'volumes', 'pods', 'details'];
-  tabs.map((tab) => {
-    it(`brings me to the ${tab} tab`, () => {
-      cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
-      cy.get('@historyPush').should('be.calledWith', `/nodes/master-0/${tab}`);
-    });
-  });
-
-  it('does not switch the tab', () => {
-    cy.get('[data-cy="alerts_tab_node_page"]').click();
-    cy.get('@historyPush').should('not.be.called');
-  });
+  tabSwitchingTestCases('alerts');
 
   // Align Node and Volume Page
   const alertSeverities = [
@@ -86,23 +76,10 @@ describe('Node page alerts tab', () => {
 describe('Node page metrics tab', () => {
   beforeEach(() => {
     cy.visit('/nodes/master-0/metrics');
-    cy.window()
-      .its('__history__')
-      .then((history) => {
-        cy.stub(history, 'push').as('historyPush');
-      });
+    cy.stubHistory();
   });
-  const tabs = ['overview', 'alerts', 'volumes', 'pods', 'details'];
-  tabs.map((tab) => {
-    it(`brings me to the ${tab} tab`, () => {
-      cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
-      cy.get('@historyPush').should('be.calledWith', `/nodes/master-0/${tab}`);
-    });
-  });
-  it('does not switch the tab', () => {
-    cy.get(`[data-cy="metrics_tab_node_page"]`).click();
-    cy.get('@historyPush').should('not.be.called');
-  });
+
+  tabSwitchingTestCases('metrics');
 
   it('brings me to the Grafana Node Detailed dashboard', () => {
     cy.get('[data-cy="advanced_metrics_node_detailed"]')
@@ -117,25 +94,10 @@ describe('Node page metrics tab', () => {
 describe('Node page volumes tabs', () => {
   beforeEach(() => {
     cy.visit('/nodes/master-0/volumes');
-    cy.window()
-      .its('__history__')
-      .then((history) => {
-        cy.stub(history, 'push').as('historyPush');
-      });
+    cy.stubHistory();
   });
 
-  const tabs = ['overview', 'alerts', 'metrics', 'pods', 'details'];
-  tabs.map((tab) => {
-    it(`brings me to the ${tab} tab`, () => {
-      cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
-      cy.get('@historyPush').should('be.calledWith', `/nodes/master-0/${tab}`);
-    });
-  });
-
-  it('does not switch the tab', () => {
-    cy.get(`[data-cy="volumes_tab_node_page"]`).click();
-    cy.get('@historyPush').should('not.be.called');
-  });
+  tabSwitchingTestCases('volumes');
 
   it('brings me to the loki-vol volume page', () => {
     cy.get('[data-cy=volume_table_name_cell]')
@@ -151,23 +113,8 @@ describe('Node page volumes tabs', () => {
 describe('Node page details tabs', () => {
   beforeEach(() => {
     cy.visit('/nodes/master-0/details');
-    cy.window()
-      .its('__history__')
-      .then((history) => {
-        cy.stub(history, 'push').as('historyPush');
-      });
+    cy.stubHistory();
   });
 
-  const tabs = ['overview', 'alerts', 'metrics', 'pods', 'volumes'];
-  tabs.map((tab) => {
-    it(`brings me to the ${tab} tab`, () => {
-      cy.get(`[data-cy="${tab}_tab_node_page"]`).click();
-      cy.get('@historyPush').should('be.calledWith', `/nodes/master-0/${tab}`);
-    });
-  });
-
-  it('does not switch the tab', () => {
-    cy.get(`[data-cy="details_tab_node_page"]`).click();
-    cy.get('@historyPush').should('not.be.called');
-  });
+  tabSwitchingTestCases('details');
 });
