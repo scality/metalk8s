28db03a8aee994403ac785955135c92a00d26646
YanJin <yanjin.cheng@scality.com>
2020-11-02 19:28:26 +0100

    ui/tests: Add window.gc()

--- ui/cypress/support/index.js
+++ ui/cypress/support/index.js
@@ -22,6 +22,22 @@ import './commands';
 // In Cypress, we disable fetch feature to catch the response.
 // You can find the solution here:
 // https://github.com/cypress-io/cypress/issues/1619
-Cypress.on('window:before:load', win => {
+Cypress.on('window:before:load', (win) => {
   win.fetch = null;
+});
+
+// Reduce the memory usage by enabling window.gc() and calling it globally in an afterEach callback.
+// Please find the issue here:
+// https://github.com/cypress-io/cypress/issues/350
+afterEach(() => {
+  cy.window().then((win) => {
+    if (typeof win.gc === 'function') {
+      // calling this more often seems to trigger major GC event more reliably
+      win.gc();
+      win.gc();
+      win.gc();
+      win.gc();
+      win.gc();
+    }
+  });
 });
