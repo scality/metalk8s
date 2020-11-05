88a91d1ec7bfd50fbc41fc6626e48f030eceeec8
YanJin <yanjin.cheng@scality.com>
2020-11-05 16:53:19 +0100

    ui/tests: Add more interaction test for nodelist

--- ui/cypress/integration/node/nodelist.spec.js
+++ ui/cypress/integration/node/nodelist.spec.js
@@ -25,7 +25,7 @@ describe('Node list', () => {
 
   it('brings me to the overview tab of master-0 Node', () => {
     cy.get('[data-cy="node_table_name_cell"]').contains('master-0').click();
-    cy.get('@historyPush').should('be.calledWith', {
+    cy.get('@historyPush').should('be.calledWithExactly', {
       pathname: '/nodes/master-0/overview',
       search: '',
     });
@@ -40,5 +40,26 @@ describe('Node list', () => {
       pathname: '/nodes/master-1/metrics',
       search: 'from=now-7d',
     });
+  });
+
+  it('brings me to create node page', () => {
+    cy.get('[data-cy="create_node_button"]').click();
+    cy.get('@historyPush').and('be.calledWithExactly', '/nodes/create');
+  });
+
+  it('updates url with the search ', () => {
+    cy.get('[data-cy="node_list_search"]').type('hello');
+    cy.get('@historyPush').and('be.calledWithExactly', '?search=hello');
+  });
+
+  it(`keeps warning severity for the alert while searching the node`, () => {
+    cy.visit('/nodes/master-0/alerts?severity=warning');
+    cy.stubHistory();
+    cy.get('[data-cy="node_list_search"]').type('hello');
+
+    cy.get('@historyPush').should(
+      'be.calledWithExactly',
+      '?severity=warning&search=hello',
+    );
   });
 });
