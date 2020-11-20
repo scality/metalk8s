2b7c339748613cd638fd2fe7489275af850f4446
YanJin <yanjin.cheng@scality.com>
2020-11-20 10:38:30 +0100

    ui/table: Replace the text unknown by hyphen icon
    Too many "Unknown" in the table seems extremly noisy and since the
    "Unknown" information is rather less important, we should replace it by
    an icon with tooltip.
    
    Refs: #2931

--- ui/src/components/VolumeListTable.js
+++ ui/src/components/VolumeListTable.js
@@ -319,7 +319,19 @@ function Table({
             ) {
               return (
                 <td {...cellProps}>
-                  <div>{intl.translate('unknown')}</div>
+                  <Tooltip
+                    placement="top"
+                    overlay={
+                      <TooltipContent>
+                        {intl.translate('unknown')}
+                      </TooltipContent>
+                    }
+                  >
+                    <UnknownIcon
+                      className="fas fa-minus"
+                      theme={theme}
+                    ></UnknownIcon>
+                  </Tooltip>
                 </td>
               );
             } else {
@@ -329,7 +341,7 @@ function Table({
         </TableRow>
       );
     },
-    [prepareRow, rowClicked, rows, volumeName],
+    [prepareRow, rowClicked, rows, volumeName, theme],
   );
 
   return (
@@ -553,7 +565,19 @@ const VolumeListTable = (props) => {
                 </Tooltip>
               );
             default:
-              return <div>{intl.translate('unknown')}</div>;
+              return (
+                <Tooltip
+                  placement="top"
+                  overlay={
+                    <TooltipContent>{intl.translate('unknown')}</TooltipContent>
+                  }
+                >
+                  <UnknownIcon
+                    className="fas fa-minus"
+                    theme={theme}
+                  ></UnknownIcon>
+                </Tooltip>
+              );
           }
         },
         sortType: 'status',
