0103cf34fbdaa528a2359cbf615a4223c7ceb35b
Teddy Andrieux <teddy.andrieux@scality.com>
2020-12-11 17:24:26 +0100

    eve: Add a script to generate snapshot from terraform output
    Add a simple bash script that use terraform environment variable and
    terraform output to generate snapshot with `openstack` client
    
    Refs: #2906

--- eve/workers/openstack-terraform/requirements.sh
+++ eve/workers/openstack-terraform/requirements.sh
@@ -5,6 +5,7 @@ yum install -y epel-release
 PACKAGES=(
     curl
     git
+    jq
     make
     python36-pip
     unzip
@@ -14,7 +15,7 @@ yum install -y "${PACKAGES[@]}"
 
 yum clean all
 
-sudo -u eve pip3.6 install --user tox
+sudo -u eve pip3.6 install --user tox python-openstackclient
 
 sudo -u eve mkdir -p /home/eve/.ssh/
 sudo -u eve ssh-keygen -t rsa -b 4096 -N '' -f /home/eve/.ssh/terraform
