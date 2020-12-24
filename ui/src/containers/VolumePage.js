48e97b37e91977fad68cf47c4ec66202592232f8
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2020-12-24 16:34:17 +0100

    ui: Introduce useTypedSelector in volume page

--- ui/src/containers/VolumePage.js
+++ ui/src/containers/VolumePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useRouteMatch } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import VolumeContent from './VolumePageContent';
 import { fetchPodsAction } from '../ducks/app/pods';
 import { refreshNodesAction, stopRefreshNodesAction } from '../ducks/app/nodes';
@@ -32,6 +32,7 @@ import { getVolumeListData } from '../services/NodeVolumesUtils';
 import { Breadcrumb } from '@scality/core-ui';
 import { PageContainer } from '../components/CommonLayoutStyle';
 import { intl } from '../translations/IntlGlobalProvider';
+import { useTypedSelector } from '../hooks';
 
 // <VolumePage> component fetchs all the data used by volume page from redux store.
 // the data for <VolumeMetricGraphCard>: get the default metrics time span `last 24 hours`, and the component itself can change the time span base on the dropdown selection.
@@ -68,28 +69,30 @@ const VolumePage = (props) => {
   useRefreshEffect(refreshAlertManagerAction, stopRefreshAlertManagerAction);
 
   // get all the pods for all the nodes
-  const theme = useSelector((state) => state.config.theme);
-  const pods = useSelector((state) => state.app.pods.list);
-  const node = useSelector((state) => makeGetNodeFromUrl(state, props));
-  const nodes = useSelector((state) => state.app.nodes.list);
-  const volumes = useSelector((state) => state.app.volumes.list);
-  const volumesLoading = useSelector((state) => state.app.volumes.isLoading);
-  const currentVolumeObject = useSelector(
+  const theme = useTypedSelector((state) => state.config.theme);
+  const pods = useTypedSelector((state) => state.app.pods.list);
+  const node = useTypedSelector((state) => makeGetNodeFromUrl(state, props));
+  const nodes = useTypedSelector((state) => state.app.nodes.list);
+  const volumes = useTypedSelector((state) => state.app.volumes.list);
+  const volumesLoading = useTypedSelector((state) => state.app.volumes.isLoading);
+  const currentVolumeObject = useTypedSelector(
     (state) => state.app.volumes.currentVolumeObject,
   );
-  const pVList = useSelector((state) => state.app.volumes.pVList);
+
+  const pVList = useTypedSelector((state) => state.app.volumes.pVList);
+
   /*
    ** The PVCs list is used to check when the alerts will be mapped to the corresponding volumes
    ** in order to auto select the volume when all the data are there.
    */
-  const pVCList = useSelector((state) => state?.app?.volumes?.pVCList);
-  const alerts = useSelector((state) => state.app.alerts);
+  const pVCList = useTypedSelector((state) => state?.app?.volumes?.pVCList);
+  const alerts = useTypedSelector((state) => state.app.alerts);
 
-  const volumeStats = useSelector(
+  const volumeStats = useTypedSelector(
     (state) => state.app.monitoring.volumeStats.metrics,
   );
   // get all the volumes maybe filter by node
-  const volumeListData = useSelector((state) =>
+  const volumeListData = useTypedSelector((state) =>
     getVolumeListData(state, props),
   );
 
