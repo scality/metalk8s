bd7f6b3957354b106eafe2bd88288179184f7824
Alexandre Allard <alexandre.allard@scality.com>
2021-01-07 17:01:25 +0100

    salt: Add sysctl custom state module
    This module is a wrapper around the Salt sysctl
    state module, it checks that the parameter has
    precedence using our custom execution module
    `metalk8s_sysctl` before applying it and exits
    in error if not.
    
    Refs: #2950

---
+++ salt/_modules/metalk8s_sysctl.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Execution module to handle MetalK8s sysctl.
+"""
+
+import configparser
+import itertools
+import pathlib
+
+from salt.exceptions import CommandExecutionError
+
+__virtualname__ = 'metalk8s_sysctl'
+
+# Order in this list defines the precedence
+SYSCTL_CFG_DIRECTORIES = [
+    '/run/sysctl.d',
+    '/etc/sysctl.d',
+    '/usr/local/lib/sysctl.d',
+    '/usr/lib/sysctl.d',
+    '/lib/sysctl.d',
+]
+# This file is applied last no matter what
+SYSCTL_DEFAULT_CFG = '/etc/sysctl.conf'
+
+
+def __virtual__():
+    return __virtualname__
+
+
+def _get_sysctl_files(config):
+    """
+    Return all the sysctl configuration files ordered as they are
+    read by the system.
+    Inject the configuration file passed in argument `config` in this
+    list, in case this file does not exist yet.
+    If the `config` file is not in an authorized path (see `SYSCTL_FILE_GLOBS`
+    and `SYSCTL_DEFAULT_CFG`) or is overwritten by a file with the same name
+    but higher precedence, it is ignored as the system will not take care
+    of it anyway.
+    """
+    config_path = pathlib.Path(config).resolve()
+    files = {}
+
+    for directory in SYSCTL_CFG_DIRECTORIES:
+        path = pathlib.Path(directory)
+        if path == config_path.parent:
+            files.setdefault(config_path.name, str(config_path))
+
+        for cfg in path.glob('*.conf'):
+            files.setdefault(cfg.name, str(cfg))
+
+    sorted_files = [files[name] for name in sorted(files)]
+    sorted_files.append(SYSCTL_DEFAULT_CFG)
+
+    return sorted_files
+
+
+def has_precedence(name, value, config, strict=False):
+    """
+    Read all sysctl configuration file to check if the passed `name` and
+    `value` are not overwritten by an already existing sysctl configuration
+    file.
+    If `strict` is set, check that the final value comes from the passed
+    `config` and not another sysctl configuration file (even if the value is
+    equal to `value`).
+    """
+    sysctl_files = _get_sysctl_files(config)
+
+    # Ignore files before the `config` one.
+    try:
+        sysctl_files = sysctl_files[sysctl_files.index(config) + 1:]
+    except ValueError:
+        # If the file is not in the list, it means it's overwritten by an
+        # other sysctl configuration file with higher precedence.
+        config_name = pathlib.PurePath(config).name
+        for sysctl_file in sysctl_files:
+            sysctl_name = pathlib.PurePath(sysctl_file).name
+            if sysctl_name == config_name:
+                raise CommandExecutionError(
+                    "'{0}' has a higher precedence and overrides '{1}'"
+                    .format(sysctl_file, config)
+                )
+
+        # The target file is not in a directory checked by the system
+        raise CommandExecutionError(
+            "{0} is not a correct path for a sysctl configuration "
+            "file, please use one of the following:\n- {1}"
+            .format(config, "\n- ".join(SYSCTL_CFG_DIRECTORIES))
+        )
+
+    parser = configparser.ConfigParser()
+    epured_value = " ".join(str(value).split())
+
+    for sysctl_file in sysctl_files:
+        with open(sysctl_file, 'r') as sysctl_fd:
+            parser.read_file(['[global]', *sysctl_fd], source=sysctl_file)
+        sysctl = dict(parser.items('global'))
+        parser.remove_section('global')
+        if name in sysctl and (
+                strict or " ".join(sysctl[name].split()) != epured_value):
+            raise CommandExecutionError(
+                "'{0}' redefines '{1}' with value '{2}'"
+                .format(sysctl_file, name, sysctl[name])
+            )
---
+++ salt/_states/metalk8s_sysctl.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Custom state to handle MetalK8s sysctl.
+"""
+
+from salt.exceptions import CommandExecutionError
+
+__virtualname__ = 'metalk8s_sysctl'
+
+
+def __virtual__():
+    if "sysctl.present" not in __states__:
+        return (False, "sysctl state module could not be loaded")
+    return __virtualname__
+
+
+def present(name, value, config=None, check_priority=True, strict=False):
+    """
+    Wrapper around `sysctl.present` state module adding a check of
+    the sysctl parameter priority if `check_priority` is `True`.
+    If `strict` is set to `True`, check that the passed `config` is
+    the last file to define this `value`.
+    """
+    if config is None:
+        config = __salt__["sysctl.default_config"]()
+
+    if check_priority:
+        try:
+            __salt__['metalk8s_sysctl.has_precedence'](
+                name, value, config, strict
+            )
+        except CommandExecutionError as exc:
+            return {
+                "name": name,
+                "result": False,
+                "changes": {},
+                "comment": "Unable to set sysctl value: {0}".format(exc)
+            }
+
+    return __states__['sysctl.present'](name, value, config)
