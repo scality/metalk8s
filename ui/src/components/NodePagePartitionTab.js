c884a20a4a0533667a3a713f37ebc04ee4f0598a
YanJin <yanjin.cheng@scality.com>
2021-01-15 17:10:43 +0100

    ui/nodes: Add Node Partition Tab
    Refs: #2932

---
+++ ui/src/components/NodePagePartitionTab.js
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Button } from '@scality/core-ui';
+import { NodeTab } from './CommonLayoutStyle';
+import { intl } from '../translations/IntlGlobalProvider';
+import { padding } from '@scality/core-ui/dist/style/theme';
+import NodePartitionTable from './NodePartitionTable';
+import { PORT_NODE_EXPORTER } from '../constants';
+
+const TitleContainer = styled.div`
+  display: flex;
+  position: sticky;
+  z-index: 100;
+  top: 0;
+  background-color: ${(props) => props.theme.brand.primary};
+  justify-content: flex-end;
+  padding: ${padding.large} ${padding.base} 0 ${padding.larger};
+`;
+
+const NodePagePartitionTab = (props: Object) => {
+  const { instanceIP } = props;
+
+  // To redirect to the right Node(Detailed) dashboard in Grafana
+  const config = useSelector((state) => state.config);
+  const unameInfos = useSelector((state) => state.app.monitoring.unameInfo);
+  const hostnameLabel = unameInfos.find(
+    (unameInfo) =>
+      unameInfo?.metric?.instance === `${instanceIP}:${PORT_NODE_EXPORTER}`,
+  )?.metric?.nodename;
+
+  return (
+    <NodeTab>
+      <TitleContainer>
+        <Button
+          text={intl.translate('advanced_metrics')}
+          variant={'base'}
+          onClick={() => {}}
+          icon={<i className="fas fa-external-link-alt" />}
+          size={'small'}
+          // We can't redirect to the Node(detailed) Filesystem Detail catagory.
+          // So we hardcode the panel ID to redirect to 'File Nodes Free' chart
+          href={`${config.api.url_grafana}/dashboard/db/nodes-detailed?var-DS_PROMETHEUS=Prometheus&var-job=node-exporter&var-name=${hostnameLabel}&viewPanel=41`}
+          target="_blank"
+          rel="noopener noreferrer"
+          data-cy="advanced_metrics_node_detailed_file_node_free"
+        />
+      </TitleContainer>
+      <NodePartitionTable instanceIP={instanceIP} />
+    </NodeTab>
+  );
+};
+
+export default NodePagePartitionTab;
--- ui/src/components/NodePageSystemDevicesTab.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NodePageSystemDevicesTab = (props) => {
-  return <div>System Devices</div>;
-};
-
-export default NodePageSystemDevicesTab;
