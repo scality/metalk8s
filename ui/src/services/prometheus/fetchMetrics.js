a25be465dd516b85dd615d632c40ae7e8ce87d19
YanJin <yanjin.cheng@scality.com>
2021-01-15 17:40:15 +0100

    ui/package: Introduce React-query, React testing library.
    Bump the version of styeld-component and core-ui
    
    Refs: #2932

--- ui/src/services/prometheus/fetchMetrics.js
+++ ui/src/services/prometheus/fetchMetrics.js
@@ -1,21 +1,20 @@
 // @flow
 import { queryPrometheus } from './api';
 import { PORT_NODE_EXPORTER } from '../../constants';
+import type { PrometheusQueryResult } from './api';
 
-export function queryNodeFSAvail(instanceIP: string): Promise<Object> {
+export function queryNodeFSUsage(
+  instanceIP: string,
+): Promise<PrometheusQueryResult> {
   // All system partitions, except the ones mounted by containerd.
-  // Ingoring the Filesystem ISSO 9660 and shared memory (device='shm')
-  const nodeFilesystemAvailBytesQuery = `node_filesystem_avail_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}",job=~"node-exporter",device!~'rootfs|shm', fstype!='iso9660'}`;
-  const nodeFileSystemAvailBytes = queryPrometheus(
-    nodeFilesystemAvailBytesQuery,
-  );
-
-  return nodeFileSystemAvailBytes;
+  // Ingoring the Filesystem ISSO 9660 and tmpfs & share memory devices.
+  const nodeFilesystemUsageQuery = `(1 - node_filesystem_avail_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}",job=~"node-exporter",device!~'rootfs|shm|tmpfs', fstype!='iso9660'} / node_filesystem_size_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}",job=~"node-exporter",device!~'rootfs|shm', fstype!='iso9660'}) * 100`;
+  return queryPrometheus(nodeFilesystemUsageQuery);
 }
 
-export function queryNodeFSSize(instanceIP: string): Promise<Object> {
-  const nodeFilesystemSizeBytesQuery = `node_filesystem_size_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}",job=~"node-exporter",device!~'rootfs|shm', fstype!='iso9660'}`;
-  const nodeFileSystemSizeBytes = queryPrometheus(nodeFilesystemSizeBytesQuery);
-
-  return nodeFileSystemSizeBytes;
+export function queryNodeFSSize(
+  instanceIP: string,
+): Promise<PrometheusQueryResult> {
+  const nodeFilesystemSizeBytesQuery = `node_filesystem_size_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}",job=~"node-exporter",device!~'rootfs|shm|tmpfs', fstype!='iso9660'}`;
+  return queryPrometheus(nodeFilesystemSizeBytesQuery);
 }
