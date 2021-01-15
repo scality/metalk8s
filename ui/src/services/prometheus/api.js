63c0122de2207e3931c3df8a6ee092c08c59b19b
YanJin <yanjin.cheng@scality.com>
2021-01-15 17:17:33 +0100

    ui/typing: Add typing for the result return by prometheus query
    Refs: #2932

--- ui/src/services/prometheus/api.js
+++ ui/src/services/prometheus/api.js
@@ -1,25 +1,79 @@
+//@flow
 import ApiClient from '../ApiClient';
+type RangeMatrixResult = {
+  resultType: 'matrix',
+  result: {
+    metric: {
+      [labelName: string]: string,
+    },
+    values: [number, string][],
+  }[],
+};
+
+export type InstantVectorResult = {
+  resultType: 'vector',
+  result: {
+    metric: {
+      [labelName: string]: string,
+    },
+    value: [number, string],
+  }[],
+};
+
+type ScalarResult = {
+  resultType: 'scalar',
+  result: [number, string],
+};
+
+type StringResult = {
+  resultType: 'string',
+  result: [number, string],
+};
+
+export type PrometheusQueryResult =
+  | {
+      status: 'error',
+      errorType: string,
+      error: string,
+    }
+  | {
+      status: 'success',
+      data:
+        | RangeMatrixResult
+        | InstantVectorResult
+        | ScalarResult
+        | StringResult,
+      warnings?: string[],
+    };
 
 let prometheusApiClient = null;
 
-export function initialize(apiUrl) {
+export function initialize(apiUrl: string) {
   prometheusApiClient = new ApiClient({ apiUrl });
 }
 
 export function getAlerts() {
-  return prometheusApiClient.get('/api/v1/alerts');
+  if (prometheusApiClient) {
+    return prometheusApiClient.get('/api/v1/alerts');
+  }
 }
 
-export function queryPrometheus(query) {
-  return prometheusApiClient.get('/api/v1/query?query=' + query);
+export function queryPrometheus(query: string): function {
+  if (prometheusApiClient) {
+    return prometheusApiClient.get('/api/v1/query?query=' + query);
+  }
 }
 
-// start (from when we want to fetch the metrics)
-// end
-// step (time in seconds between 2 metrics)
-// query (the actual PromQL expression)
-export function queryPrometheusRange(start, end, step, query) {
-  return prometheusApiClient.get(
-    `/api/v1/query_range?start=${start}&end=${end}&step=${step}&query=` + query,
-  );
+export function queryPrometheusRange(
+  start: string, // start (from when we want to fetch the metrics)
+  end: string,
+  step: number, // step (time in seconds between 2 metrics)
+  query: string, // query (the actual PromQL expression)
+) {
+  if (prometheusApiClient) {
+    return prometheusApiClient.get(
+      `/api/v1/query_range?start=${start}&end=${end}&step=${step}&query=` +
+        query,
+    );
+  }
 }
