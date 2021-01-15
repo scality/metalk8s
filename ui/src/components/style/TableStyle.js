062e03bc93992b661c7be1d3991f76f20acc5af0
YanJin <yanjin.cheng@scality.com>
2021-01-15 17:16:13 +0100

    ui/style: Move all the style component to a dedicate file
    Refs: #2932

---
+++ ui/src/components/style/TableStyle.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { padding, fontWeight } from '@scality/core-ui/dist/style/theme';
+
+export const TooltipContent = styled.div`
+  color: ${(props) => props.theme.brand.textSecondary};
+  font-weight: ${fontWeight.bold};
+  min-width: 60px;
+`;
+
+export const UnknownIcon = styled.i`
+  color: ${(props) => props.theme.brand.textSecondary};
+  // Increase the height so that the users don't need to hover precisely on the hyphen.
+  height: 30px;
+  padding-top: ${padding.base};
+`;
