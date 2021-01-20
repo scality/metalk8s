03da3bc5b208aaa50d1ceebb38dacd0bcea8f593
YanJin <yanjin.cheng@scality.com>
2021-01-20 13:02:43 +0100

    ui/typing: Use useTypedSelector to access the typed redux-store
    Refs: #2932

--- ui/src/components/NodePagePartitionTab.js
+++ ui/src/components/NodePagePartitionTab.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Button } from '@scality/core-ui';
 import { NodeTab } from './CommonLayoutStyle';
@@ -8,6 +7,7 @@ import { intl } from '../translations/IntlGlobalProvider';
 import { padding } from '@scality/core-ui/dist/style/theme';
 import NodePartitionTable from './NodePartitionTable';
 import { PORT_NODE_EXPORTER } from '../constants';
+import { useTypedSelector } from '../hooks';
 
 const TitleContainer = styled.div`
   display: flex;
@@ -23,8 +23,10 @@ const NodePagePartitionTab = (props: Object) => {
   const { instanceIP } = props;
 
   // To redirect to the right Node(Detailed) dashboard in Grafana
-  const config = useSelector((state) => state.config);
-  const unameInfos = useSelector((state) => state.app.monitoring.unameInfo);
+  const api = useTypedSelector((state) => state.config.api);
+  const unameInfos = useTypedSelector(
+    (state) => state.app.monitoring.unameInfo,
+  );
   const hostnameLabel = unameInfos.find(
     (unameInfo) =>
       unameInfo?.metric?.instance === `${instanceIP}:${PORT_NODE_EXPORTER}`,
@@ -33,19 +35,21 @@ const NodePagePartitionTab = (props: Object) => {
   return (
     <NodeTab>
       <TitleContainer>
-        <Button
-          text={intl.translate('advanced_metrics')}
-          variant={'base'}
-          onClick={() => {}}
-          icon={<i className="fas fa-external-link-alt" />}
-          size={'small'}
-          // We can't redirect to the Node(detailed) Filesystem Detail catagory.
-          // So we hardcode the panel ID to redirect to 'File Nodes Free' chart
-          href={`${config.api.url_grafana}/dashboard/db/nodes-detailed?var-DS_PROMETHEUS=Prometheus&var-job=node-exporter&var-name=${hostnameLabel}&viewPanel=41`}
-          target="_blank"
-          rel="noopener noreferrer"
-          data-cy="advanced_metrics_node_detailed_file_node_free"
-        />
+        {api && api.url_grafana && (
+          <Button
+            text={intl.translate('advanced_metrics')}
+            variant={'base'}
+            onClick={() => {}}
+            icon={<i className="fas fa-external-link-alt" />}
+            size={'small'}
+            // We can't redirect to the Node(detailed) Filesystem Detail catagory.
+            // So we hardcode the panel ID to redirect to 'File Nodes Free' chart
+            href={`${api.url_grafana}/dashboard/db/nodes-detailed?var-DS_PROMETHEUS=Prometheus&var-job=node-exporter&var-name=${hostnameLabel}&viewPanel=41`}
+            target="_blank"
+            rel="noopener noreferrer"
+            data-cy="advanced_metrics_node_detailed_file_node_free"
+          />
+        )}
       </TitleContainer>
       <NodePartitionTable instanceIP={instanceIP} />
     </NodeTab>
