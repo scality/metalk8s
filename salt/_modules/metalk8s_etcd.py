93514341c1f41aa61e53cdad8296bdbb398aa453
Teddy Andrieux <teddy.andrieux@scality.com>
2021-01-25 10:44:41 +0100

    salt-lint: metalk8s_etcd module fix "blacklisted-module"
    Use six urllib parse module from salt instead of six directly
    ```
    salt/_modules/metalk8s_etcd.py:6: [E9402(blacklisted-module), ] Uses of a blacklisted module 'six.moves.urllib.parse': Please report this error to SaltStack so we can fix it: Trying to import urlparse from six.moves.urllib.parse
    ```

--- salt/_modules/metalk8s_etcd.py
+++ salt/_modules/metalk8s_etcd.py
@@ -3,8 +3,8 @@
 Module for handling etcd client specific calls.
 '''
 import logging
-from six.moves.urllib.parse import urlparse
 
+from salt.ext.six.moves.urllib.parse import urlparse
 from salt.exceptions import CommandExecutionError
 
 PYTHON_ETCD_PRESENT = False
