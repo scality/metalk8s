06d20b123702964c360e2d294fff9a3a6db57e6a
Teddy Andrieux <teddy.andrieux@scality.com>
2021-02-09 15:57:40 +0100

    salt,upgrade: Wait for containers to be running after kubelet upgrade
    After kubelet upgrade all static pod may restart, so we need to wait for
    some "useful" pods to be running before going further in the upgrade
    process.
    NOTE: That before this PR we already sleep 20 seconds, so this commit
    only add a proper check after this sleep
    
    (cherry picked from commit 9ee0c533d4336edabc5832f2351240cd8995c99b)

--- salt/tests/unit/modules/test_cri.py
+++ salt/tests/unit/modules/test_cri.py
@@ -151,29 +151,76 @@ class CriTestCase(TestCase, mixins.LoaderModuleMockMixin):
                     'crictl exec {} my command '.format(stdout_ps)
                 )
 
-    @parameterized.expand([
-        (None, 6, 0, "292c3b07b", True),
-        (None, 6, 0, "", False),
-        (None, 6, 1, "Error occurred", False),
-        ("running", 6, 0, "292c3b07b", True),
-        ("running", 6, 0, "", False),
-        ("running", 6, 1, "Error occurred", False)
-    ])
-    def test_wait_container(self, state, timeout, retcode, stdout, result):
+    @parameterized.expand(
+        [
+            # Success: Found one container
+            (None, 6, 0, "292c3b07b", True),
+            # Failure: Container does not exist
+            (
+                None,
+                6,
+                0,
+                "",
+                'Failed to find container "my_cont": No container found',
+                True,
+            ),
+            # Failure: Error occurred when executing crictl command
+            (
+                None,
+                6,
+                1,
+                "Error occurred",
+                'Failed to find container "my_cont": Error occurred',
+                True,
+            ),
+            # Success: Found one running container
+            ("running", 6, 0, "292c3b07b", True),
+            # Failure: Container does not exist or is not running
+            (
+                "running",
+                6,
+                0,
+                "",
+                'Failed to find container "my_cont" in state "running": No container found',
+                True,
+            ),
+            # Failure: Error occurred when executing crictl command
+            (
+                "running",
+                6,
+                1,
+                "Error occurred",
+                'Failed to find container "my_cont" in state "running": Error occurred',
+                True,
+            ),
+        ]
+    )
+    def test_wait_container(
+        self, state, timeout, retcode, stdout, result, raises=False
+    ):
         """
         Tests the return of `wait_container` function
         """
         cmd = utils.cmd_output(retcode=retcode, stdout=stdout)
         mock_cmd = MagicMock(return_value=cmd)
-        with patch.dict(cri.__salt__, {'cmd.run_all': mock_cmd}), \
-                patch("time.sleep", MagicMock()):
-            self.assertEqual(cri.wait_container(
-                "my_cont",
-                state=state, timeout=timeout
-            ), result)
-            cmd_call = (
-                'crictl ps -q --label io.kubernetes.container.name="my_cont"'
-            )
+
+        with patch.dict(cri.__salt__, {"cmd.run_all": mock_cmd}), patch(
+            "time.sleep", MagicMock()
+        ):
+            if raises:
+                self.assertRaisesRegex(
+                    Exception,
+                    result,
+                    cri.wait_container,
+                    "my_cont",
+                    state=state,
+                    timeout=timeout,
+                )
+            else:
+                self.assertEqual(
+                    cri.wait_container("my_cont", state=state, timeout=timeout), result
+                )
+            cmd_call = 'crictl ps -q --label io.kubernetes.container.name="my_cont"'
             if state:
                 cmd_call += " --state {}".format(state)
             mock_cmd.assert_called_with(cmd_call)
