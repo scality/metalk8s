aa8730d755abb7698c5f9ad05cb9f42f36965f50
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-02-15 10:28:46 +0100

    tools: Move ui-metalk8s-client-generator to tools/crd-client-generator-js and refactor it to be completely uncoupled with metalk8s

---
+++ tools/crd-client-generator-js/src/index.ts
@@ -0,0 +1,15 @@
+import { generateClient } from "./generateCLient";
+
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+
+if (args.length < 2 || args.length > 3) {
+  console.log('usage : ts-node src/index.ts path/to/crd/file.yaml path/to/generated/js/file.js [typePrefix]');
+  process.exit(-1);
+}
+const crdFile = args[0];
+const destinationFile = args[1];
+const typePrefix = args[2];
+
+fs.writeFileSync(destinationFile, generateClient(crdFile, destinationFile, typePrefix));
