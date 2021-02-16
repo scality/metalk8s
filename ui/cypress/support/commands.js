2901568a697ef2252b9fbb564230f8883b53363d
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-02-16 09:57:07 +0100

    ui: Review tests following to solutions navbar integration
    Refs: #3104

--- ui/cypress/support/commands.js
+++ ui/cypress/support/commands.js
@@ -24,6 +24,8 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import 'cypress-wait-until';
+import '@testing-library/cypress/add-commands'
+
 
 import {
   EMPTY_QUERY_RANGE_RESULT,
