9e9f4085d1f786f28bee016625a5ffe33080e457
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-02-24 19:29:41 +0100

    ui: Review tests following to solutions navbar integration

--- ui/cypress/integration/e2e/common/login.js
+++ ui/cypress/integration/e2e/common/login.js
@@ -12,7 +12,7 @@ Given('I am logged in', () => {
   cy.location('pathname').should('eq', '/oidc/auth');
 
   //click "Login with email" button when DEX "alwaysShowLoginScreen" is true
-  cy.get('.theme-form-row button').click();
+  cy.findByText(/Log in with Email/i).click();
 
   const email = Cypress.env('email');
   const userName = Cypress.env('username');
@@ -24,7 +24,7 @@ Given('I am logged in', () => {
   cy.get('button').click();
 
   //Check if the login is successfull and we going back to the UI
-  cy.location('pathname').should('eq', '/oauth2/callback');
+  cy.location('pathname').should('eq', '/');
   cy.server();
   cy.route('POST', '/api/salt/login').as('saltAuthentication');
   const timeOut = {
@@ -33,16 +33,18 @@ Given('I am logged in', () => {
   };
   cy.wait('@saltAuthentication', timeOut);
 
-  cy.get('[data-cy="user_dropdown"] .trigger > .sc-trigger-text').should(
+  cy.findByRole('navigation').should(
     'contain',
     userName,
   );
 });
 
 Then('I log out', () => {
-  cy.get('[data-cy="user_dropdown"] .trigger > .sc-trigger-text').click();
-  cy.get('[data-cy="logout_button"]').click();
-
+  const userName = Cypress.env('username');
+  cy.findByRole('navigation').within(() => {
+    cy.findByText(userName).click();
+    cy.findByText(/log out/i).click();
+  })
   //Check if we are redirected to the DEX login page
   cy.location('pathname').should('eq', '/oidc/auth');
 });
