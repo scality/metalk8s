78aae391f7ffb0ea73c0ef8c17c1070e01b692ef
Guillaume Demonet <guillaume.demonet@scality.com>
2021-03-03 19:50:40 +0100

    salt/tests: List all Jinja templates
    We expose, and use, a `template_path` fixture, which is parameterized to
    generate one test case per Jinja template in the salt/ directory.
    
    Running `tox -e unit-tests -- -m formulas` shows:
    - 93 passed
    - 143 xfailed
    
    Interestingly, this means more than a third of the `.sls`/`.sls.in`/...
    files are "simple" (or empty) Jinja templates, which don't require any
    available context to render (e.g. many `init.sls` are only containing an
    `include` list, without rendering logic).
    
    See: #3073

--- salt/tests/unit/formulas/conftest.py
+++ salt/tests/unit/formulas/conftest.py
@@ -3,5 +3,6 @@
 # pylint: disable=unused-import
 from tests.unit.formulas.fixtures.environment import fixture_template_loader
 from tests.unit.formulas.fixtures.environment import fixture_environment
+from tests.unit.formulas.fixtures.templates import fixture_template_path
 
 # pylint: enable=unused-import
