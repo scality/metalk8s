eb60b7e2733eed8c0ab694cddedf9bd381022211
Guillaume Demonet <guillaume.demonet@scality.com>
2021-03-03 23:15:16 +0100

    salt/tests: Enable rendering context configuration
    A configuration file, at `salt/tests/unit/formulas/config.yaml`,
    describes which templates to validate, and how.
    This commit introduces the logic for parameterizing the rendering
    context through configuration options, and demonstrates its most minimal
    usage by skipping all formulas under "metalk8s" directory.
    
    Running `tox -e unit-tests -- -m formulas` shows:
    - 235 skipped
    - 1 xfailed (the only formula which doesn't live under metalk8s/, which
    is top.sls.in)
    
    See: #3073

--- salt/tests/unit/formulas/conftest.py
+++ salt/tests/unit/formulas/conftest.py
@@ -1,6 +1,7 @@
 """Import fixture definitions for use in tests."""
 
 # pylint: disable=unused-import
+from tests.unit.formulas.fixtures.context import fixture_render_contexts
 from tests.unit.formulas.fixtures.environment import fixture_template_loader
 from tests.unit.formulas.fixtures.environment import fixture_environment
 from tests.unit.formulas.fixtures.templates import fixture_template_path
--- salt/tests/unit/formulas/test_formulas.py
+++ salt/tests/unit/formulas/test_formulas.py
@@ -4,16 +4,27 @@ Currently only define a single test function, focused on rendering any Jinja tem
 available under the salt/ directory.
 """
 from pathlib import Path
+from typing import Iterable
 
 import jinja2
 import pytest
 
+from tests.unit.formulas.fixtures.context import Context
+
 
 @pytest.mark.formulas
-def test_template_renders(environment: jinja2.Environment, template_path: Path) -> None:
+def test_template_renders(
+    environment: jinja2.Environment,
+    render_contexts: Iterable[Context],
+    template_path: Path,
+) -> None:
     """Check that a Jinja template can render without errors."""
     template = environment.get_template(str(template_path))
-    try:
-        template.render()
-    except jinja2.exceptions.TemplateError as exc:
-        pytest.xfail(f"Cannot render {template_path}: {exc!r}")
+
+    for context in render_contexts:
+        try:
+            template.render(**context.data)
+        except jinja2.exceptions.TemplateError as exc:
+            pytest.xfail(
+                f"Cannot render {template_path} with context '{context.id}': {exc!r}"
+            )
