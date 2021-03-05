4792c69394c209fb8bd207188009c67bb3bf45d4
Guillaume Demonet <guillaume.demonet@scality.com>
2021-03-05 01:35:40 +0100

    salt/tests: Check rendering for metalk8s.volumes.*
    Add a new "volumes" configuration option to control the
    metalk8s:volumes" pillar using simple aliases.
    
    Running `tox -e unit-tests -- -m formulas` shows:
    - 141 passed
    - 95 skipped
    
    See: #3073

--- salt/tests/unit/formulas/fixtures/salt.py
+++ salt/tests/unit/formulas/fixtures/salt.py
@@ -272,6 +272,7 @@ def pillar_get(salt_mock: SaltMock, key: str) -> Any:
 
 register_basic("file.join")(lambda *args: "/".join(args))
 register_basic("hashutil.base64_b64decode")(lambda input_data: input_data)
+register_basic("log.warning")(print)
 register_basic("metalk8s.format_san")(", ".join)
 
 # Static values for these IPs should be sufficient for rendering.
