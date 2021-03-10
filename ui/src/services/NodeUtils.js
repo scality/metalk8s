7818cd9e6855b79b94736897ad7283e009a2afda
YanJin <yanjin.cheng@scality.com>
2021-03-10 13:02:32 +0100

    ui/nodeUtils: Fix the comments to

--- ui/src/services/NodeUtils.js
+++ ui/src/services/NodeUtils.js
@@ -65,11 +65,11 @@ export const getNodeListData = createSelector(
         }
 
         const computedStatus = [];
-        // The rules of the color of the node status
-        // "green" when status.conditions['Ready'] == True and all other conditions are false
-        // "yellow" when status.conditions['Ready'] == True and some other conditions are true
-        // "red" when status.conditions['Ready'] == False
-        // "grey" when there is no status.conditions
+        /*  The rules of the color of the node status
+         <green>  when status.conditions['Ready'] == True and all other conditions are false
+         <yellow> when status.conditions['Ready'] == True and some other conditions are true
+         <red>    when status.conditions['Ready'] == False
+         <grey>   when there is no status.conditions */
         if (node?.status === API_STATUS_READY && conditions.length === 0) {
           statusTextColor = brand?.healthy;
           computedStatus.push(API_STATUS_READY);
@@ -124,22 +124,23 @@ export const getNodeListData = createSelector(
   },
 );
 
-// This function returns the IP and interface of Control Plane and Workload Plane for each Node
-// Arguments:
-//  ipsInterfacesObject =
-// {
-//    ip_interface: {
-//      eth1:['10.0.1.42', 'fe80::f816:3eff:fe25:5843'],
-//      eth3:['10.100.0.2', 'fe80::f816:3eff:fe37:2f34']
-// },
-//    metalk8s:control_plane_ip: "10.0.1.42",
-//    metalk8s:workload_plane_ip: "10.100.0.2"
-// }
-// Return
-// {
-//   controlPlane: { ip: '10.0.1.42', interface: 'eth1'}
-//   workloadPlane: { ip: '10.100.0.2', interface: 'eth3'},
-// }
+/*
+This function returns the IP and interface of Control Plane and Workload Plane for each Node
+Arguments:
+  ipsInterfacesObject = {
+    ip_interface: {
+      eth1:['10.0.1.42', 'fe80::f816:3eff:fe25:5843'],
+      eth3:['10.100.0.2', 'fe80::f816:3eff:fe37:2f34']
+   },
+    metalk8s:control_plane_ip: "10.0.1.42",
+    metalk8s:workload_plane_ip: "10.100.0.2"
+  }
+Return
+  {
+   controlPlane: { ip: '10.0.1.42', interface: 'eth1'}
+   workloadPlane: { ip: '10.100.0.2', interface: 'eth3'},
+  }
+*/
 export const nodesCPWPIPsInterface = (
   IPsInterfacesObject: IPInterfaces | boolean,
 ): {
