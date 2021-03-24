4654fd7212928ba8296b6a94c0e36db24ff8c2fd
Alexandre Allard <alexandre.allard@scality.com>
2021-03-24 11:21:47 +0100

    build: Display the time elapsed for tasks
    Each task not skipped or ignored is now print
    2 times when started and ended, whether it succeed
    or not.
    This is necessary to display the time elapsed
    even with multithreaded builds.
    Also change the status of each step to more
    human readable status and colors.
    
    Refs: #3217

--- buildchain/dodo.py
+++ buildchain/dodo.py
@@ -31,18 +31,26 @@ from buildchain.vagrant import *
 class CustomReporter(doit.reporter.JsonReporter):  # type: ignore
     """A custom reporter that display a JSON object for each task."""
 
+    tag = {
+        "started": "\033[1;33mSTARTED\033[0m  ",
+        "success": "\033[1;32mSUCCESS\033[0m  ",
+        "failed": "\033[1;31mFAILED\033[0m   ",
+        "skipped": "\033[1;34mSKIPPED\033[0m  ",
+        "ignored": "\033[1;35mIGNORED\033[0m  ",
+    }
     desc = "console, display the execution of each task, generate a build log"
 
     def __init__(self, outstream: TextIO, options: Dict[str, Any]):
         super().__init__(outstream, options)
         self.failures: List[Dict[str, Any]] = []
         self.runtime_errors: List[str] = []
+        self.failure_verbosity: int = options.get("failure_verbosity", 0)
 
     def execute_task(self, task: doit.task.Task) -> None:
         """Called when a task is executed."""
         super().execute_task(task)
         if task.actions:  # Ignore tasks that do not define actions.
-            self._write(".  {}\n".format(task.title()))
+            self._write("{}{}\n".format(self.tag["started"], task.title()))
 
     def add_failure(
         self, task: doit.task.Task, exception: doit.exceptions.CatchedException
@@ -51,16 +59,35 @@ class CustomReporter(doit.reporter.JsonReporter):  # type: ignore
         super().add_failure(task, exception)
         result = {"task": task, "exception": exception}
         self.failures.append(result)
+        if task.actions:
+            time_elapsed = self.t_results[task.name].to_dict()["elapsed"] or 0
+            self._write(
+                "{}{} [{:.0f}s]\n".format(
+                    self.tag["failed"], task.title(), time_elapsed
+                )
+            )
+            self._write_failure(result)
+
+    def add_success(self, task: doit.task.Task) -> None:
+        """Called when execution finishes successfully"""
+        super().add_success(task)
+        if task.actions:
+            time_elapsed = self.t_results[task.name].to_dict()["elapsed"]
+            self._write(
+                "{}{} [{:.0f}s]\n".format(
+                    self.tag["success"], task.title(), time_elapsed
+                )
+            )
 
     def skip_uptodate(self, task: doit.task.Task) -> None:
         """Called when a task is skipped (up-to-date)."""
         super().skip_uptodate(task)
-        self._write("-- {}\n".format(task.title()))
+        self._write("{}{}\n".format(self.tag["skipped"], task.title()))
 
     def skip_ignore(self, task: doit.task.Task) -> None:
         """Called when a task is skipped (ignored)."""
         super().skip_ignore(task)
-        self._write("!! {}\n".format(task.title()))
+        self._write("{}{}\n".format(self.tag["ignored"], task.title()))
 
     def cleanup_error(self, exception: doit.exceptions.CatchedException) -> None:
         """Error during cleanup."""
@@ -80,13 +107,16 @@ class CustomReporter(doit.reporter.JsonReporter):  # type: ignore
             # Makes no sense to print output if task was not executed.
             if not task.executed:
                 continue
-            self._write(failure_header)
-            self._write_failure(result)
-            err = "".join([action.err for action in task.actions if action.err])
-            if err:
+            show_err = task.verbosity < 1 or self.failure_verbosity > 0
+            show_out = task.verbosity < 2 or self.failure_verbosity == 2
+            if show_err or show_out:
+                self._write(failure_header)
+            if show_err:
+                self._write_failure(result)
+                err = "".join([action.err for action in task.actions if action.err])
                 self._write("{} <stderr>:\n{}\n".format(task.name, err))
-            out = "".join([action.out for action in task.actions if action.out])
-            if out:
+            if show_out:
+                out = "".join([action.out for action in task.actions if action.out])
                 self._write("{} <stdout>:\n{}\n".format(task.name, out))
 
         if self.runtime_errors:
@@ -94,6 +124,7 @@ class CustomReporter(doit.reporter.JsonReporter):  # type: ignore
             self._write("Execution aborted.\n")
             self._write("\n".join(self.runtime_errors))
             self._write("\n")
+
         # Generate the build log.
         build_log = constants.ROOT / "build.log"
         with build_log.open("w", encoding="utf-8") as fp:
