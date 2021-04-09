f43115174f921957c60c838c61f1b442ba03af48
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-04-09 12:09:59 +0200

    shell-ui: Swith whatwg-fetch to node-fetch to polyfill fetch in node (refs: https://github.com/mswjs/msw/issues/448)

--- shell-ui/src/setupTests.js
+++ shell-ui/src/setupTests.js
@@ -1,4 +1,4 @@
-import 'whatwg-fetch';
+import fetch from 'node-fetch';
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-localstorage-mock';
@@ -9,3 +9,8 @@ window.crypto = {
     return nodeCrypto.randomFillSync(buffer);
   },
 };
+
+window.fetch = (url, ...rest) => fetch(
+	/^https?:/.test(url) ? url : new URL(url, 'http://localhost'),
+	...rest
+);
