55b67f445b1daaf4f0fa3a099e6b8960a576f034
YanJin <yanjin.cheng@scality.com>
2021-04-23 21:28:22 +0200

    ui: Add error auth page if not authenticate in PrivateRoute
    Refs: #3266

--- ui/src/containers/PrivateRoute.js
+++ ui/src/containers/PrivateRoute.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
+import { ErrorPageAuth } from '@scality/core-ui';
+import { useTypedSelector } from '../hooks';
 
-const PrivateRoute = ({ component, ...rest }) => {
+const PrivateRoute = ({ component, location, ...rest }) => {
+  const { language, api } = useTypedSelector((state) => state.config);
+  const url_support = api?.url_support;
   const authenticated = useSelector((state) => !!state.oidc.user);
 
-  if (!authenticated) {
-    return <>You are not authenticated</>;//todo display a nicer message here
-  } else {
+  if (authenticated) {
     return <Route {...rest} component={component} />;
+  } else {
+    return (
+      <ErrorPageAuth
+        data-cy="sc-error-pageauth"
+        supportLink={url_support}
+        locale={language}
+      />
+    );
   }
 };
 
