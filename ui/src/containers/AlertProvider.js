d7ceb35b59fb887cd8a8be14aa90c64dfa2b8107
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-06-04 20:12:26 +0200

    ui: Federate AlertProvider from shell-ui instead of using window attached library (WIP)

--- ui/src/containers/AlertProvider.js
+++ ui/src/containers/AlertProvider.js
@@ -1,104 +1,68 @@
 //@flow
-import React, {
-  type Node,
-  useLayoutEffect,
-  createContext,
-  useContext,
-  useState,
-  useRef,
-} from 'react';
-import { useQuery } from 'react-query';
+import React, { type Node } from 'react';
 import { useTypedSelector } from '../hooks';
 import { ErrorBoundary } from 'react-error-boundary';
 import type { FilterLabels } from '../services/alertUtils';
 import { ErrorPage500 } from '@scality/core-ui';
+import { useTheme } from 'styled-components';
+// import { FederatedComponent } from '../ModuleFederation';
 
 export const useAlerts = (filters: FilterLabels) => {
-  const alertsVersion = useTypedSelector(
-    (state) => state.config.api?.alerts_lib_version,
-  );
-
-  if (window.shellUIAlerts && window.shellUIAlerts[alertsVersion]) {
-    return window.shellUIAlerts[alertsVersion].useAlerts(useContext)(filters);
-  }
+  //return alertGlobal.hooks.useAlerts(filters);
+  return {alerts: []}
 };
 
-/**
- * This hook dynamically load Alert library.
- *
- * It first creates a script entry if none exists referring the given library src
- *  and then initiates the library by calling `createAlertContext`
- *
- * @param {string} src library bundle url
- * @param {string?} version library version
- */
-function useAlertLibrary(src?: string, version?: string) {
-  const [hasFailed, setHasFailed] = useState(false);
-  useLayoutEffect(() => {
-    const body = document.body;
-    // $flow-disable-line
-    const element = [...(body?.querySelectorAll('script') || [])].find(
-      // $flow-disable-line
-      (scriptElement) => scriptElement.attributes.src?.value === src,
-    );
+const InternalAlertProvider = ({
+  moduleExports,
+  children,
+}: {
+  moduleExports: {},
+  children: Node,
+}): Node => {
+  //alertGlobal.hooks = moduleExports['./alerts/alertHooks'];
 
-    if (!element && body && src && version) {
-      const scriptElement = document.createElement('script');
-      scriptElement.src = src;
-      scriptElement.onerror = () => {
-        setHasFailed(true);
-      };
-      scriptElement.onload = () => {
-        window.shellUIAlerts[version].createAlertContext(createContext)
-      }
-      body.appendChild(scriptElement);
-    }
-  }, [src, version]);
+  console.log(useTheme())
 
-  if (hasFailed) {
-    throw new Error(`Failed to load library ${src || ''}`);
-  }
-}
+  return <>{children}</>
 
-const InternalAlertProvider = ({ children }: { children: Node }): Node => {
-  const alertsUrl = useTypedSelector((state) => state.config.api?.url_alerts);
-  const alertsVersion = useTypedSelector(
-    (state) => state.config.api?.alerts_lib_version,
-  );
-  const alertManagerUrl = useTypedSelector(
-    (state) => state.config.api?.url_alertmanager,
-  );
-
-  useAlertLibrary(alertsUrl, alertsVersion);
-
-  const AlertProviderRef = useRef(null);
-
-  // In order to avoid react to uselessly rerender children every time we move
-  // the AlertProvider to a ref and renders it only a single time once the library is ready.
-  if (window.shellUIAlerts && window.shellUIAlerts[alertsVersion] && !AlertProviderRef.current) {
-    AlertProviderRef.current = window.shellUIAlerts[alertsVersion].AlertProvider(useQuery);
-  }
-
-  if (AlertProviderRef.current) {
-    return (
-      <AlertProviderRef.current alertManagerUrl={alertManagerUrl}>
-        {children}
-      </AlertProviderRef.current>
-    );
-  }
-
-  return <>{children}</>;
+  // const alertManagerUrl = useTypedSelector(
+  //   (state) => state.config.api.alertManagerUrl,
+  // );
+  // return (
+  //   <FederatedComponent
+  //     module={'./alerts/AlertProvider'}//TODO find a way to inject those values
+  //     scope={'shell'}
+  //     url={'http://localhost:8084/shell/remoteEntry.js'}
+  //     alertManagerUrl={alertManagerUrl}
+  //   >
+  //     {children}
+  //   </FederatedComponent>
+  // );
 };
 
 function ErrorFallback() {
   const { language, api } = useTypedSelector((state) => state.config);
   const url_support = api?.url_support;
-  return <ErrorPage500 data-cy='sc-error-page500' locale={ language } supportLink={ url_support }/>;
+  return (
+    <ErrorPage500
+      data-cy="sc-error-page500"
+      locale={language}
+      supportLink={url_support}
+    />
+  );
 }
 
 const AlertProvider = ({ children }: { children: Node }): Node => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
+      {/* <ComponentWithLazyHook
+        componentWithInjectedHook={InternalAlertProvider}
+        renderOnError={<ErrorPage500 />}
+        remoteEntryUrl={'http://localhost:8084/shell/remoteEntry.js'} //TODO find a way to inject those values
+        federatedModule={'./alerts/alertHooks'}
+        moduleFederationScope={'shell'}
+        componentProps={{ children }}
+      /> */}
       <InternalAlertProvider>{children}</InternalAlertProvider>
     </ErrorBoundary>
   );
