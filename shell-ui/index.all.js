b087f96ef039e99777e7c54714c30d61a235a5cf
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-06-21 11:24:39 +0200

    shell: Add an index file to shell-ui to debug remote entry

---
+++ shell-ui/index.all.js
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom';
+import 'regenerator-runtime/runtime';
+import "@fortawesome/fontawesome-free/js/all.js";
+
+import NavbarMdx from './src/navbar/index.mdx';
+import { FederatedComponent } from './src/ModuleFederation';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+ReactDOM.render(<>
+    <QueryClientProvider client={new QueryClient()}>
+    <FederatedComponent
+      module={'./alerts/AlertProvider'}//TODO find a way to inject those values
+      scope={'shell'}
+      url={'http://localhost:8084/shell/remoteEntry.js'}
+      props={{alertManagerUrl:'http://toto/alertmanager', children: 'hello'}}
+    >
+    </FederatedComponent>
+    </QueryClientProvider>
+</>, document.getElementById('app'));
