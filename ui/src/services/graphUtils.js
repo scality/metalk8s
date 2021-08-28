7b0fa16b6aeeaed7889aec8aed6ebd2df4bff9b4
YanJin <yanjin.cheng@scality.com>
2021-08-28 12:11:00 +0200

    ui/dashboard: Add logic to prepare the series

--- ui/src/services/graphUtils.js
+++ ui/src/services/graphUtils.js
@@ -1,45 +1,58 @@
-import { fromUnixTimestampToDate } from './utils';
+//import { fromUnixTimestampToDate } from './utils';
 import type {
   PrometheusQueryResult,
   RangeMatrixResult,
 } from './prometheus/api';
-
+import { PORT_NODE_EXPORTER } from '../constants';
+export type Series = {
+  label: string,
+  data: { timestamp: number, value: number }[],
+}[];
 export type FormattedChartNodePromRange = {
   date: Date,
   type: string,
   y: string,
 };
 export type FormattedChartNodesPromRange = Array<FormattedChartNodePromRange>;
+
+// Format the prometheus data to Series
+// if the graph is in loading status, the
 export const formatNodesPromRangeForChart = (
-  result: Promise<PrometheusQueryResult>[],
+  results: Promise<PrometheusQueryResult>[],
   nodes: Array<{ internalIP: string, name: string }>,
-): FormattedChartNodesPromRange => {
-  const reduced = nodes.reduce((acc, node, index) => {
-    let temp = [];
-    if (
-      result[index] &&
-      result[index].status === 'success' &&
-      result[index].data.result[0] &&
-      result[index].data.result[0].values.length
-    ) {
-      const matrixResult: RangeMatrixResult = result[index].data;
-      temp = matrixResult.result[0].values.map((item) => ({
-        date: fromUnixTimestampToDate(item[0]),
-        type: node.name,
-        y: item[1],
-      }));
-    }
-    return acc.concat(temp);
-  }, []);
-  return reduced;
+  isLoading: boolean,
+): Series => {
+  return nodes.map((node, index) => {
+    const internalIP = node.internalIP;
+    const matrixResult: RangeMatrixResult =
+      results?.find(
+        (i) =>
+          i?.data?.result[0]?.metric?.instance ===
+          `${internalIP}:${PORT_NODE_EXPORTER}`,
+      )?.data?.result[0] || results[index]?.data.result[0]; // for the memory, the metric field is empty :(
+
+    const prometheusData = matrixResult?.values; //[number,string][] => {number,string}[] // no need any more
+
+    return {
+      data: prometheusData,
+      resource: node.name,
+      getTooltipLabel: (_, resource: node.name) => {
+        return node.name;
+      },
+      getLegendLabel: (_, resource: node.name) => {
+        return node.name;
+      },
+    };
+  });
 };
 
+// Format prometheus data to read/write format
 export const formatNodesThroughputPromRangeForChart = (
-  result: Promise<PrometheusQueryResult>[],
+  results: Promise<PrometheusQueryResult>[],
   nodes: Array<{ internalIP: string, name: string }>,
-): FormattedChartNodesPromRange => {
-  const readRes = result[0];
-  const writeRes = result[1];
+): Series => {
+  const readRes = results[0];
+  const writeRes = results[1];
 
   if (
     readRes &&
@@ -47,34 +60,38 @@ export const formatNodesThroughputPromRangeForChart = (
     writeRes &&
     writeRes.status === 'success'
   ) {
-    const reduced = nodes.reduce((acc, node) => {
-      let tempRead = [];
-      let tempWrite = [];
-
-      const nodeReadData = readRes.data?.result?.find(
+    const tempRead = nodes.map((node) => {
+      const nodeReadData = readRes?.data?.result?.find(
         (item) => item.metric?.instance.split(':')[0] === node.internalIP,
       );
-      const nodeWriteData = writeRes.data?.result?.find(
+      const prometheusReadData = nodeReadData?.values;
+
+      return {
+        metricPrefix: 'read',
+        data: prometheusReadData,
+        resource: node.name,
+        getTooltipLabel: (metricPrefix: 'read', resource: node.name) => {
+          return `${resource}-${metricPrefix}`;
+        },
+      };
+    });
+
+    const tempWrite = nodes.map((node) => {
+      const nodeWriteData = writeRes?.data?.result?.find(
         (item) => item.metric?.instance.split(':')[0] === node.internalIP,
       );
 
-      if (nodeReadData)
-        tempRead = nodeReadData.values.map((item) => ({
-          date: fromUnixTimestampToDate(item[0]),
-          type: `${node.name}-read`,
-          y: 0 - item[1],
-        }));
-      if (nodeWriteData)
-        tempWrite = nodeWriteData.values.map((item) => ({
-          date: fromUnixTimestampToDate(item[0]),
-          type: `${node.name}-write`,
-          y: item[1],
-        }));
+      const prometheusWriteData = nodeWriteData?.values;
+      return {
+        metricPrefix: 'write',
+        data: prometheusWriteData,
+        resource: node.name,
+        getTooltipLabel: (metricPrefix: 'read', resource: node.name) => {
+          return `${resource}-${metricPrefix}`;
+        },
+      };
+    });
 
-      return acc.concat(tempRead).concat(tempWrite);
-    }, []);
-    return reduced;
+    return tempRead.concat(tempWrite);
   }
-
-  return null;
 };
--- ui/src/services/prometheus/fetchMetrics.js
+++ ui/src/services/prometheus/fetchMetrics.js
@@ -1,9 +1,7 @@
 // @flow
 import { queryPrometheus, queryPrometheusRange } from './api';
-import { PORT_NODE_EXPORTER, queryTimeSpansCodes } from '../../constants';
-
+import { PORT_NODE_EXPORTER } from '../../constants';
 import type { PrometheusQueryResult } from './api';
-import type { MetricsTimeSpan } from '../../hooks';
 
 export function queryNodeFSUsage(
   instanceIP: string,
@@ -33,35 +31,13 @@ export function queryNodeFSSize(
   });
 }
 
-const getMetricsTimeValues = (
-  timeSpan: MetricsTimeSpan,
-): {
-  startingTimeISO: string,
-  currentTimeISO: string,
-  sampleFrequency: number,
-} => {
-  const sampleFrequency: number =
-    queryTimeSpansCodes.find((item) => item.duration === timeSpan)?.frequency ||
-    0;
-
-  const currentTime = new Date();
-  const currentTimeISO = currentTime.toISOString(); // To query Prometheus the date should follow `RFC3339` format
-  const startingTimestamp = Math.round(currentTime.getTime() / 1000) - timeSpan;
-  const startingTimeISO = new Date(startingTimestamp * 1000).toISOString();
-
-  return { startingTimeISO, currentTimeISO, sampleFrequency };
-};
-
 export function queryNodeCPUMetrics(
   instanceIP: string,
-  timeSpan: MetricsTimeSpan,
+  sampleFrequency: number,
+  startingTimeISO: string,
+  currentTimeISO: string,
 ): Promise<PrometheusQueryResult> {
   const cpuUsageQuery = `100 - (avg by (instance) (irate(node_cpu_seconds_total{mode="idle",instance=~"${instanceIP}:${PORT_NODE_EXPORTER}"}[5m])) * 100)`;
-  const {
-    startingTimeISO,
-    currentTimeISO,
-    sampleFrequency,
-  } = getMetricsTimeValues(timeSpan);
 
   return queryPrometheusRange(
     startingTimeISO,
@@ -78,14 +54,11 @@ export function queryNodeCPUMetrics(
 
 export function queryNodeMemoryMetrics(
   instanceIP: string,
-  timeSpan: MetricsTimeSpan,
+  sampleFrequency: number,
+  startingTimeISO: string,
+  currentTimeISO: string,
 ): ?Promise<PrometheusQueryResult> {
   const memoryQuery = `sum(100 - ((node_memory_MemAvailable_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}"} * 100) / node_memory_MemTotal_bytes{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}"}))`;
-  const {
-    startingTimeISO,
-    currentTimeISO,
-    sampleFrequency,
-  } = getMetricsTimeValues(timeSpan);
   return queryPrometheusRange(
     startingTimeISO,
     currentTimeISO,
@@ -101,14 +74,11 @@ export function queryNodeMemoryMetrics(
 
 export function queryNodeLoadMetrics(
   instanceIP: string,
-  timeSpan: MetricsTimeSpan,
+  sampleFrequency: number,
+  startingTimeISO: string,
+  currentTimeISO: string,
 ): ?Promise<PrometheusQueryResult> {
   const systemLoadQuery = `avg(node_load1{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}"}) / count(count(node_cpu_seconds_total{instance=~"${instanceIP}:${PORT_NODE_EXPORTER}"}) by (cpu)) * 100`;
-  const {
-    startingTimeISO,
-    currentTimeISO,
-    sampleFrequency,
-  } = getMetricsTimeValues(timeSpan);
 
   return queryPrometheusRange(
     startingTimeISO,
@@ -123,15 +93,12 @@ export function queryNodeLoadMetrics(
   });
 }
 
-export function queryThroughputRead(
-  timeSpan: MetricsTimeSpan,
+export function queryThroughputReadAllInstances(
+  sampleFrequency: number,
+  startingTimeISO: string,
+  currentTimeISO: string,
 ): Promise<PrometheusQueryResult> {
-  const nodeThroughputReadQuery = `sum(sum(irate(node_disk_read_bytes_total[1m])) by (instance, device) * 0.000001) by(instance)`;
-  const {
-    startingTimeISO,
-    currentTimeISO,
-    sampleFrequency,
-  } = getMetricsTimeValues(timeSpan);
+  const nodeThroughputReadQuery = `sum(sum(irate(node_disk_read_bytes_total[1m])) by (instance, device))by(instance)`;
 
   return queryPrometheusRange(
     startingTimeISO,
@@ -146,15 +113,12 @@ export function queryThroughputRead(
   });
 }
 
-export function queryThroughputWrite(
-  timeSpan: MetricsTimeSpan,
+export function queryThroughputWriteAllInstances(
+  sampleFrequency: number,
+  startingTimeISO: string,
+  currentTimeISO: string,
 ): Promise<PrometheusQueryResult> {
-  const nodeThroughputWriteQuery = `sum(sum(irate(node_disk_written_bytes_total[1m])) by (instance, device) * 0.000001)by(instance)`;
-  const {
-    startingTimeISO,
-    currentTimeISO,
-    sampleFrequency,
-  } = getMetricsTimeValues(timeSpan);
+  const nodeThroughputWriteQuery = `sum(sum(irate(node_disk_written_bytes_total[1m])) by (instance, device))by(instance)`;
 
   return queryPrometheusRange(
     startingTimeISO,
