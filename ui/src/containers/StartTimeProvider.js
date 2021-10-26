805bce2c7679ba3057a9848a3904c5b260e1a9ed
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-10-26 15:46:45 +0200

    ui: Backport refresh chart issues

--- ui/src/containers/StartTimeProvider.js
+++ ui/src/containers/StartTimeProvider.js
@@ -3,8 +3,8 @@ import React, {
   createContext,
   useContext,
   type Node,
-  useState,
   useCallback,
+  useState,
 } from 'react';
 import { useMetricsTimeSpan } from '@scality/core-ui/dist/next';
 import { REFRESH_METRICS_GRAPH } from '../constants';
@@ -20,31 +20,39 @@ export const useStartingTimeStamp = (): {
   startingTimeISO: string,
   currentTimeISO: string,
 } => {
-  const { startingTimeISO, currentTimeISO } = useContext(StartTimeContext);
-  if (!startingTimeISO || !currentTimeISO) {
+  const startTimeContext = useContext(StartTimeContext);
+  if (!startTimeContext) {
     throw new Error(
       'The useStartingTimeStamp hook can only be used within StartTimeProvider.',
     );
   }
-  return { startingTimeISO, currentTimeISO };
+  return startTimeContext;
 };
 
 const StartTimeProvider = ({ children }: { children: Node }) => {
-  const { duration } = useMetricsTimeSpan();
+  const { duration, frequency } = useMetricsTimeSpan();
 
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(() => {
+    const newCurrentDate = new Date().getTime();
+    return newCurrentDate - (newCurrentDate % (frequency * 1000));
+  });
 
   const [startingTimeISO, setStartingTimeISO] = useState(
     new Date((currentTime / 1000 - duration) * 1000).toISOString(),
   );
 
   const updateCurrentTime = useCallback(() => {
-    const newCurrentDate = new Date();
-    setCurrentTime(newCurrentDate);
+    const newCurrentDate = new Date().getTime();
+    //In order to always display the same data on the charts over refresh and new entroes comming
+    //we round start and end time to frequency factors. Hence for example for a 30 seconds fequency
+    //we will rount current time and start time to the previous 30 seconds factor (00, or 30 seconds for each minute)
+    const newCurrentTime =
+      newCurrentDate - (newCurrentDate % (frequency * 1000));
+    setCurrentTime(newCurrentTime);
     setStartingTimeISO(
-      new Date((newCurrentDate / 1000 - duration) * 1000).toISOString(),
+      new Date((newCurrentTime / 1000 - duration) * 1000).toISOString(),
     );
-  }, [duration]);
+  }, [duration, frequency]);
 
   useMemo(() => {
     updateCurrentTime();
@@ -62,7 +70,10 @@ const StartTimeProvider = ({ children }: { children: Node }) => {
 
   return (
     <StartTimeContext.Provider
-      value={{ startingTimeISO, currentTimeISO: currentTime.toISOString() }}
+      value={{
+        startingTimeISO,
+        currentTimeISO: new Date(currentTime).toISOString(),
+      }}
     >
       {children}
     </StartTimeContext.Provider>
