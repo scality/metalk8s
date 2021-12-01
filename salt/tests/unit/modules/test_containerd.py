53270c0d349e62f2ed9450673180b31ca9ab8f13
Teddy Andrieux <teddy.andrieux@scality.com>
2021-12-01 17:20:29 +0100

    salt: Add ability to set index name for imported image with `ctr`

--- salt/tests/unit/modules/test_containerd.py
+++ salt/tests/unit/modules/test_containerd.py
@@ -39,3 +39,27 @@ class ContainerdTestCase(TestCase, mixins.LoaderModuleMockMixin):
             mock_cmd.assert_called_once_with(
                 'ctr --debug -n k8s.io image import "{}"'.format(path)
             )
+
+    def test_load_cri_image_with_fullname(self):
+        """
+        Tests the return of `load_cri_image` function
+        """
+        cmd = utils.cmd_output(
+            stderr='time="2020-07-02T08:02:46Z" '
+            'level=debug msg="unpacking 1 images"',
+            stdout="unpacking abc.def/my-image:3.5 (sha256:3efe4ff64c93123e"
+            "1217b0ad6d23b4c87a1fc2109afeff55d2f27d70c55d8f73)...done\n"
+            "unpacking k8s.gcr.io/my-image:3.5 (sha256:3efe4ff64c93123e"
+            "1217b0ad6d23b4c87a1fc2109afeff55d2f27d70c55d8f73)...done",
+        )
+        mock_cmd = MagicMock(return_value=cmd)
+        with patch.dict(containerd.__salt__, {"cmd.run_all": mock_cmd}):
+            self.assertEqual(
+                containerd.load_cri_image(
+                    "/tmp/toto.tar", fullname="abc.def/my-image:3.5"
+                ),
+                cmd,
+            )
+            mock_cmd.assert_called_once_with(
+                'ctr --debug -n k8s.io image import "/tmp/toto.tar" --index-name "abc.def/my-image:3.5"'
+            )
