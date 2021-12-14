ec9482bacb05deb8cef5a9efd9a8ae500822ee08
YanJin <yanjin.cheng@scality.com>
2021-12-14 16:57:41 +0100

    ui: Add Error reducer to store the isAuthError

---
+++ ui/src/ducks/app/authError.js
@@ -0,0 +1,29 @@
+//@flow
+export type AuthErrorState = {
+  isAuthError: boolean,
+};
+
+type AuthErrorAction = {
+  type: 'AUTH_ERROR',
+};
+
+const defaultState: AuthErrorState = {
+  isAuthError: false,
+};
+
+export default function reducer(
+  state: AuthErrorState = defaultState,
+  action: AuthErrorAction,
+) {
+  switch (action.type) {
+    case 'AUTH_ERROR': {
+      return { ...state, isAuthError: true };
+    }
+    default:
+      return { ...state };
+  }
+}
+
+export function authErrorAction() {
+  return { type: 'AUTH_ERROR' };
+}
