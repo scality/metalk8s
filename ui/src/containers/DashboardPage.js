6ea65116e8515397cd0176d873f96a6e7ea4f1b3
YanJin <yanjin.cheng@scality.com>
2022-11-07 12:00:22 +0100

    ui: refactor the layout in dashboard page

--- ui/src/containers/DashboardPage.js
+++ ui/src/containers/DashboardPage.js
@@ -5,18 +5,16 @@ import DashboardMetrics from '../components/DashboardMetrics';
 import DashboardInventory from '../components/DashboardInventory';
 import DashboardServices from '../components/DashboardServices';
 import DashboardGlobalHealth from '../components/DashboardGlobalHealth';
-import { padding, spacing } from '@scality/core-ui/dist/style/theme';
+import { spacing } from '@scality/core-ui/dist/style/theme';
 import { SyncedCursorCharts } from '@scality/core-ui/dist/components/vegachartv2/SyncedCursorCharts';
 import TimespanSelector from './TimespanSelector';
 import DashboardNetwork from '../components/DashboardNetwork';
-import { AppContainer } from '@scality/core-ui';
+import { AppContainer, Wrap } from '@scality/core-ui';
 
 const DashboardGrid = styled.div`
   display: grid;
   gap: ${AppContainer.sectionDistance};
   grid-template:
-    'header header header header header' 3rem
-    'health     health  health  health  health' 6.1rem
     'inventory  network network metrics metrics' auto
     / 1fr 1fr 1fr 1fr 1fr;
   overflow: hidden;
@@ -31,11 +29,6 @@ const DashboardGrid = styled.div`
       background-color: inherit !important;
     }
   }
-  .health {
-    grid-area: health;
-    background-color: ${(props) => props.theme.backgroundLevel2};
-    padding: 0;
-  }
   .inventory {
     grid-area: inventory;
     padding: ${spacing.sp8} ${spacing.sp12};
@@ -49,22 +42,6 @@ const DashboardGrid = styled.div`
   .network {
     grid-area: network;
     display: flex;
-  }
-  .header {
-    grid-area: header;
-    display: flex;
-    justify-content: flex-end;
-    padding: ${padding.smaller} 0px;
-
-    background-color: ${(props) => {
-      return props.theme.backgroundLevel1;
-    }};
-
-    div {
-      background-color: ${(props) => {
-        return props.theme.backgroundLevel1;
-      }};
-    }
   }
   .metrics {
     grid-area: metrics;
@@ -85,32 +62,35 @@ export const DashboardScrollableArea = styled.div`
 
 const DashboardPage = () => {
   return (
-    <AppContainer.MainContent background="backgroundLevel1">
-      <DashboardGrid>
-        <div className="header">
-          <AppContainer.ContextContainer>
-            <TimespanSelector />
-          </AppContainer.ContextContainer>
-        </div>
-        <SyncedCursorCharts>
-          <DashboardScrollableArea className="health">
-            <DashboardGlobalHealth />
-          </DashboardScrollableArea>
-          <DashboardScrollableArea className="inventory">
-            <DashboardInventory />
-            <DashboardServices />
-          </DashboardScrollableArea>
+    <>
+      <AppContainer.ContextContainer>
+        <Wrap>
+          <div></div>
+          <TimespanSelector />
+        </Wrap>
+      </AppContainer.ContextContainer>
+      <SyncedCursorCharts>
+        <AppContainer.OverallSummary>
+          <DashboardGlobalHealth />
+        </AppContainer.OverallSummary>
+        <AppContainer.MainContent background="backgroundLevel1">
+          <DashboardGrid>
+            <DashboardScrollableArea className="inventory">
+              <DashboardInventory />
+              <DashboardServices />
+            </DashboardScrollableArea>
 
-          <DashboardScrollableArea className="network">
-            <DashboardNetwork />
-          </DashboardScrollableArea>
+            <DashboardScrollableArea className="network">
+              <DashboardNetwork />
+            </DashboardScrollableArea>
 
-          <div className="metrics">
-            <DashboardMetrics />
-          </div>
-        </SyncedCursorCharts>
-      </DashboardGrid>
-    </AppContainer.MainContent>
+            <div className="metrics">
+              <DashboardMetrics />
+            </div>
+          </DashboardGrid>
+        </AppContainer.MainContent>
+      </SyncedCursorCharts>
+    </>
   );
 };
 export default DashboardPage;
