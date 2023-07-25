ef01d442babb714e776532d5028a60620a4d9421
Teddy Andrieux <teddy.andrieux@scality.com>
2023-07-25 13:47:40 +0200

    operator: Bump to operator-sdk v1.25.0
    It come with a bump of go to 1.19
    
    NOTE: Nothing to do for upgrade to v1.24
    Sees: https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.25.0/

--- operator/pkg/controller/utils/image_test.go
+++ operator/pkg/controller/utils/image_test.go
@@ -1,7 +1,7 @@
 package utils_test
 
 import (
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/scality/metalk8s/operator/pkg/controller/utils"
