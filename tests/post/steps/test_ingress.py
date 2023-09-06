25ff21afa0f75bd4bbe0e04268cfc164f5d056c3
JBWatenbergScality <75977494+JBWatenbergScality@users.noreply.github.com>
2023-09-06 11:25:27 +0200

    Fix linter issues

--- tests/post/steps/test_ingress.py
+++ tests/post/steps/test_ingress.py
@@ -569,17 +569,19 @@ def server_returns(host, context, status_code, reason):
     assert response.status_code == int(status_code)
     assert response.reason == reason
 
+
 @then("the server should respond with shell-ui index")
 def shell_ui_returns(host, context):
     response = context.get("response")
     assert response is not None
-    assert "window.shellUIRemoteEntryUrl = \"/shell/remoteEntry.js" in response.text
+    assert "window.shellUIRemoteEntryUrl = "/shell/remoteEntry.js" in response.text
+
 
 @then("the server should not respond with shell-ui index")
 def shell_ui_not_returns(host, context):
     response = context.get("response")
     assert response is not None
-    assert "window.shellUIRemoteEntryUrl = \"/shell/remoteEntry.js" not in response.text
+    assert "window.shellUIRemoteEntryUrl = "/shell/remoteEntry.js" not in response.text
 
 
 @then("the server should not respond")
