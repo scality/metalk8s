dae35db022e8f002af9248d8be7fb385ac65c557
Jean-Marc Millet <jean-marc.millet@scality.com>
2023-12-29 17:37:27 +0100

    import types from modules, delete useless styled.ts for Theme

--- ui/src/styled.d.ts
+++
@@ -1,5 +0,0 @@
-import 'styled-components';
-import { CoreUITheme } from '@scality/core-ui/dist/style/theme';
-declare module 'styled-components' {
-  export interface DefaultTheme extends CoreUITheme {}
-}
