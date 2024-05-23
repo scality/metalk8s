9ad0aadd43b286c1e327ee06e259d62441bba9c0
Ayoub Nasr <ayoub.nasr@scality.com>
2024-05-23 11:18:19 +0200

    Bump to 3007.1 - don't patch for await none bug

--- images/salt-master/patch.sh
+++ images/salt-master/patch.sh
@@ -2,14 +2,11 @@
 
 patch_salt() {
     declare -a PATCH_FILES=(
-        loader/lazy.py
-        metaproxy/deltaproxy.py
-        metaproxy/proxy.py
-        minion.py
+        states/x509_v2.py
     )
     for pfile in "${PATCH_FILES[@]}"; do
         mkdir -p "/tmp/$(dirname "$pfile")"
-        curl -Lo "/tmp/$pfile" "https://github.com/saltstack/salt/raw/3007.x/salt/$pfile"
+        curl -Lo "/tmp/$pfile" "https://github.com/lkubb/salt/raw/x509-v2-signing-key-policy/salt/$pfile"
         cp "/tmp/$pfile" "/opt/saltstack/salt/lib/python3.10/site-packages/salt/$pfile"
     done
 }
