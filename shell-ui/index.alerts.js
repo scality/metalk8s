989a5805b3cacb306b03a88e2671b4b3677bad64
Patrick Ear <patrick.ear@scality.com>
2024-06-21 18:14:51 +0200

    remove legacy files

--- shell-ui/index.alerts.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import "@fortawesome/fontawesome-free/js/all.js";
-
-import AlertsMdx from './src/alerts/index.mdx';
-
-ReactDOM.render(<>
-    <AlertsMdx />
-</>, document.getElementById('app'));
