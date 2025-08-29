4d062d1f5ccbeceebdb718b01559f0561fc83ed4
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-08-29 12:20:35 +0200

    Remove SpacedBox as it ill be removed from Core-UI

--- ui/src/components/DashboardNetwork.tsx
+++ ui/src/components/DashboardNetwork.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useIntl } from 'react-intl';
-import { spacing } from '@scality/core-ui/dist/style/theme';
 import {
   PageSubtitle,
   GraphsWrapper,
@@ -9,11 +8,12 @@ import {
 import DashboardPlaneHealth from './DashboardPlaneHealth';
 import DashboardBandwidthChart from './DashboardBandwidthChart';
 import { DashboardScrollableArea } from '../containers/DashboardPage';
-import { SpacedBox } from '@scality/core-ui';
 import { useShowQuantileChart } from '../hooks';
-import { Tooltip, Icon, SmallerText } from '@scality/core-ui';
+import { QuantileHelpTooltip } from './DashboardMetrics';
+import { Box } from '@scality/core-ui/dist/next';
+import { spacing } from '@scality/core-ui';
 export const NetworkContainer = styled.div`
-  padding: ${spacing.sp2} ${spacing.sp4};
+  padding: ${spacing.r2} ${spacing.r4};
   display: flex;
   flex-direction: column;
   flex-grow: 1;
@@ -21,7 +21,7 @@ export const NetworkContainer = styled.div`
 `;
 export const PanelActions = styled.div`
   display: flex;
-  padding: ${spacing.sp4};
+  padding: ${spacing.r4};
   align-items: center;
   justify-content: space-between;
 `;
@@ -37,40 +37,12 @@ const DashboardNetwork = () => {
     >
       <PanelActions>
         <PageSubtitle>
-          <SpacedBox mr={8}>
-            {' '}
+          <Box mr={spacing.r8}>
             {intl.formatMessage({
               id: 'network',
             })}
-          </SpacedBox>
-          {isShowQuantileChart && (
-            <Tooltip
-              placement="bottom"
-              overlay={
-                <SmallerText
-                  // @ts-expect-error - FIXME when you are working on it
-                  style={{
-                    minWidth: '30rem',
-                    display: 'block',
-                    textAlign: 'left',
-                  }}
-                >
-                  {intl
-                    .formatMessage({
-                      id: 'network_quantile_explanation',
-                    })
-                    .split('\n')
-                    .map((line, key) => (
-                      <SpacedBox key={`globalheathexplanation-${key}`} mb={8}>
-                        {line}
-                      </SpacedBox>
-                    ))}
-                </SmallerText>
-              }
-            >
-              <Icon name="Info" color="buttonSecondary" />
-            </Tooltip>
-          )}
+          </Box>
+          {isShowQuantileChart && <QuantileHelpTooltip />}
         </PageSubtitle>
       </PanelActions>
 
