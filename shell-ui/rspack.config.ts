5640809b65f90d9703ca8a2716b55a3ce8a8e02b
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-12-18 17:53:12 +0100

    put back core-ui singleton in rspack configs

--- shell-ui/rspack.config.ts
+++ shell-ui/rspack.config.ts
@@ -122,6 +122,9 @@ const config: Configuration = {
         ...Object.fromEntries(
           Object.entries(deps).map(([key, version]) => [key, {}]),
         ),
+        '@scality/core-ui': {
+          singleton: true,
+        },
         'react-intl': {
           eager: true,
           singleton: true,
--- ui/rspack.config.ts
+++ ui/rspack.config.ts
@@ -132,6 +132,9 @@ const config: Configuration = {
         ...Object.fromEntries(
           Object.entries(deps).map(([key, version]) => [key, {}]),
         ),
+        '@scality/core-ui': {
+          singleton: true,
+        },
         '@scality/module-federation': {
           singleton: true,
         },
