136cf78442011ea1d035f0dd8f4cc52ec0470464
Patrick Ear <patrick.ear@outlook.com>
2026-01-29 18:40:54 +0100

    rework dev environment for shell-ui and metalk8s-ui

--- shell-ui/rspack.config.ts
+++ shell-ui/rspack.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
+import fs from 'fs';
 import packageJson from './package.json';
-import { Configuration } from '@rspack/cli';
+import type { Configuration } from '@rspack/cli';
 import * as rspack from '@rspack/core';
 import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
@@ -11,6 +12,32 @@ const controlPlaneIP = '{{IP}}';
 const controlPlaneBaseUrl = `https://${controlPlaneIP}:8443`;
 
 const isProduction = process.env.NODE_ENV === 'production';
+
+let proxy = [
+  {
+    context: ['/static/js', '/.well-known'],
+    target: 'http://localhost:3000',
+    secure: false,
+  },
+  {
+    context: [
+      '/auth',
+      '/api/kubernetes',
+      '/api/salt',
+      '/api/prometheus',
+      '/api/alertmanager',
+      '/api/loki',
+      '/grafana',
+      '/docs',
+    ],
+    target: controlPlaneBaseUrl,
+    secure: false,
+  },
+];
+
+if (fs.existsSync(path.join(__dirname, 'dev.proxy.json'))) {
+  proxy = JSON.parse(fs.readFileSync(path.join(__dirname, 'dev.proxy.json'), 'utf8'));
+}
 
 const config: Configuration = {
   experiments: {
@@ -110,18 +137,13 @@ const config: Configuration = {
         './alerts/AlertProvider': './src/alerts/AlertProvider.tsx',
         './alerts/alertHooks': './src/alerts/alertHooks.ts',
         './navbar/navbarHooks': './src/navbar/navbarHooks.ts',
-        './moduleFederation/ConfigurationProvider':
-          './src/initFederation/ConfigurationProviders.tsx',
-        './moduleFederation/ShellConfigurationProvider':
-          './src/initFederation/ShellConfigProvider.tsx',
-        './moduleFederation/UIListProvider':
-          './src/initFederation/UIListProvider.tsx',
+        './moduleFederation/ConfigurationProvider': './src/initFederation/ConfigurationProviders.tsx',
+        './moduleFederation/ShellConfigurationProvider': './src/initFederation/ShellConfigProvider.tsx',
+        './moduleFederation/UIListProvider': './src/initFederation/UIListProvider.tsx',
         './useNotificationCenter': './src/useNotificationCenter.ts',
       },
       shared: {
-        ...Object.fromEntries(
-          Object.entries(deps).map(([key, version]) => [key, {}]),
-        ),
+        ...Object.fromEntries(Object.entries(deps).map(([key, version]) => [key, {}])),
         'react-intl': {
           eager: true,
           singleton: true,
@@ -188,7 +210,7 @@ const config: Configuration = {
       excludedChunks: ['shell'],
     }),
     new rspack.CopyRspackPlugin({
-      patterns: [{ from: 'public/shell' }],
+      patterns: [{ from: 'public' }],
     }),
     process.env.RSDOCTOR && new RsdoctorRspackPlugin({}),
   ].filter(Boolean),
@@ -205,27 +227,23 @@ const config: Configuration = {
       },
     },
     static: path.join(__dirname, 'public'),
-    proxy: [
-      {
-        context: ['/static/js', '/.well-known'],
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-      {
-        context: [
-          '/auth',
-          '/api/kubernetes',
-          '/api/salt',
-          '/api/prometheus',
-          '/api/alertmanager',
-          '/api/loki',
-          '/grafana',
-          '/docs',
-        ],
-        target: controlPlaneBaseUrl,
-        secure: false,
-      },
-    ],
+    proxy: proxy,
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.app.get('/shell/config.json', (req, res) => {
+        const devConfigPath = path.join(__dirname, 'public/dev.config.json');
+        const defaultConfigPath = path.join(__dirname, 'public/config.json');
+        const configPath = fs.existsSync(devConfigPath) ? devConfigPath : defaultConfigPath;
+        res.sendFile(configPath);
+      });
+
+      devServer.app.get('/shell/deployed-ui-apps.json', (req, res) => {
+        const devConfigPath = path.join(__dirname, 'public/dev.deployed-ui-apps.json');
+        const defaultConfigPath = path.join(__dirname, 'public/deployed-ui-apps.json');
+        const configPath = fs.existsSync(devConfigPath) ? devConfigPath : defaultConfigPath;
+        res.sendFile(configPath);
+      });
+      return middlewares;
+    },
   },
 };
 
