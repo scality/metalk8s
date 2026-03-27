a1fc172315355ffed15479064cf2679605b8bce5
Jean-Baptiste Watenberg <jean-baptiste.watenberg@scality.com>
2026-03-27 18:07:42 +0100

    fix(MK8S-196): add @mcp-b to Jest transform allowlist for ESM compat

--- shell-ui/jest.config.js
+++ shell-ui/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  transformIgnorePatterns: ['/node_modules/(?!vega-lite|@scality|pretty-bytes|uuid|@fortawesome)'],
+  transformIgnorePatterns: ['/node_modules/(?!vega-lite|@scality|pretty-bytes|uuid|@fortawesome|@mcp-b)'],
   setupFilesAfterEnv: ['./src/setupTests.ts'],
   clearMocks: true,
   moduleNameMapper: {
