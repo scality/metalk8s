2a7365f9af232f53490fd89e650aecef2c5b2d27
Teddy Andrieux <teddy.andrieux@scality.com>
2026-04-27 06:12:13 +0000

    fix(salt): explicitly set the name argument in module.run

--- salt/_states/metalk8s.py
+++ salt/_states/metalk8s.py
@@ -140,7 +140,7 @@ def module_run(name, attempts=1, sleep_time=10, **kwargs):
     ret = {"name": name, "changes": {}, "result": False, "comment": ""}
     while retry > 0 and not ret["result"]:
         try:
-            ret = __states__["module.run"](name, **kwargs)
+            ret = __states__["module.run"](name=name, **kwargs)
         except Exception as exc:  # pylint: disable=broad-except
             ret["comment"] = str(exc)
 
