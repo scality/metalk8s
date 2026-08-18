b0f08c775af350f06a2950b6b6e443689a3c5364
Ayoub Nasr <ayoub.nasr@scality.com>
2026-08-18 14:47:23 +0200

    feat: add metalk8s-registry-operator to buildchain
    Ref: MK8S-168

--- buildchain/buildchain/image.py
+++ buildchain/buildchain/image.py
@@ -227,6 +227,7 @@ IMGS_PER_REPOSITORY: Dict[str, List[str]] = {
         "crl-operator",
         "disk-management-agent",
         "node-warden-operator",
+        "metalk8s-registry-operator",
     ],
 }
 
